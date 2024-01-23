c047e1432bcdced36dd2c78d54fb3d7909dbf513
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-01-23 12:01:51 +0100

    some changes

--- apps/summerfi-api/lib/serverless-shared/src/helpers.ts
+++ apps/summerfi-api/lib/serverless-shared/src/helpers.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 export const getDefaultErrorMessage = (error: any): string => {
   if (error && 'message' in error && typeof error.message === 'string') {
     return error.message
