f5c533320f85fd9b1033bf3a865947f359902d66
Roberto Cano <3525807+robercano@users.noreply.github.com>
2024-01-24 13:26:54 +0100

    fix: adjust eslint rules

--- packages/eslint-config/library.cjs
+++ packages/eslint-config/library.cjs
@@ -4,25 +4,30 @@ const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['eslint:recommended', 'prettier', 'eslint-config-turbo', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'eslint-config-turbo',
+    'plugin:@typescript-eslint/recommended',
+  ],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
     node: true,
-    jest: true
+    jest: true,
   },
   plugins: ['unused-imports'],
   rules: {
     'no-unused-vars': 'off',
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'off',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/ban-ts-comment": "off"
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
   },
   settings: {
     'import/resolver': {
