62a7155ecd48a6781aaec479896bc82785f61ee2
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-01-26 10:53:21 +0100

    Update libraries

--- apps/summerfi-api/lib/setup-trigger-function/src/services/get-addresses.ts
+++ apps/summerfi-api/lib/setup-trigger-function/src/services/get-addresses.ts
@@ -11,14 +11,6 @@ export function getAddresses(chainId: ChainId): Addresses {
   const network = NetworkByChainID[chainId]
   const addresses = ADDRESSES[network]
 
-  if (chainId === ChainId.BASE) {
-    return {
-      AutomationBot: '0x96D494b4544Bb7c3CB687ef7a9886Ed469e01ed8' as Address,
-      AaveDataPoolProvider: addresses['aave']['v3'].PoolDataProvider as Address,
-      AaveOracle: addresses['aave']['v3'].Oracle as Address,
-    }
-  }
-
   return {
     AutomationBot: addresses['automation']['AutomationBotV2'] as Address,
     AaveDataPoolProvider: addresses['aave']['v3'].PoolDataProvider as Address,
