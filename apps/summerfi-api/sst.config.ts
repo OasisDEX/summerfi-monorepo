bbcd6673ee7a202a2c7be5f7d2e6a556a5224eaa
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-01-29 20:12:38 +0100

    cleanup

--- apps/summerfi-api/sst.config.ts
+++ apps/summerfi-api/sst.config.ts
@@ -3,14 +3,14 @@ import { API } from './stacks/summer-stack'
 
 export const sstConfig: SSTConfig = {
   config(_input) {
-    if (!['', 'feature', 'staging', 'production'].includes(_input.stage ?? '')) {
+    if (!['dev', 'feature', 'staging', 'production'].includes(_input.stage ?? '')) {
       throw new Error('Invalid stage')
     }
     return {
       name: 'summerfi-stack',
       region: `${process.env.AWS_REGION}`,
       profile: `${process.env.AWS_PROFILE}`,
-      stage: `${_input.stage ?? 'dev'}`,
+      stage: `${_input.stage}`,
     }
   },
   stacks(app) {
