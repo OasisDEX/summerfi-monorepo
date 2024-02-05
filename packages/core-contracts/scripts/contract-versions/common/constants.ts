05b22c842ecea2847b2a7ae742ef6caba7cd37fb
Roberto Cano <3525807+robercano@users.noreply.github.com>
2024-02-05 19:28:52 +0100

    feat: add contract versioning tools

---
+++ packages/core-contracts/scripts/contract-versions/common/constants.ts
@@ -0,0 +1,5 @@
+export const ContractsVersionsDefaultName = 'contracts-versions'
+export const SnapshotDirectory = 'versions'
+export const SnapshotTrailingTag = 'snapshot'
+export const SnapshotTagDelimiter = '.'
+export const SnapshotNameExtension = 'json'
