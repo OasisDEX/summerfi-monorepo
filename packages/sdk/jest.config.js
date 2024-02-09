5e82a8d1dca2092ab04449f31d34d20288c9ca06
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-02-09 14:58:44 +0100

    Fixed lint and jest

--- packages/sdk/jest.config.js
+++ packages/sdk/jest.config.js
@@ -1,29 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./tsconfig')
+const sharedConfig = require('@summerfi/jest-config/jest.base')
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  extensionsToTreatAsEsm: ['.ts'],
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  testMatch: ['**/tests/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
-  silent: true,
-  maxWorkers: 1,
-  testTimeout: 10000,
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['dist', 'node_modules'],
-  modulePaths: ['src'],
-  moduleNameMapper: pathsToModuleNameMapper({
-    '~sdk': ['<rootDir>/src/index.ts'],
-    '~sdk/*': ['<rootDir>/src/*'],
-  }),
-  transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        useESM: true,
-        tsconfig: '<rootDir>/tsconfig.test.json',
-      },
-    ],
-  },
+  ...sharedConfig(compilerOptions),
 }
