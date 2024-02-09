7afa79c5ac1294ebff5c9771e2b463f1c21233f5
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-02-09 14:07:05 +0100

    Fixes for sst setup

--- apps/summerfi-api/stacks/sdk.ts
+++ apps/summerfi-api/stacks/sdk.ts
@@ -1,12 +1,12 @@
 import { Api, StackContext, Function } from 'sst/constructs'
 
 export function addSdkConfig({ stack, api }: StackContext & { api: Api }) {
-  const exampleFunction = new Function(stack, 'sdk-demo-function', {
-    handler: 'lib/sdk-demo-function/src/index.handler',
+  const sdkRouterFunction = new Function(stack, 'sdk-router-function', {
+    handler: 'lib/sdk-router-function/src/index.handler',
     runtime: 'nodejs20.x',
   })
 
   api.addRoutes(stack, {
-    'GET /api/sdk': exampleFunction,
+    'GET /api/sdk': sdkRouterFunction,
   })
 }
