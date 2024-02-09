4eaf0d438cdd9e3870f0040a7517348c99fd5db6
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-02-09 12:38:16 +0100

    Fixes for sst setup (#51)

--- apps/summerfi-api/sst.config.ts
+++ apps/summerfi-api/sst.config.ts
@@ -8,7 +8,11 @@ export const sstConfig: SSTConfig = {
     if (_input.stage === undefined && process.env.SST_USER === undefined) {
       throw new Error('Please specify stage or set SST_USER env variable')
     }
-    if (_input.stage && !availableStage.includes(_input.stage)) {
+    if (
+      _input.stage &&
+      !availableStage.includes(_input.stage) &&
+      !_input.stage.startsWith('dev-')
+    ) {
       throw new Error('Invalid stage, use one of: ' + availableStage.join(', '))
     }
     const stage = _input.stage ?? `dev-${process.env.SST_USER}`
