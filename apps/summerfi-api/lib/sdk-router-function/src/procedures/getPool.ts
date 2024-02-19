a5b5d1cfad76f13c2a40b3a17ad2209f579c30e7
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-02-19 10:29:36 +0100

    added pools

---
+++ apps/summerfi-api/lib/sdk-router-function/src/procedures/getPool.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod'
+import { publicProcedure } from '~src/trpc'
+import { mockPool } from '@summerfi/sdk/mocks'
+import type { Wallet } from '@summerfi/sdk/common'
+import type { PoolParameters, Protocol } from '@summerfi/sdk/protocols'
+
+type PoolParams = Parameters<typeof mockPool>[0]
+
+export const getPool = publicProcedure
+  .input(
+    z.object({
+      protocol: z.custom<Protocol>((id) => id !== undefined),
+      poolParameters: z.custom<PoolParameters>((chain) => chain !== undefined),
+      protocolParameters: z.custom<Wallet>((wallet) => wallet !== undefined).optional(),
+    }),
+  )
+  .query(async (opts) => {
+    // get position from chain / graph
+    const params: PoolParams = opts.input
+    return await mockPool(params)
+  })
