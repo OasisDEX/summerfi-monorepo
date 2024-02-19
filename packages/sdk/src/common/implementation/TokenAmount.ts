d7717250dc0ca70660f016b2d9d66b7a73b877d0
Damian Paszkowski <paszko.damian@gmail.com>
2024-02-19 19:25:03 +0100

    feat: step reducer

--- packages/sdk/src/common/implementation/TokenAmount.ts
+++ packages/sdk/src/common/implementation/TokenAmount.ts
@@ -9,15 +9,38 @@ import { Token } from './Token'
  */
 export class TokenAmount implements Printable {
   public readonly token: Token
-  public readonly amount: string
+  public readonly amount: string // propably we need to go with BigNumber
 
   constructor(token: Token, amount: string) {
     this.token = token
     this.amount = amount
   }
 
+// amount human readable (1eth = 1, 1btc = 1 etc)
   public static createFrom(params: { token: Token; amount: string }): TokenAmount {
     return new TokenAmount(params.token, params.amount)
+  }
+
+  public static createFromBaseUnit(parmas: {token: Token, amount: string}): TokenAmount {
+    return new TokenAmount(parmas.token, (parseFloat(parmas.amount) / Math.pow(10, parmas.token.decimals)).toString())
+  }
+
+  public add(amount: TokenAmount): TokenAmount {
+    if (amount.token.symbol !== this.token.symbol) {
+      throw new Error('Token symbols do not match')
+    }
+
+    // TODO: we need to go with BigNumber
+    return new TokenAmount(this.token, (parseFloat(this.amount) + parseFloat(amount.amount)).toString())
+  }
+
+  public substrac(amount: TokenAmount): TokenAmount {
+    if (amount.token.symbol !== this.token.symbol) {
+      throw new Error('Token symbols do not match')
+    }
+
+    // TODO: we need to go with BigNumber
+    return new TokenAmount(this.token, (parseFloat(this.amount) - parseFloat(amount.amount)).toString())
   }
 
   public toString(): string {
--- packages/sdk/src/orders/interfaces/common/Simulation.ts
+++ packages/sdk/src/orders/interfaces/common/Simulation.ts
@@ -1,4 +1,4 @@
-import { Token, TokenAmount } from '~sdk/common'
+import { Address, Token, TokenAmount } from '~sdk/common'
 import { SimulationType } from './SimulationType'
 import { Position } from '~sdk/users'
 
@@ -10,6 +10,7 @@ export enum SimulationSteps {
   PullToken = 'PullToken',
   ReturnFunds = 'ReturnFunds',
   PaybackFlashloan = 'PaybackFlashloan',
+  Awesomeness = 'Awesomeness',
 }
 
 interface ValueReference<T> {
@@ -17,15 +18,18 @@ interface ValueReference<T> {
   path: [string, string]
 }
 
+type ReferencableField<T> = T | ValueReference<T>
+
 export enum FlashloanProvider {
   Maker = 'Maker',
   Balancer = 'Balancer',
 }
 
-interface Step<T extends SimulationSteps, I, O> {
+interface Step<T extends SimulationSteps, I, O = undefined> {
   type: T,
   inputs: I,
   outputs: O,
+  skip?: boolean
 }
 
 interface FlashloanStep extends Step<
@@ -33,20 +37,18 @@ interface FlashloanStep extends Step<
   {
     amount: TokenAmount
     provider: FlashloanProvider
-  },
-  never
+  }
 > { }
 
 interface PullTokenStep extends Step<
   SimulationSteps.PullToken,
-  { amount: TokenAmount | ValueReference<TokenAmount> },
-  never
+  { amount: ReferencableField<TokenAmount> }
 > { }
 
 interface DepositBorrowStep extends Step<SimulationSteps.DepositBorrow,
   {
-    depositAmount: TokenAmount | ValueReference<TokenAmount>
-    borrowAmount: TokenAmount| ValueReference<TokenAmount>
+    depositAmount: ReferencableField<TokenAmount>
+    borrowAmount: ReferencableField<TokenAmount>
     position: Position
     additionalDeposit?: ValueReference<TokenAmount>
   },
@@ -59,8 +61,8 @@ interface DepositBorrowStep extends Step<SimulationSteps.DepositBorrow,
 interface PaybackWithdrawStep extends Step<
   SimulationSteps.PaybackWithdraw,
   {
-    paybackAmount: TokenAmount
-    withdrawAmount: TokenAmount
+    paybackAmount: ReferencableField<TokenAmount>
+    withdrawAmount: ReferencableField<TokenAmount>
     position: Position,
   },
   {
@@ -84,14 +86,12 @@ interface SwapStep extends Step<
 
 interface ReturnFunds extends Step<
   SimulationSteps.ReturnFunds,
-  { token: Token },
-  never
+  { token: Token }
 > { }
 
 interface RepayFlashloan extends Step<
   SimulationSteps.PaybackFlashloan,
-  never,
-  never
+  { amount: TokenAmount }
 > { }
 
 export type Steps =
@@ -115,9 +115,9 @@ export interface Simulation<T extends SimulationType> {
   // OPEN QUESTION: where errors and warnings and info messages?
 }
 
-interface SchemaStep {
+export interface SchemaStep {
   name: string
-  step: SimulationSteps
+  step: Steps['type']
   optional: boolean
 }
 
@@ -157,20 +157,15 @@ const refinanceSchema = [
   {
     // In case of target debt being different then source debt we need a swap, 
     // We cannot forsee the exact amount of the swap, so we need to return excess tokens to user
-    name: 'returnDebtFunds' as const,
+    name: 'returnDebtFunds',
     step: SimulationSteps.ReturnFunds,
     optional: true,
   }
 ] as const
 
-type StepNames<T extends SimulationSchema> = T[number]['name']
-
-type StorageMap<T extends SimulationSchema> = {[K in T[number]['name']]: T[number]['step'] }
-
-type names = StorageMap<typeof refinanceSchema>
-
 type Tail<T extends readonly any[]> = ((...t: T) => void) extends ((h: any, ...r: infer R) => void) ? R : never
 type EmptyArray = readonly []
+type Where<T, U> = T extends U ? T : never
 
 interface SimulationState {
   // mapping between token address and token amount
@@ -179,30 +174,166 @@ interface SimulationState {
   storage: Record<string, Steps['outputs']>
 }
 
+function isReference<T>(value: ReferencableField<T>): value is ValueReference<T> {
+  return (value as ValueReference<T>).path !== undefined && (value as ValueReference<T>).estimatedValue !== undefined
+}
+
+function getReferencedValue<T>(referencableValue: ReferencableField<T>): T {
+  if (isReference(referencableValue)) {
+    return referencableValue.estimatedValue
+  }
+  return referencableValue
+}
+
+function getTokenBalance(token: Token, balances: Record<string, TokenAmount>): TokenAmount {
+  return balances[token.address.hexValue] || TokenAmount.createFrom({ amount: '0', token })
+}
+
+function addBalance(amount: TokenAmount, balance: Record<string, TokenAmount>): Record<string, TokenAmount> {
+  return {
+    ...balance,
+    [amount.token.address.hexValue]: balance[amount.token.address.hexValue] ? balance[amount.token.address.hexValue].add(amount) : amount
+  }
+}
+
+function subtractBalance(amount: TokenAmount, balance: Record<string, TokenAmount>): Record<string, TokenAmount> {
+  return {
+    ...balance,
+    [amount.token.address.hexValue]: balance[amount.token.address.hexValue].substrac(amount)
+  }
+}
+
+function switchCheck(a: never): never {
+  throw new Error('Run out of cases')
+}
+
+function processStep(step: Steps, state: SimulationState) {
+  switch (step.type) {
+    case SimulationSteps.Flashloan: {
+      return {
+        ...state,
+        balances: addBalance(step.inputs.amount, state.balances)
+      }
+    }
+    case SimulationSteps.DepositBorrow: {
+      const afterDeposit = subtractBalance(getReferencedValue(step.inputs.depositAmount), state.balances)
+      const afterBorrow = addBalance(getReferencedValue(step.inputs.borrowAmount), afterDeposit)
+      // step.inputs.position// `dpmId-protocol(aavev3 | aavev2)`
+      // step.inputs.position// `dpmId-protocol(morpho)-morketId`
+      // step.inputs.position// `dpmId-protocol(ajna)-`
+      // const updatedPosition = deposit(state.positions[step.inputs.position.positionId] || step.inputs.position, amount): Position
+      return {
+        ...state,
+        positions: {
+          [step.inputs.position.positionId.id]: updatedPosition
+        },
+        balances: afterBorrow
+      }
+    }
+    case SimulationSteps.PaybackWithdraw: {
+      const afterPayback = addBalance(getReferencedValue(step.inputs.paybackAmount), state.balances)
+      const afterWithdraw = subtractBalance(getReferencedValue(step.inputs.withdrawAmount), afterPayback)
+      return {
+        ...state,
+        balances: afterWithdraw
+      }
+    }
+    case SimulationSteps.Swap: {
+      const balanceWithoutFromToken = subtractBalance(step.inputs.fromTokenAmount, state.balances)
+      const balanceWithToToken = addBalance(step.outputs.recievedAmount, balanceWithoutFromToken)
+      return {
+        ...state,
+        balances: balanceWithToToken
+      }
+    }
+    case SimulationSteps.ReturnFunds: {
+      return {
+        ...state,
+        balances: subtractBalance(getTokenBalance(step.inputs.token, state.balances), state.balances)
+      }
+    }
+    case SimulationSteps.PaybackFlashloan: {
+      return {
+        ...state,
+        balances: subtractBalance(step.inputs.amount, state.balances)
+      }
+    }
+    case SimulationSteps.PullToken: {
+      return {
+        ...state,
+        balances: addBalance(getReferencedValue(step.inputs.amount), state.balances)
+      }
+    }
+    default: {
+      switchCheck(step)
+    }
+  }
+}
+
+type NextFunction<T extends SimulationSchema, Ctx> = (ctx: Ctx) => T extends EmptyArray ? never : Where<Steps, { type: T[0]['step'] }>
+
 class Simulator<S extends SimulationSchema> {
   private state: SimulationState
+  private steps: Steps[] = []
 
-  private constructor(public schema: S, state: SimulationState = { balances: {}, positions: {}, storage: {} }) {
+  private constructor(public schema: S, steps: Steps[] = [], state: SimulationState = { balances: {}, positions: {}, storage: {} }) {
     this.state = state
+    this.steps = steps
   }
 
   static create<S extends SimulationSchema>(schema: S) {
     return new Simulator(schema)
   }
 
-  public next(next: S extends EmptyArray ? never : S[0]['step']): Simulator<Tail<S>> {
-    const [, ...rest] = this.schema
-    return new Simulator(rest, this.state) as Simulator<Tail<S>>
+  public next(next: NextFunction<S, SimulationState>): Simulator<Tail<S>> {
+    const [currentSchemaStep, ...rest] = this.schema
+    const proccesedStep = next(this.state)
+
+    if (currentSchemaStep.step !== proccesedStep.type) {
+      throw new Error(`Invalid step, extected ${currentSchemaStep.step} but got ${proccesedStep.type}`)
+    }
+
+    if (!currentSchemaStep.optional && proccesedStep.skip) {
+      throw new Error(`Step ${currentSchemaStep.name} is not optional`)
+    }
+
+    const nextState = processStep(proccesedStep, this.state)
+
+    this.steps.push(proccesedStep)
+    this.state = nextState
+
+    return new Simulator<Tail<S>>(rest as Tail<S>, this.steps, this.state)
   }
 }
 
-const simulator =  Simulator.create(refinanceSchema)
+const simulator = Simulator.create(refinanceSchema)
 
 simulator
-  .next(SimulationSteps.Flashloan)
-  .next(SimulationSteps.PaybackWithdraw)
-  .next(SimulationSteps.Swap)
-  .next(SimulationSteps.DepositBorrow)
-  .next(SimulationSteps.Swap)
-  .next(SimulationSteps.PaybackFlashloan)
-  .next(SimulationSteps.ReturnFunds)
+  .next((ctx) => ({
+    name: 'fl',
+    type: SimulationSteps.Flashloan,
+    inputs: {
+      amount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }),
+      provider: FlashloanProvider.Maker
+    },
+    outputs: undefined
+  }))
+  .next((ctx) => ({
+    type: SimulationSteps.PaybackWithdraw,
+    inputs: {
+      paybackAmount: {
+        estimatedValue: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }),
+        path: ['fl', 'output']
+      },
+      withdrawAmount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }),
+      position: { positionId: { id: '' }, debtAmount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }), collateralAmount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }), pool: { poolId: { id: '' }, protocolId: { id: '' }, type: 'Lending', debtToken: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 }, collateralToken: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } } }
+    },
+    outputs: {
+      paybackAmount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } }),
+      withdrawAmount: TokenAmount.createFrom({ amount: '0', token: { address: Address.createFrom({ hexValue: '0x00' }), decimals: 18 } })
+    }
+    }
+))
+
+
+
