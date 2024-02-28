8a9396881664c216857f1aa8243de795472a7f38
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-02-28 07:32:13 +0100

    Add portfolio functions and improve environment handling (#90)
    Added portfolio functions to the Summerfi API and made changes to better
    handle environment variables. These updates help ensure necessary
    environment variables are set to deploy functions. Additionally, the
    build command was modified in portfolio-assets-function/package.json for
    significant performance improvements.

--- apps/summerfi-api/stacks/summer-stack.ts
+++ apps/summerfi-api/stacks/summer-stack.ts
@@ -2,6 +2,7 @@ import { StackContext, Api } from 'sst/constructs'
 import { addTriggersConfig } from './triggers'
 import { addSdkConfig } from './sdk'
 import { addMigrationsConfig } from './migrations'
+import { addPortfolioConfig } from './portfolio'
 
 export function API(stackContext: StackContext) {
   const { stack } = stackContext
@@ -15,6 +16,7 @@ export function API(stackContext: StackContext) {
   addTriggersConfig({ ...stackContext, api })
   addSdkConfig({ ...stackContext, api })
   addMigrationsConfig({ ...stackContext, api })
+  addPortfolioConfig({ ...stackContext, api })
 
   stack.addOutputs({
     ApiEndpoint: api.url,
