d4e33d0f9b6cc93db43fc1eef4c8eb11313e918b
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-03-04 16:02:03 +0100

    clean

--- sdk/sdk-common/src/client/implementation/simulations/SimulationManager.ts
+++ sdk/sdk-common/src/client/implementation/simulations/SimulationManager.ts
@@ -1,7 +1,3 @@
-import type { ChainInfo } from '~sdk-common/common/implementation/ChainInfo'
-import type { Position } from '~sdk-common/common/implementation/Position'
-import type { PositionId } from '~sdk-common/common/implementation/PositionId'
-import type { Wallet } from '~sdk-common/common/implementation/Wallet'
 import { RefinanceSimulationManager } from './RefinanceSimulationManager'
 
 export class SimulationManager {
@@ -13,15 +9,5 @@ export class SimulationManager {
 
   public constructor() {
     this.refinance = new RefinanceSimulationManager()
-  }
-
-  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-  public async getPositions(_params: {
-    networks: ChainInfo[]
-    wallet: Wallet
-    ids?: PositionId[]
-  }): Promise<Position[]> {
-    // TODO: Implement
-    return [] as Position[]
   }
 }
