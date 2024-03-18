dfaf7675621020f102deca4de2aadfbbc75373ba
Roberto Cano <3525807+robercano@users.noreply.github.com>
2024-03-18 19:27:26 +0100

    test: add new order test

---
+++ sdk/sdk-client/tests/queries/newOrder.subtest.ts
@@ -0,0 +1,136 @@
+import { IProtocol, PoolType, ProtocolName } from '@summerfi/sdk-common/protocols'
+import { SDKManager } from '../../src/implementation/SDKManager'
+import { RPCClientType } from '../../src/rpc/SDKClient'
+import { MakerLendingPool, SparkLendingPool } from '@summerfi/protocol-manager'
+import { Simulation, SimulationType } from '@summerfi/sdk-common/simulation'
+import {
+  Address,
+  ChainFamilyMap,
+  ChainInfo,
+  Maybe,
+  Percentage,
+  Position,
+  PositionId,
+  RiskRatio,
+  Token,
+  TokenAmount,
+} from '@summerfi/sdk-common/common'
+import { IPositionsManager, Order } from '@summerfi/sdk-common/orders'
+import { User } from '../../src/implementation/User'
+
+export default async function simulateNewOrder() {
+  const chainInfo: ChainInfo = ChainFamilyMap.Ethereum.Mainnet
+
+  // Tokens
+  const WETH = Token.createFrom({
+    chainInfo,
+    address: Address.createFrom({ value: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' }),
+    symbol: 'WETH',
+    name: 'Wrapped Ether',
+    decimals: 18,
+  })
+
+  const DAI = Token.createFrom({
+    chainInfo,
+    address: Address.createFrom({ value: '0x6B175474E89094C44Da98b954EedeAC495271d0F' }),
+    symbol: 'DAI',
+    name: 'Dai Stablecoin',
+    decimals: 18,
+  })
+
+  const protocol: IProtocol = {
+    name: ProtocolName.Maker,
+    chainInfo: chainInfo,
+  }
+
+  const pool: MakerLendingPool = {
+    type: PoolType.Lending,
+    protocol: protocol,
+    poolId: {
+      protocol: protocol,
+    },
+    collaterals: {},
+    debts: {},
+    baseCurrency: DAI,
+  }
+
+  const prevPosition: Position = {
+    pool: pool,
+    debtAmount: TokenAmount.createFrom({ token: DAI, amount: '56.78' }),
+    collateralAmount: TokenAmount.createFrom({ token: WETH, amount: '105.98' }),
+    positionId: PositionId.createFrom({ id: '1234567890' }),
+    riskRatio: RiskRatio.createFrom({
+      ratio: Percentage.createFrom({ percentage: 0.5 }),
+      type: RiskRatio.type.LTV,
+    }),
+  }
+
+  const targetPool: SparkLendingPool = {
+    type: PoolType.Lending,
+    protocol: protocol,
+    poolId: {
+      protocol: protocol,
+    },
+    collaterals: {},
+    debts: {},
+    baseCurrency: DAI,
+  }
+
+  const simulation: Simulation<SimulationType.Refinance> = {
+    simulationType: SimulationType.Refinance,
+    sourcePosition: prevPosition,
+    targetPosition: {
+      positionId: PositionId.createFrom({ id: '1234567890' }),
+      debtAmount: TokenAmount.createFrom({ token: DAI, amount: '56.78' }),
+      collateralAmount: TokenAmount.createFrom({ token: WETH, amount: '105.98' }),
+      pool: targetPool,
+      riskRatio: RiskRatio.createFrom({
+        ratio: Percentage.createFrom({ percentage: 0.5 }),
+        type: RiskRatio.type.LTV,
+      }),
+    },
+    steps: [],
+  }
+
+  const positionsManager: IPositionsManager = {
+    address: Address.ZeroAddressEthereum,
+  }
+
+  let user: User | undefined = undefined
+
+  type BuildOrderType = RPCClientType['orders']['buildOrder']['query']
+  const buildOrder: BuildOrderType = jest.fn(async (params) => {
+    expect(params).toBeDefined()
+    expect(params.positionsManager).toBeDefined()
+    expect(params.user).toBeDefined()
+    expect(params.user).toBe(user)
+
+    expect(params.simulation).toBeDefined()
+    expect(params.simulation).toBe(simulation)
+
+    return {} as Maybe<Order>
+  })
+
+  const rpcClient = {
+    orders: {
+      buildOrder: {
+        query: buildOrder,
+      },
+    },
+  } as unknown as RPCClientType
+
+  const sdkManager = new SDKManager({ rpcClient })
+
+  expect(sdkManager).toBeDefined()
+
+  user = await sdkManager.users.getUser({
+    chainInfo: chainInfo,
+    walletAddress: Address.createFrom({ value: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266' }),
+  })
+
+  const order: Maybe<Order> = await user.newOrder({ simulation, positionsManager })
+
+  if (!order) {
+    fail('Order not generated')
+  }
+}
--- sdk/sdk-client/tests/queries/queries.entrypoint.spec.ts
+++ sdk/sdk-client/tests/queries/queries.entrypoint.spec.ts
@@ -1,9 +1,11 @@
 import getPoolTest from './getPool.subtest'
 import makeSDKTest from './makeSDK.subtest'
+import simulateNewOrder from './newOrder.subtest'
 import simulateRefinanceTest from './simulateRefinance.subtest'
 
 describe('SDK Client', () => {
   it('should create SDK client', makeSDKTest.bind(this))
   it('should use the getPool query', getPoolTest.bind(this))
   it('should use the simulateRefinance query', simulateRefinanceTest.bind(this))
+  it('should use the newOrder query', simulateNewOrder.bind(this))
 })
