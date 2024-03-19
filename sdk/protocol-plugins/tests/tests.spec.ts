77150cadbd7302f9e12b8faf47feb5c488a59325
James Tuckett <james@tuckett.me>
2024-03-19 22:19:56 +0000

    test: increase timeout on protocol plugin tests

--- sdk/protocol-plugins/tests/tests.spec.ts
+++ sdk/protocol-plugins/tests/tests.spec.ts
@@ -25,7 +25,7 @@ async function createProtocolPluginContext(): Promise<IProtocolPluginContext> {
 }
 
 const SECONDS = 1000;
-jest.setTimeout(2 * SECONDS)
+jest.setTimeout(10 * SECONDS)
 
 describe('playground', () => {
   let ctx: IProtocolPluginContext
