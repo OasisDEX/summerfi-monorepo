90e020bebeec4b14ad5b514ef43f8f00cc254f9b
James Tuckett <james@tuckett.me>
2024-03-19 12:43:17 +0000

    refactor: remove commented export

--- sdk/sdk-common/src/common/enums/index.ts
+++ sdk/sdk-common/src/common/enums/index.ts
@@ -1,7 +1,6 @@
 export { AddressType } from './AddressType'
 export { CurrencySymbol } from './CurrencySymbol'
 export { TokenSymbol } from './TokenSymbol'
-// export { ChainId } from './ChainId'
 export {
   ArbitrumChainNames,
   BaseChainNames,
