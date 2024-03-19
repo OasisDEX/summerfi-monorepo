c3cfd2bcecc566992fc0fa5098bf094c56b31cc6
Jakub Swierczek <jakub.swierczek@outlook.com>
2024-03-19 16:32:08 +0100

    chore: [sc-14915] [BE] Small Improvements in the API (#132)
    Story details: https://app.shortcut.com/oazo-apps/story/14915
    
    The package serverless-shared now utilizes the Viem library for address
    handling. The address schema was updated to use Viem's getAddress
    function for transformation. Furthermore, updates were made on parameter
    schemas and reward token handling for better data management in the
    meta-morpho details function.

--- packages/serverless-shared/src/validators.ts
+++ packages/serverless-shared/src/validators.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod'
-
+import { getAddress } from 'viem'
 import { isValidAddress } from './guards'
 import { Address, ChainId, ProtocolId } from './domain-types'
 import { SUPPORTED_CHAIN_IDS, SUPPORTED_PROTOCOL_IDS } from './constants'
 import { isBigInt } from './numbers-helpers'
 
-export const addressSchema = z.custom<Address>((val: unknown) => {
-  return isValidAddress(val)
-}, 'Invalid address format')
+export const addressSchema = z
+  .custom<Address>((val: unknown) => {
+    return isValidAddress(val)
+  }, 'Invalid address format')
+  .transform((a) => getAddress(a))
 
 export const bigIntSchema = z
   .string()
