1c3532e12833b408d28dca433793dfa016d9a602
James Tuckett <james@tuckett.me>
2024-03-20 20:01:36 +0000

    refactor: create IProtocolManager

--- sdk/protocol-manager/src/implementation/ProtocolManager.ts
+++ sdk/protocol-manager/src/implementation/ProtocolManager.ts
@@ -7,31 +7,13 @@ import {
   makerProtocolPlugin,
   sparkProtocolPlugin,
 } from '@summerfi/protocol-plugins'
-import { IPoolId } from '@summerfi/sdk-common/protocols'
+import type { GetPoolIds, ReturnPool, IProtocolManager } from '../interfaces/IProtocolManager'
 import { z } from 'zod'
 
-type GetPoolIds<ProtocolPlugins extends BaseProtocolPlugin<any>[]> = {
-  [K in keyof ProtocolPlugins]: ProtocolPlugins[K] extends BaseProtocolPlugin<infer T> ? T : never
-}[number]
-type UnPackPromise<T> = T extends Promise<infer U> ? U : T
-type MatchProtocol<
-  ProtocolPlugins extends BaseProtocolPlugin<any>[],
-  PoolId extends IPoolId,
-> = ProtocolPlugins extends [infer First, ...infer Rest]
-  ? First extends BaseProtocolPlugin<PoolId>
-    ? First
-    : Rest extends BaseProtocolPlugin<any>[]
-      ? MatchProtocol<Rest, PoolId>
-      : never
-  : never
-type ReturnPool<
-  ProtocolPlugins extends BaseProtocolPlugin<any>[],
-  PoolId extends IPoolId,
-> = UnPackPromise<ReturnType<MatchProtocol<ProtocolPlugins, PoolId>['getPool']>>
 type ExtractPoolIds<P extends ProtocolManager<any>> =
   P extends ProtocolManager<infer T> ? GetPoolIds<T> : never
 
-export class ProtocolManager<ProtocolPlugins extends BaseProtocolPlugin<any>[]> {
+export class ProtocolManager<ProtocolPlugins extends BaseProtocolPlugin<any>[]> implements IProtocolManager<ProtocolPlugins> {
   private plugins: ProtocolPlugins
   private _ctx: IProtocolManagerContext | undefined
 
