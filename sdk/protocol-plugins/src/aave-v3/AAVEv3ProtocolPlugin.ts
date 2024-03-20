6a4e1b98efaa5eb51682c9377e271f3baada7a52
James Tuckett <james@tuckett.me>
2024-03-20 21:50:16 +0000

    refactor: AaveV3 plugin

--- sdk/protocol-plugins/src/aave-v3/AAVEv3ProtocolPlugin.ts
+++ sdk/protocol-plugins/src/aave-v3/AAVEv3ProtocolPlugin.ts
@@ -3,6 +3,7 @@ import {
   AddressValue,
   Percentage,
   TokenAmount,
+  Token,
   TokenSymbol,
   Price,
   CurrencySymbol,
@@ -23,6 +24,9 @@ import {
   filterAssetsListByEMode,
 } from '../implementation/AAVEv3LikeBuilder'
 import { UNCAPPED_SUPPLY, PRECISION_BI } from '../implementation/constants'
+
+type AssetsList = ReturnType<AaveV3ProtocolPlugin['buildAssetsList']>;
+type Asset = Awaited<AssetsList> extends (infer U)[] ? U : never;
 
 export class AaveV3ProtocolPlugin extends BaseProtocolPlugin<AaveV3PoolId> {
   public static protocol: ProtocolName.AAVEv3 = ProtocolName.AAVEv3
@@ -69,142 +73,26 @@ export class AaveV3ProtocolPlugin extends BaseProtocolPlugin<AaveV3PoolId> {
       throw new Error(`Chain ID ${chainId} is not supported`)
     }
 
-    const builder = await new AaveV3LikePluginBuilder(ctx, aaveV3PoolId.protocol.name).init()
-    const reservesAssetsList = await builder
-      .addPrices()
-      .addReservesCaps()
-      .addReservesConfigData()
-      .addReservesData()
-      .addEmodeCategories()
-      .build()
-
-    const filteredAssetsList = filterAssetsListByEMode(reservesAssetsList, emode)
+    const assetsList = await this.buildAssetsList(emode)
 
     // Both USDC & DAI use fixed price oracles that keep both stable at 1 USD
     const poolBaseCurrencyToken = CurrencySymbol.USD
 
-    const collaterals: Record<AddressValue, AaveV3PoolCollateralConfig> = {}
-    for (const asset of filteredAssetsList) {
-      const {
-        token: collateralToken,
-        config: { usageAsCollateralEnabled, ltv, liquidationThreshold, liquidationBonus },
-        caps: { supplyCap },
-        data: { totalAToken },
-      } = asset
+    const collaterals = assetsList.reduce<Record<AddressValue, AaveV3PoolCollateralConfig>>((colls, asset) => {
+          const assetInfo = this.getCollateralAssetInfo(asset, poolBaseCurrencyToken)
+          if (!assetInfo) return colls
+          const { token: collateralToken } = asset
+          colls[collateralToken.address.value] = assetInfo
+          return colls;
+    }, {})
 
-      const LTV_TO_PERCENTAGE_DIVISOR = new BigNumber(100)
-
-      try {
-        collaterals[collateralToken.address.value] = {
-          token: collateralToken,
-          price: Price.createFrom({
-            baseToken: collateralToken,
-            quoteToken: poolBaseCurrencyToken,
-            value: asset.price.toString(),
-          }),
-          priceUSD: Price.createFrom({
-            baseToken: collateralToken,
-            quoteToken: CurrencySymbol.USD,
-            value: asset.price.toString(),
-          }),
-          maxLtv: RiskRatio.createFrom({
-            ratio: Percentage.createFrom({
-              percentage: new BigNumber(ltv.toString()).div(LTV_TO_PERCENTAGE_DIVISOR).toNumber(),
-            }),
-            type: RiskRatio.type.LTV,
-          }),
-          liquidationThreshold: RiskRatio.createFrom({
-            ratio: Percentage.createFrom({
-              percentage: new BigNumber(liquidationThreshold.toString())
-                .div(LTV_TO_PERCENTAGE_DIVISOR)
-                .toNumber(),
-            }),
-            type: RiskRatio.type.LTV,
-          }),
-          tokensLocked: TokenAmount.createFromBaseUnit({
-            token: collateralToken,
-            amount: totalAToken.toString(),
-          }),
-          maxSupply: TokenAmount.createFrom({
-            token: collateralToken,
-            amount: supplyCap === 0n ? UNCAPPED_SUPPLY : supplyCap.toString(),
-          }),
-          liquidationPenalty: Percentage.createFrom({
-            percentage: new BigNumber(liquidationBonus.toString())
-              .div(LTV_TO_PERCENTAGE_DIVISOR)
-              .toNumber(),
-          }),
-          apy: Percentage.createFrom({ percentage: 0 }),
-          usageAsCollateralEnabled,
-        }
-      } catch (e) {
-        console.log('error in collateral loop', e)
-        throw new Error(`error in collateral loop ${e}`)
-      }
-    }
-
-    const debts: Record<AddressValue, AaveV3PoolDebtConfig> = {}
-    for (const asset of filteredAssetsList) {
-      const {
-        token: quoteToken,
-        config: { borrowingEnabled, reserveFactor },
-        caps: { borrowCap },
-        data: { totalVariableDebt, totalStableDebt, variableBorrowRate },
-      } = asset
-      if (quoteToken.symbol === TokenSymbol.WETH) {
-        // WETH can be used as collateral on AaveV3 but not borrowed.
-        continue
-      }
-
-      try {
-        const RESERVE_FACTOR_TO_PERCENTAGE_DIVISOR = 10000n
-        const PRECISION_PRESERVING_OFFSET = 1000000n
-        const RATE_DIVISOR_TO_GET_PERCENTAGE = Number(
-          (PRECISION_PRESERVING_OFFSET - 100n).toString(),
-        )
-
-        const rate =
-          Number(
-            ((variableBorrowRate * PRECISION_PRESERVING_OFFSET) / PRECISION_BI.RAY).toString(),
-          ) / RATE_DIVISOR_TO_GET_PERCENTAGE
-        const totalBorrowed = totalVariableDebt + totalStableDebt
-        debts[quoteToken.address.value] = {
-          token: quoteToken,
-          // TODO: If we further restricted pools we could have token pair prices
-          price: Price.createFrom({
-            baseToken: quoteToken,
-            quoteToken: poolBaseCurrencyToken,
-            value: new BigNumber(asset.price.toString()).toString(),
-          }),
-          priceUSD: Price.createFrom({
-            baseToken: quoteToken,
-            quoteToken: CurrencySymbol.USD,
-            value: new BigNumber(asset.price.toString()).toString(),
-          }),
-          rate: Percentage.createFrom({ percentage: rate }),
-          totalBorrowed: TokenAmount.createFromBaseUnit({
-            token: quoteToken,
-            amount: totalBorrowed.toString(),
-          }),
-          debtCeiling: TokenAmount.createFrom({
-            token: quoteToken,
-            amount: borrowCap === 0n ? UNCAPPED_SUPPLY : borrowCap.toString(),
-          }),
-          debtAvailable: TokenAmount.createFromBaseUnit({
-            token: quoteToken,
-            amount: borrowCap === 0n ? UNCAPPED_SUPPLY : (borrowCap - totalBorrowed).toString(),
-          }),
-          dustLimit: TokenAmount.createFromBaseUnit({ token: quoteToken, amount: '0' }),
-          originationFee: Percentage.createFrom({
-            percentage: Number((reserveFactor / RESERVE_FACTOR_TO_PERCENTAGE_DIVISOR).toString()),
-          }),
-          borrowingEnabled,
-        }
-      } catch (e) {
-        console.log('error in debt loop', e)
-        throw new Error(`error in debt loop ${e}`)
-      }
-    }
+    const debts = assetsList.reduce<Record<AddressValue, AaveV3PoolDebtConfig>>((debts, asset) => {
+      const assetInfo = this.getDebtAssetInfo(asset, poolBaseCurrencyToken)
+      if (!assetInfo) return debts
+      const { token: quoteToken } = asset
+      debts[quoteToken.address.value] = assetInfo
+      return debts;
+    }, {})
 
     return {
       type: PoolType.Lending,
@@ -218,6 +106,140 @@ export class AaveV3ProtocolPlugin extends BaseProtocolPlugin<AaveV3PoolId> {
 
   async getPosition(positionId: string): Promise<Position> {
     throw new Error(`Not implemented ${positionId}`)
+  }
+
+  private async buildAssetsList(emode: bigint) {
+    const builder = await new AaveV3LikePluginBuilder(this.ctx, AaveV3ProtocolPlugin.protocol).init()
+    const list = await builder
+        .addPrices()
+        .addReservesCaps()
+        .addReservesConfigData()
+        .addReservesData()
+        .addEmodeCategories()
+        .build()
+
+    return filterAssetsListByEMode(list, emode)
+  }
+
+  private getCollateralAssetInfo(asset: Asset, poolBaseCurrencyToken: Token | CurrencySymbol): AaveV3PoolCollateralConfig | undefined {
+    const {
+      token: collateralToken,
+      config: { usageAsCollateralEnabled, ltv, liquidationThreshold, liquidationBonus },
+      caps: { supplyCap },
+      data: { totalAToken },
+    } = asset
+
+    const LTV_TO_PERCENTAGE_DIVISOR = new BigNumber(100)
+
+    try {
+      return {
+        token: collateralToken,
+        price: Price.createFrom({
+          baseToken: collateralToken,
+          quoteToken: poolBaseCurrencyToken,
+          value: asset.price.toString(),
+        }),
+        priceUSD: Price.createFrom({
+          baseToken: collateralToken,
+          quoteToken: CurrencySymbol.USD,
+          value: asset.price.toString(),
+        }),
+        maxLtv: RiskRatio.createFrom({
+          ratio: Percentage.createFrom({
+            percentage: new BigNumber(ltv.toString()).div(LTV_TO_PERCENTAGE_DIVISOR).toNumber(),
+          }),
+          type: RiskRatio.type.LTV,
+        }),
+        liquidationThreshold: RiskRatio.createFrom({
+          ratio: Percentage.createFrom({
+            percentage: new BigNumber(liquidationThreshold.toString())
+                .div(LTV_TO_PERCENTAGE_DIVISOR)
+                .toNumber(),
+          }),
+          type: RiskRatio.type.LTV,
+        }),
+        tokensLocked: TokenAmount.createFromBaseUnit({
+          token: collateralToken,
+          amount: totalAToken.toString(),
+        }),
+        maxSupply: TokenAmount.createFrom({
+          token: collateralToken,
+          amount: supplyCap === 0n ? UNCAPPED_SUPPLY : supplyCap.toString(),
+        }),
+        liquidationPenalty: Percentage.createFrom({
+          percentage: new BigNumber(liquidationBonus.toString())
+              .div(LTV_TO_PERCENTAGE_DIVISOR)
+              .toNumber(),
+        }),
+        apy: Percentage.createFrom({ percentage: 0 }),
+        usageAsCollateralEnabled,
+      }
+    } catch (e) {
+      console.log('error in collateral loop', e)
+      throw new Error(`error in collateral loop ${e}`)
+    }
+  }
+
+  private getDebtAssetInfo(asset: Asset, poolBaseCurrencyToken: CurrencySymbol | Token): AaveV3PoolDebtConfig | undefined {
+    const {
+      token: quoteToken,
+      config: { borrowingEnabled, reserveFactor },
+      caps: { borrowCap },
+      data: { totalVariableDebt, totalStableDebt, variableBorrowRate },
+    } = asset
+    if (quoteToken.symbol === TokenSymbol.WETH) {
+      // WETH can be used as collateral on AaveV3 but not borrowed.
+      return;
+    }
+
+    try {
+      const RESERVE_FACTOR_TO_PERCENTAGE_DIVISOR = 10000n
+      const PRECISION_PRESERVING_OFFSET = 1000000n
+      const RATE_DIVISOR_TO_GET_PERCENTAGE = Number(
+          (PRECISION_PRESERVING_OFFSET - 100n).toString(),
+      )
+
+      const rate =
+          Number(
+              ((variableBorrowRate * PRECISION_PRESERVING_OFFSET) / PRECISION_BI.RAY).toString(),
+          ) / RATE_DIVISOR_TO_GET_PERCENTAGE
+      const totalBorrowed = totalVariableDebt + totalStableDebt
+      return {
+        token: quoteToken,
+        // TODO: If we further restricted pools we could have token pair prices
+        price: Price.createFrom({
+          baseToken: quoteToken,
+          quoteToken: poolBaseCurrencyToken,
+          value: new BigNumber(asset.price.toString()).toString(),
+        }),
+        priceUSD: Price.createFrom({
+          baseToken: quoteToken,
+          quoteToken: CurrencySymbol.USD,
+          value: new BigNumber(asset.price.toString()).toString(),
+        }),
+        rate: Percentage.createFrom({ percentage: rate }),
+        totalBorrowed: TokenAmount.createFromBaseUnit({
+          token: quoteToken,
+          amount: totalBorrowed.toString(),
+        }),
+        debtCeiling: TokenAmount.createFrom({
+          token: quoteToken,
+          amount: borrowCap === 0n ? UNCAPPED_SUPPLY : borrowCap.toString(),
+        }),
+        debtAvailable: TokenAmount.createFromBaseUnit({
+          token: quoteToken,
+          amount: borrowCap === 0n ? UNCAPPED_SUPPLY : (borrowCap - totalBorrowed).toString(),
+        }),
+        dustLimit: TokenAmount.createFromBaseUnit({ token: quoteToken, amount: '0' }),
+        originationFee: Percentage.createFrom({
+          percentage: Number((reserveFactor / RESERVE_FACTOR_TO_PERCENTAGE_DIVISOR).toString()),
+        }),
+        borrowingEnabled,
+      }
+    } catch (e) {
+      console.log('error in debt loop', e)
+      throw new Error(`error in debt loop ${e}`)
+    }
   }
 }
 
