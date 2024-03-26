7771ca4f12c3f7f214ebec9480a85aa28aa75b04
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-03-26 19:24:14 +0100

    Added PositionUtils class in the sdk-client (#145)
    Added PositionUtils class in the sdk-client so the front-end can use
    that to calculate and show extra position info in the UI

---
+++ sdk/sdk-client/src/utils/PositionUtils.ts
@@ -0,0 +1,59 @@
+import {
+  Percentage,
+  type Position,
+  type Price,
+  type TokenAmount,
+} from '@summerfi/sdk-common/common'
+import BigNumber from 'bignumber.js'
+
+export class PositionUtils {
+  static getLTV({
+    collateralTokenAmount,
+    debtTokenAmount,
+    collateralPriceInUsd,
+    debtPriceInUsd,
+  }: {
+    collateralTokenAmount: TokenAmount
+    debtTokenAmount: TokenAmount
+    collateralPriceInUsd: Price
+    debtPriceInUsd: Price
+  }): Percentage {
+    // Determine the Collateral Value:
+    const collValue = new BigNumber(collateralTokenAmount.amount).times(collateralPriceInUsd.value)
+    // Determine the Borrowed Value:
+    const debtValue = new BigNumber(debtTokenAmount.amount).times(debtPriceInUsd.value)
+    // If the collateral value is 0, return 0.
+    if (collValue.isZero()) {
+      return Percentage.createFrom({ value: 0 })
+    }
+    // Divide the borrowed value by the collateral value and multiply by 100 to get the percentage LTV.
+    const ltvRatio = debtValue.div(collValue).times(100).toNumber()
+    return Percentage.createFrom({ value: ltvRatio })
+  }
+
+  static getLiquidationPriceInUsd({
+    position,
+    liquidationThreshold,
+    debtPriceInUsd,
+  }: {
+    position: Position
+    // TODO: it is not defined in Position yet, we should use Pool in the future
+    liquidationThreshold: Percentage
+    debtPriceInUsd: Price
+  }): string {
+    // Determine the Collateral Value:
+    const collateralAmount = new BigNumber(position.collateralAmount.amount)
+    // Determine the Borrowed Value:
+    const debtAmount = new BigNumber(position.debtAmount.amount)
+    // If the debt value is 0, return 0.
+    if (debtAmount.isZero()) {
+      return '0'
+    }
+    const debtValue = debtAmount.times(debtPriceInUsd.value)
+    // (loanAmount * loanPrice) / (liquidationThreshold * collateralAmount)
+    const liquidationPrice = debtValue.div(
+      collateralAmount.times(liquidationThreshold.toProportion()),
+    )
+    return liquidationPrice.toString()
+  }
+}
