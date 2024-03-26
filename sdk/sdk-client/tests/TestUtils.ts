4c343149a5e892b8e5885a51d0ca9a0081d6f47f
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-03-26 19:18:13 +0100

    Fixed type in test

--- sdk/sdk-client/tests/TestUtils.ts
+++ sdk/sdk-client/tests/TestUtils.ts
@@ -1,11 +1,14 @@
-import { Address, ChainFamilyMap, Token } from '@summerfi/sdk-common/common'
+import { Address, ChainFamilyMap, Token, AddressType } from '@summerfi/sdk-common/common'
 
 export const chainInfo = ChainFamilyMap.Ethereum.Mainnet
 
 // Tokens
 export const WETH = Token.createFrom({
   chainInfo,
-  address: Address.createFrom({ value: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2' }),
+  address: Address.createFrom({
+    value: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    type: AddressType.Ethereum,
+  }),
   symbol: 'WETH',
   name: 'Wrapped Ether',
   decimals: 18,
@@ -13,7 +16,10 @@ export const WETH = Token.createFrom({
 
 export const DAI = Token.createFrom({
   chainInfo,
-  address: Address.createFrom({ value: '0x6B175474E89094C44Da98b954EedeAC495271d0F' }),
+  address: Address.createFrom({
+    value: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
+    type: AddressType.Ethereum,
+  }),
   symbol: 'DAI',
   name: 'Dai Stablecoin',
   decimals: 18,
