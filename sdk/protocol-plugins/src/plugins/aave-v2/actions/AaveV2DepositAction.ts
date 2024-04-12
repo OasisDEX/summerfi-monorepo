de0643e0f5c6a1eec32a46a63a8d432c39a0f8f6
James Tuckett <james@tuckett.me>
2024-04-12 10:04:22 +0100

    feat: add AaveV2DepositAction

--- sdk/protocol-plugins/src/plugins/aave-v2/actions/AaveV2DepositAction.ts
+++ sdk/protocol-plugins/src/plugins/aave-v2/actions/AaveV2DepositAction.ts
@@ -1,24 +1,31 @@
-import { ActionCall, BaseAction } from '@summerfi/order-planner-common/actions'
+import { ActionCall, BaseAction, InputSlotsMapping } from '@summerfi/protocol-plugins-common'
 import { TokenAmount } from '@summerfi/sdk-common/common'
 
 export class AaveV2DepositAction extends BaseAction {
   public readonly config = {
-    name: 'AaveV2Deposit',
-    version: 0,
-    parametersAbi: '/** INSERT HERE - THE ACTION PARAMETERS **/',
-    storageInputs: [/** INSERT HERE - THE STORAGE INPUTS **/],
-    storageOutputs: [/** INSERT HERE - THE STORAGE OUTPUTS **/],
+    name: 'AaveDeposit',
+    version: 3,
+    parametersAbi: '(address asset, uint256 amount, bool sumAmounts, bool setAsCollateral)',
+    storageInputs: ['amountToDeposit'],
+    storageOutputs: ['depositedAmount'],
   } as const
 
   public encodeCall(
     params: {
-        /** INSERT HERE - THE ACTION PARAMETERS **/
+      depositAmount: TokenAmount
+      sumAmounts: boolean
+      setAsCollateral: boolean
     },
-    paramsMapping?: number[],
+    paramsMapping?: InputSlotsMapping,
   ): ActionCall {
     return this._encodeCall({
       arguments: [
-        /** INSERT HERE - THE ACTION ARGS  **/
+        {
+          asset: params.depositAmount.token.address.value,
+          amount: params.depositAmount.toBaseUnit(),
+          sumAmounts: params.sumAmounts,
+          setAsCollateral: params.setAsCollateral,
+        },
       ],
       mapping: paramsMapping,
     })
