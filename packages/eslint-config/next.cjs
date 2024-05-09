22858d444a89de477f8929ac36441628c2f40258
Marcin Ciarka <marcin@oazoapps.com>
2024-05-09 17:07:35 +0200

    chore: Update build-watch script to disable live reloading in vite

--- packages/eslint-config/next.cjs
+++ packages/eslint-config/next.cjs
@@ -4,12 +4,8 @@ const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'prettier',
-    require.resolve('@vercel/style-guide/eslint/next'),
-    'eslint-config-turbo',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
   globals: {
     React: true,
     JSX: true,
@@ -17,17 +13,6 @@ module.exports = {
   env: {
     node: true,
     browser: true,
-  },
-  plugins: ['only-warn', 'unused-imports'],
-  rules: {
-    'no-unused-vars': 'off',
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-    ],
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/ban-ts-comment': 'off',
   },
   settings: {
     'import/resolver': {
