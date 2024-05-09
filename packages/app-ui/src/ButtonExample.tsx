034354fd884674311634a65383a3908492a0acdf
Marcin Ciarka <marcin@oazoapps.com>
2024-05-09 13:39:42 +0200

    chore: Update package names and scaffolding

--- packages/app-ui/src/ButtonExample.tsx
+++ packages/app-ui/src/ButtonExample.tsx
@@ -1,10 +1,15 @@
 import buttonExampleStyles from './ButtonExample.module.scss'
+import classnames from 'classnames'
 
 export const ButtonExample = ({ children, test }: { children: React.ReactNode; test: string }) => {
   return (
-    <button className={buttonExampleStyles.buttonExampleStyle}>
-      {children}
-      {test}
-    </button>
+    <>
+      <button
+        className={classnames(buttonExampleStyles.buttonExampleStyle, buttonExampleStyles.test)}
+      >
+        {children}
+        {test}
+      </button>
+    </>
   )
 }
