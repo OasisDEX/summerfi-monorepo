804b9048a5318f07b6a08f419dd242a135211130
Roberto Cano <3525807+robercano@users.noreply.github.com>
2024-05-14 15:46:10 +0200

    feat: add global coverage report

---
+++ scripts/coverage-report/GenerateCoverageReport.js
@@ -0,0 +1,17 @@
+const {
+  getAllPathsForPackagesSummaries,
+  readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal,
+  createCoverageReportForVisualRepresentation,
+} = require('./Utils')
+
+// Execution Stages
+// 1. Read all coverage-total.json files
+const packagesSummaryPaths = getAllPathsForPackagesSummaries()
+// 2. Generate consolidated report
+const currCoverageReport =
+  readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(packagesSummaryPaths)
+// 3. Reformat the report for visual representation
+const coverageReportForVisualRepresentation =
+  createCoverageReportForVisualRepresentation(currCoverageReport)
+// 4. Print the report
+console.table(coverageReportForVisualRepresentation)
---
+++ scripts/coverage-report/Utils.js
@@ -0,0 +1,105 @@
+const fs = require('fs')
+const path = require('path')
+
+function getAllPathsForPackagesSummaries() {
+  const getDirectories = (source) =>
+    fs
+      .readdirSync(source, { withFileTypes: true })
+      .filter((dirent) => dirent.isDirectory())
+      .map((dirent) => dirent.name)
+
+  const appsPath = 'sdk'
+  const appsNames = getDirectories(appsPath)
+
+  const appsSummaries = appsNames.reduce((summary, appName) => {
+    return {
+      ...summary,
+      [appName]: path.join(appsPath, appName, 'coverage', 'coverage-summary.json'),
+    }
+  }, {})
+
+  const packagesPath = 'packages'
+  const packageNames = getDirectories(packagesPath)
+
+  const packagesSummaries = packageNames.reduce((summary, packageName) => {
+    return {
+      ...summary,
+      [packageName]: path.join(packagesPath, packageName, 'coverage', 'coverage-summary.json'),
+    }
+  }, {})
+
+  return { ...appsSummaries, ...packagesSummaries }
+}
+
+function readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal(packagesSummaryPaths) {
+  return Object.keys(packagesSummaryPaths).reduce(
+    (summary, packageName) => {
+      const reportPath = packagesSummaryPaths[packageName]
+      if (fs.existsSync(reportPath)) {
+        const report = JSON.parse(fs.readFileSync(reportPath, 'utf8'))
+
+        const { total } = summary
+
+        Object.keys(report.total).forEach((key) => {
+          if (total[key]) {
+            total[key].total += report.total[key].total
+            total[key].covered += report.total[key].covered
+            total[key].skipped += report.total[key].skipped
+            total[key].pct = Number(((total[key].covered / total[key].total) * 100).toFixed(2))
+          } else {
+            total[key] = { ...report.total[key] }
+          }
+        })
+
+        return { [packageName]: report.total, ...summary, total }
+      }
+
+      return summary
+    },
+    { total: {} },
+  )
+}
+
+function createCoverageReportForVisualRepresentation(coverageReport) {
+  const separator = {
+    ['-------------']: {
+      'lines (%)': '-----',
+      'statements (%)': '-----',
+      'functions (%)': '-----',
+      'branches (%)': '-----',
+    },
+  }
+
+  return Object.keys(coverageReport).reduce((report, packageName) => {
+    const { lines, statements, functions, branches } = coverageReport[packageName]
+
+    if (packageName === 'total') {
+      return {
+        ...report,
+        ...separator,
+        ['TOTAL']: {
+          'lines (%)': lines.pct,
+          'statements (%)': statements.pct,
+          'functions (%)': functions.pct,
+          'branches (%)': branches.pct,
+        },
+      }
+    }
+
+    return {
+      ...report,
+      [packageName]: {
+        'lines (%)': lines.pct,
+        'statements (%)': statements.pct,
+        'functions (%)': functions.pct,
+        'branches (%)': branches.pct,
+      },
+    }
+  }, {})
+}
+
+module.exports = {
+  getAllPathsForPackagesSummaries,
+  readSummaryPerPackageAndCreateJoinedSummaryReportWithTotal,
+  createCoverageReportForVisualRepresentation,
+}
