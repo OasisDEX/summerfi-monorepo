1b81d4ea0d9b9cf5992388c34e6f0490f6d2235c
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-15 21:10:09 +0200

    added morpho blue reexport

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -3,4 +3,5 @@
 export { MakerLendingPoolId, MakerPosition } from '@summerfi/protocol-plugins/plugins/maker'
 export { SparkLendingPoolId, SparkPosition } from '@summerfi/protocol-plugins/plugins/spark'
 export { AaveV3LendingPoolId, AaveV3Position } from '@summerfi/protocol-plugins/plugins/aave-v3'
+export { MorphoLendingPoolId, MorphoPosition } from '@summerfi/protocol-plugins/plugins/morphoblue'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
