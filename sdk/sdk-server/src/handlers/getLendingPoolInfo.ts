ba744f4d2dc3dbc186fdf3492990412038505ed2
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-15 20:17:22 +0200

    updated validation in getLendingPoolInfo

--- sdk/sdk-server/src/handlers/getLendingPoolInfo.ts
+++ sdk/sdk-server/src/handlers/getLendingPoolInfo.ts
@@ -1,9 +1,13 @@
+import { LendingPoolIdSchema } from '@summerfi/sdk-common'
 import { publicProcedure } from '../TRPC'
-import { PoolIdSchema } from '@summerfi/sdk-common/protocols'
 import { z } from 'zod'
 
 export const getLendingPoolInfo = publicProcedure
-  .input(z.object({ poolId: PoolIdSchema }))
+  .input(
+    z.object({
+      poolId: LendingPoolIdSchema.catchall(z.any()),
+    }),
+  )
   .query(async (opts) => {
     return await opts.ctx.protocolManager.getLendingPoolInfo(opts.input.poolId)
   })
