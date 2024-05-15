aa963986fa7b86b60ac87e4778608f9bc289ed55
Piotr Konowrocki <piotr@oazoapps.com>
2024-05-15 15:48:08 +0200

    add missing packages

--- packages/app-ui/src/ButtonExample.tsx
+++ packages/app-ui/src/ButtonExample.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from 'react'
-import classnames from 'classnames'
 
 import buttonExampleStyles from '@/ButtonExample.module.scss'
 
 export const ButtonExample = ({ children, text }: { children: ReactNode; text?: string }) => {
   return (
-    <button
-      className={classnames(buttonExampleStyles.buttonExampleStyle, buttonExampleStyles.testets)}
-    >
+    <button className={buttonExampleStyles.buttonExampleStyle}>
       {text}
       {children}
     </button>
