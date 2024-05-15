d2312b150d0af8360e89d9853be09c1da5d71fe4
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-15 10:14:04 +0200

    Added positions reexport

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -1,6 +1,6 @@
 // WORKAROUND: re-exporting protocol-plugins to give FE access to protocol plugins types
 
-export { MakerLendingPoolId } from '@summerfi/protocol-plugins/plugins/maker'
-export { SparkLendingPoolId } from '@summerfi/protocol-plugins/plugins/spark'
-export { AaveV3LendingPoolId } from '@summerfi/protocol-plugins/plugins/aave-v3'
+export { MakerLendingPoolId, MakerPosition } from '@summerfi/protocol-plugins/plugins/maker'
+export { SparkLendingPoolId, SparkPosition } from '@summerfi/protocol-plugins/plugins/spark'
+export { AaveV3LendingPoolId, AaveV3Position } from '@summerfi/protocol-plugins/plugins/aave-v3'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
