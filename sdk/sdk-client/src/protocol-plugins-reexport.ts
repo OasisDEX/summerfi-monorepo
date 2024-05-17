c9efbeadda3a9c0e794495a7dd0169aba22fe026
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-17 14:12:40 +0200

    added reexports

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -1,7 +1,23 @@
 // WORKAROUND: re-exporting protocol-plugins to give FE access to protocol plugins types
 
-export { MakerLendingPoolId, MakerPosition } from '@summerfi/protocol-plugins/plugins/maker'
-export { SparkLendingPoolId, SparkPosition } from '@summerfi/protocol-plugins/plugins/spark'
-export { AaveV3LendingPoolId, AaveV3Position } from '@summerfi/protocol-plugins/plugins/aave-v3'
-export { MorphoLendingPoolId, MorphoPosition } from '@summerfi/protocol-plugins/plugins/morphoblue'
+export {
+  MakerLendingPoolId,
+  MakerPosition,
+  MakerPositionId,
+} from '@summerfi/protocol-plugins/plugins/maker'
+export {
+  SparkLendingPoolId,
+  SparkPosition,
+  SparkPositionId,
+} from '@summerfi/protocol-plugins/plugins/spark'
+export {
+  AaveV3LendingPoolId,
+  AaveV3Position,
+  AaveV3PositionId,
+} from '@summerfi/protocol-plugins/plugins/aave-v3'
+export {
+  MorphoLendingPoolId,
+  MorphoPosition,
+  MorphoPositionId,
+} from '@summerfi/protocol-plugins/plugins/morphoblue'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
