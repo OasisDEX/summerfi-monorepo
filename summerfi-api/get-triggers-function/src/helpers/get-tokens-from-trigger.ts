cbab4fd3fd8d7b5d66bd6f25136753991d3ab291
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-05-20 14:53:47 +0200

    misc

--- summerfi-api/get-triggers-function/src/helpers/get-tokens-from-trigger.ts
+++ summerfi-api/get-triggers-function/src/helpers/get-tokens-from-trigger.ts
@@ -6,22 +6,21 @@ import type { TriggersQuery } from '@summerfi/automation-subgraph'
  * @returns collateral and debt token basic data
  */
 export const getTokensFromTrigger = (trigger: TriggersQuery['triggers'][0]) => {
-  const debtAddressIndex = trigger.decodedDataNames.findIndex((x) => x === 'debtToken')
-  let debtAddress = ''
   let debtToken
+  const debtAddressIndex = trigger.decodedDataNames.findIndex((x) => x === 'debtToken')
   if (debtAddressIndex != -1) {
-    debtAddress = trigger.decodedData[debtAddressIndex]
+    const debtAddress = trigger.decodedData[debtAddressIndex]
     debtToken = trigger.tokens.find((x) => x.address.toLowerCase() === debtAddress.toLowerCase())
   }
 
-  const collateralAddressIndex = trigger.decodedDataNames.findIndex((x) => x === 'collateralToken')
-  let collateralAddress = ''
   let collateralToken
+  const collateralAddressIndex = trigger.decodedDataNames.findIndex((x) => x === 'collateralToken')
   if (collateralAddressIndex != -1) {
-    collateralAddress = trigger.decodedData[collateralAddressIndex]
+    const collateralAddress = trigger.decodedData[collateralAddressIndex]
     collateralToken = trigger.tokens.find(
       (x) => x.address.toLowerCase() === collateralAddress.toLowerCase(),
     )
   }
+
   return { collateralToken, debtToken }
 }
