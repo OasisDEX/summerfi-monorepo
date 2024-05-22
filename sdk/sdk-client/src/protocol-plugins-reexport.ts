237299fe952f8e766aed651f4475432fac05cdbb
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-22 18:04:36 +0200

    Publish npm packages

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -2,24 +2,28 @@
 
 export {
   MakerLendingPoolId,
+  type IMakerLendingPoolId,
   MakerPosition,
   MakerPositionId,
   isMakerLendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/maker'
 export {
   SparkLendingPoolId,
+  type ISparkLendingPoolId,
   SparkPosition,
   SparkPositionId,
   isSparkLendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/spark'
 export {
   AaveV3LendingPoolId,
+  type IAaveV3LendingPoolId,
   AaveV3Position,
   AaveV3PositionId,
   isAaveV3LendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/aave-v3'
 export {
   MorphoLendingPoolId,
+  type IMorphoLendingPoolId,
   MorphoPosition,
   MorphoPositionId,
   isMorphoLendingPoolId,
