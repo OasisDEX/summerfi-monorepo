b04bb0c0b117fe8c9cba3039b22545c005f384bd
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-05-22 15:02:29 +0200

    Added protocol plugins reexports (#283)
    Added protocol plugins reexports

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -4,20 +4,24 @@ export {
   MakerLendingPoolId,
   MakerPosition,
   MakerPositionId,
+  isMakerLendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/maker'
 export {
   SparkLendingPoolId,
   SparkPosition,
   SparkPositionId,
+  isSparkLendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/spark'
 export {
   AaveV3LendingPoolId,
   AaveV3Position,
   AaveV3PositionId,
+  isAaveV3LendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/aave-v3'
 export {
   MorphoLendingPoolId,
   MorphoPosition,
   MorphoPositionId,
+  isMorphoLendingPoolId,
 } from '@summerfi/protocol-plugins/plugins/morphoblue'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
