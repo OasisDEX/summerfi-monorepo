cfb6643c5ed07dbe5fa5ec14eaefcf8b42848fe1
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-05-23 13:19:07 +0200

    Temporary app-tokens package with basic data

---
+++ packages/app-tokens/src/helpers/index.ts
@@ -0,0 +1,41 @@
+import { TokenConfig } from '../types'
+import { tokenConfigs } from '../config'
+
+function keyBy<T>(array: T[], key: keyof T): Record<string | number | symbol, T> {
+  return array.reduce((result, item) => {
+    const keyValue = item[key];
+    result[keyValue as string | number | symbol] = item;
+    return result;
+  }, {} as Record<string | number | symbol, T>);
+}
+
+export const tokens: TokenConfig[] = [...tokenConfigs]
+export const tokensBySymbol = keyBy(tokens, 'symbol')
+
+export const getTokenDisplayName = (token: string): string => {
+  if (token.toLowerCase() === '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2') {
+    return 'MKR'
+  }
+
+  return (token === 'WETH' ? 'ETH' : token?.toUpperCase()).replace(/-/gi, '')
+}
+
+export function getToken(tokenSymbol: string): TokenConfig {
+  if (!tokensBySymbol[tokenSymbol]) {
+    throw new Error(`No meta information for token: ${tokenSymbol}`)
+  }
+  return tokensBySymbol[tokenSymbol]
+}
+
+export const getTokens = (tokenSymbol: string[]): typeof tokens => {
+  if (tokenSymbol instanceof Array) {
+    return tokenSymbol.map(getToken)
+  }
+  throw new Error(`tokenSymbol should be an array, got ${tokenSymbol}`)
+}
+
+export const getTokenGuarded = (
+  tokenSymbol: string,
+): ReturnType<typeof getToken> | undefined => {
+  return Object.keys(tokensBySymbol).includes(tokenSymbol) ? getToken(tokenSymbol) : undefined
+}
---
+++ packages/app-ui/src/components/atoms/Icon/Icon.tsx
@@ -0,0 +1,37 @@
+import React, { FC, JSX } from 'react'
+
+interface IconProps {
+  variant?: 'large' | 'small'
+  role?: 'presentation'
+  focusable?: boolean
+  icon: {
+    path: JSX.Element
+    viewBox?: string
+  }
+}
+
+export const Icon: FC<IconProps> = ({
+  variant = 'small',
+  role = 'presentation',
+  focusable = false,
+  icon,
+}) => {
+  const size = {
+    large: 44,
+    small: 30,
+  }[variant]
+
+  return (
+    <svg
+      viewBox={icon.viewBox ?? '0 0 24 24'}
+      color="inherit"
+      display="inline-block"
+      focusable={focusable}
+      role={role}
+      width={size}
+      height={size}
+    >
+      {icon.path}
+    </svg>
+  )
+}
