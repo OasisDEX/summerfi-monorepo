1912e2bc93030443f59fe2382cc9cd842344e873
halaprix <panicz.klos@gmail.com>
2024-06-13 14:09:32 +0200

    feat: add `ens` to user details in rays

--- packages/rays-db/src/database-types.ts
+++ packages/rays-db/src/database-types.ts
@@ -4,8 +4,8 @@ export type AddressType = 'ETH' | 'SOL'
 
 export type Generated<T> =
   T extends ColumnType<infer S, infer I, infer U>
-    ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>
 
 export type Int8 = ColumnType<string, bigint | number | string, bigint | number | string>
 
@@ -75,6 +75,8 @@ export interface Leaderboard {
   position: Int8 | null
   totalPoints: Numeric | null
   userAddress: string | null
+  details: Json | null
+  ens: string | null
 }
 
 export interface Multiplier {
@@ -135,6 +137,8 @@ export interface UpdatePointsLock {
 export interface UserAddress {
   address: string
   createdAt: Generated<Timestamp>
+  details: Json | null
+  ens: string | null
   id: Generated<number>
   type: Generated<AddressType>
   updatedAt: Generated<Timestamp>
