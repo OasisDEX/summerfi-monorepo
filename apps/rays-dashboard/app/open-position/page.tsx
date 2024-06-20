53ee9e395f5f5c76839299f8d27a096aa2703c7b
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-06-20 20:14:52 +0200

    eh

--- apps/rays-dashboard/app/open-position/page.tsx
+++ apps/rays-dashboard/app/open-position/page.tsx
@@ -32,7 +32,7 @@ export default async function OpenPositionPage() {
       <Text as="p" variant="p1semi" style={{ marginTop: 'var(--space-m)' }}>
         <Link passHref legacyBehavior prefetch={false} href={INTERNAL_LINKS.earn}>
           <ProxyLinkComponent target="_blank">
-            → Explore over 50+ positions with major protocols and collateral types supports
+            Explore over 50+ positions with major protocols and collateral types supports →
           </ProxyLinkComponent>
         </Link>
       </Text>
