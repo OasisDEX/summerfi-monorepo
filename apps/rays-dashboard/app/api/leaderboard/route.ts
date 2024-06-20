9685ab4ba2ed8c92236a35cc08ac969a336e0f0a
Marcin Ciarka <marcin@oazoapps.com>
2024-06-20 13:20:36 +0200

    chore: Update caching for leaderboard and rays routes

--- apps/rays-dashboard/app/api/leaderboard/route.ts
+++ apps/rays-dashboard/app/api/leaderboard/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { fetchLeaderboard } from '@/server-handlers/leaderboard'
 
+// this route is always dynamic, but the underlying
+// fetchLeaderboard call is cached, so we're good
+export const revalidate = 0
+
 export async function GET(req: NextRequest) {
   const response = await fetchLeaderboard(req.nextUrl.search)
 
--- apps/rays-dashboard/app/api/rays/route.ts
+++ apps/rays-dashboard/app/api/rays/route.ts
@@ -2,6 +2,10 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { fetchRays } from '@/server-handlers/rays'
 
+// this route is always dynamic, but the underlying
+// fetchRays call is cached, so we're good
+export const revalidate = 0
+
 export async function GET(req: NextRequest) {
   const response = await fetchRays(req.nextUrl.search)
 
