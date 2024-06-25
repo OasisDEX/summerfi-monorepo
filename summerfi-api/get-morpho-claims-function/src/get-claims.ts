8e32143964d7f826a9c970754e188ac2f1d0356f
piekczyk <sebastian@oazoapps.com>
2024-06-25 16:07:45 +0200

    Update morpho rewards API (#398)
    - updated morpho rewards API since old API was deprecated

--- summerfi-api/get-morpho-claims-function/src/get-claims.ts
+++ summerfi-api/get-morpho-claims-function/src/get-claims.ts
@@ -1,211 +1,97 @@
-import { Address, IRpcConfig, safeParseBigInt } from '@summerfi/serverless-shared'
-import { Logger } from '@aws-lambda-powertools/logger'
+import { safeParseBigInt } from '@summerfi/serverless-shared'
+import {
+  UserRewardsResponse,
+  UserRewardsDistributionResponse,
+  MorphoAggregatedClaims,
+  GetClaimsParams,
+  MorphoClaims,
+  ClaimableMorphoReward,
+} from './types'
 
-type Token = {
-  address: Address
-  decimals: number
-  symbol: string
-  name: string
-  price?: string
-}
-type Args = {
-  address: string
-  token: string
-  amount: string
-  proof: string[]
-}
-/* 
-accrued - all rewards accrued from the beginning of rewards program
-available - available to claim
-claimed - claimed by the user
-claimable - all tokens cliamable by the user - that included claimed amount
+/**
+ * Aggregates the `claimable`, `claimed`, and `accrued` fields of an array of `MorphoAggregatedClaims` objects by `rewardTokenAddress`.
+ *
+ * @param {MorphoAggregatedClaims[]} claims - An array of `MorphoAggregatedClaims` objects to be aggregated.
+ * @returns {MorphoAggregatedClaims[]} - An array of `MorphoAggregatedClaims` objects with aggregated values for `claimable`, `claimed`, and `accrued` fields, grouped by `rewardTokenAddress`.
+ *
+ * @example
+ * const claims: MorphoAggregatedClaims[] = [
+ *   { rewardTokenAddress: '0xToken1', claimable: BigInt(100), claimed: BigInt(50), accrued: BigInt(150) },
+ *   { rewardTokenAddress: '0xToken2', claimable: BigInt(200), claimed: BigInt(100), accrued: BigInt(300) },
+ *   { rewardTokenAddress: '0xToken1', claimable: BigInt(50), claimed: BigInt(25), accrued: BigInt(75) }
+ * ];
+ *
+ * const aggregated = aggregateClaimsByToken(claims);
+ * console.log(aggregated);
  */
-type TokenInfo = {
-  token: Token
-  accrued: string
-  available: string
-  claimable: string
-  claimed: string
-  function_signature: string
-  args?: Args
-}
+const aggregateClaimsByToken = (claims: MorphoAggregatedClaims[]): MorphoAggregatedClaims[] => {
+  const aggregatedClaims: { [key: string]: MorphoAggregatedClaims } = {}
 
-type Reward = {
-  rewards_distributor: string
-  root: string
-  tokens: TokenInfo[]
-}
+  claims.forEach((claim) => {
+    if (!aggregatedClaims[claim.rewardTokenAddress]) {
+      aggregatedClaims[claim.rewardTokenAddress] = {
+        rewardTokenAddress: claim.rewardTokenAddress,
+        claimable: BigInt(0),
+        claimed: BigInt(0),
+        accrued: BigInt(0),
+      }
+    }
 
-type UserRewards = {
-  user: string
-  timestamp: number
-  rewards: Reward[]
-}
+    aggregatedClaims[claim.rewardTokenAddress].claimable += claim.claimable
+    aggregatedClaims[claim.rewardTokenAddress].claimed += claim.claimed
+    aggregatedClaims[claim.rewardTokenAddress].accrued += claim.accrued
+  })
 
-type Proof = Array<`0x${string}`>
-
-export const rpcConfig: IRpcConfig = {
-  skipCache: false,
-  skipMulticall: false,
-  skipGraph: true,
-  stage: 'prod',
-  source: 'summerfi-api',
-}
-
-export interface GetClaimsParams {
-  account: Address
-  logger?: Logger
-}
-
-export interface MorphoReward {
-  urd: Address
-  reward: Token
-  rewardSymbol: string
-  amount: bigint
-}
-export interface ClaimableMorphoReward extends MorphoReward {
-  claimable: bigint
-  proof: Proof
-}
-export interface MorphoAggregatedClaims {
-  rewardToken: {
-    address: Address
-    symbol: string
-    decimals: number
-  }
-  claimable: bigint
-  claimed: bigint
-  accrued: bigint
-  total: bigint
-}
-
-export interface MorphoClaims {
-  claimable: ClaimableMorphoReward[]
-  claimed: MorphoReward[]
-  accrued: MorphoReward[]
-  claimsAggregated: MorphoAggregatedClaims[]
+  return Object.values(aggregatedClaims)
 }
 
 export const getClaims = async ({ account, logger }: GetClaimsParams): Promise<MorphoClaims> => {
   try {
-    const response = await fetch(`https://rewards.morpho.org/v1/users/${account}/rewards`)
-    if (response.status !== 200) {
+    const [responseRewards, responseDistribution] = await Promise.all([
+      fetch(`https://rewards.morpho.org/v1/users/${account}/rewards`),
+      fetch(`https://rewards.morpho.org/v1/users/${account}/distributions`),
+    ])
+
+    // const responseRewards = await fetch(`https://rewards.morpho.org/v1/users/${account}/rewards`)
+    if (responseRewards.status !== 200 || responseDistribution.status !== 200) {
       logger?.warn('Failed to fetch data from morpho rewards endpointa', {
-        error: response.statusText,
+        error: responseRewards.statusText,
       })
     }
-    const data = (await response.json()) as UserRewards
 
-    const claimed: MorphoReward[] = []
+    const userRewardsResponse = (await responseRewards.json()) as UserRewardsResponse
 
-    data.rewards.forEach((reward) => {
-      reward.tokens.forEach((token) => {
-        if (token.claimed !== '0') {
-          claimed.push({
-            urd: reward.rewards_distributor as Address,
-            reward: {
-              address: token.token.address,
-              decimals: token.token.decimals,
-              symbol: token.token.symbol,
-              name: token.token.name,
-            },
-            rewardSymbol: token.token.symbol,
-            amount: safeParseBigInt(token.claimed) ? BigInt(token.claimed) : 0n,
-          })
-        }
-      })
+    const userRewardsDistribution =
+      (await responseDistribution.json()) as UserRewardsDistributionResponse
+
+    const claimable: ClaimableMorphoReward[] = userRewardsDistribution.data.map((item) => {
+      return {
+        rewardTokenAddress: item.asset.address,
+        urd: item.distributor.address,
+        claimable: BigInt(item.claimable),
+        proof: item.proof,
+      }
     })
 
-    const claimable: ClaimableMorphoReward[] = data.rewards.flatMap((reward) => {
-      return reward.tokens
-        .filter((token) => token.available != '0')
-        .map((token) => {
-          return {
-            urd: reward.rewards_distributor as Address,
-            reward: {
-              address: token.token.address,
-              decimals: token.token.decimals,
-              symbol: token.token.symbol,
-              name: token.token.name,
-            },
-            amount: safeParseBigInt(token.available) ? BigInt(token.available) : 0n,
-            claimable: safeParseBigInt(token.available) ? BigInt(token.available) : 0n,
-            rewardSymbol: token.token.symbol,
-            proof: token.args ? (token.args.proof as Proof) : [],
-          }
-        })
-    })
-    const accrued: MorphoReward[] = data.rewards.flatMap((reward) => {
-      return reward.tokens
-        .filter((token) => token.accrued !== '0')
-        .map((token) => {
-          return {
-            urd: reward.rewards_distributor as Address,
-            reward: {
-              address: token.token.address,
-              decimals: token.token.decimals,
-              symbol: token.token.symbol,
-              name: token.token.name,
-            },
-            rewardSymbol: token.token.symbol,
-            amount: safeParseBigInt(token.accrued) ? BigInt(token.accrued) : 0n,
-          }
-        })
-    })
-
-    const allTokens = data.rewards
-      .flatMap((reward) => reward.tokens)
-      .reduce((acc, token) => {
-        const existing = acc.find((t) => t.token.address === token.token.address)
-        if (existing) {
-          return acc
-        }
-        return [...acc, token]
-      }, [] as TokenInfo[])
-
-    const claimsAggregated: MorphoAggregatedClaims[] = allTokens
-      .map((token) => {
-        const claimedForReward = claimed
-          .filter((c) => c.reward.address === token.token.address)
-          .reduce((acc, c) => acc + c.amount, 0n)
-        const claimableForReward = claimable
-          .filter((c) => c.reward.address === token.token.address)
-          .reduce((acc, c) => acc + c.amount, 0n)
-        const accruedForReward = accrued
-          .filter((c) => c.reward.address === token.token.address)
-          .reduce((acc, c) => acc + c.amount, 0n)
-        const total = accruedForReward
+    const claimsAggregated: MorphoAggregatedClaims[] = aggregateClaimsByToken(
+      userRewardsResponse.data.map((item) => {
         return {
-          rewardToken: token.token,
-          claimable: claimableForReward,
-          claimed: claimedForReward,
-          accrued: accruedForReward,
-          total: total,
+          rewardTokenAddress: item.program.asset.address,
+          claimable: safeParseBigInt(item.for_supply?.claimable_now) || 0n,
+          accrued: safeParseBigInt(item.for_supply?.total) || 0n,
+          claimed: safeParseBigInt(item.for_supply?.claimed) || 0n,
         }
-      })
-      .reduce((acc, claim) => {
-        const existing = acc.find((c) => c.rewardToken.address === claim.rewardToken.address)
-        if (existing) {
-          existing.claimable += claim.claimable
-          existing.claimed += claim.claimed
-          existing.accrued += claim.accrued
-          existing.total += claim.total
-          return acc
-        }
-        return [...acc, claim]
-      }, [] as MorphoAggregatedClaims[])
+      }),
+    )
 
     return {
       claimable,
-      claimed,
-      accrued,
       claimsAggregated,
     }
   } catch (error) {
+    console.error('Get morpho reward claims failed', error)
     return {
       claimable: [],
-      claimed: [],
-      accrued: [],
       claimsAggregated: [],
     }
   }
