9cb48d3f71de29a4091c5b46d27fcad7599e2fae
Marcin Ciarka <marcin@oazoapps.com>
2024-07-04 10:01:53 +0200

    chore: Update CalculatorModal to use form element for better accessibility

--- apps/rays-dashboard/components/organisms/CalculatorModal/CalculatorModal.tsx
+++ apps/rays-dashboard/components/organisms/CalculatorModal/CalculatorModal.tsx
@@ -9,15 +9,17 @@ import { AnimatedNumber } from '@/components/molecules/AnimatedNumber/AnimatedNu
 import { ModalButton, ModalButtonProps } from '@/components/molecules/Modal/ModalButton'
 import { formatAsShorthandNumbers } from '@/helpers/formatters'
 
+import calculatorModalStyles from './CalculatorModal.module.scss'
+
 const amountTooHighValue = 9999999999999
 const amounttooHighLocked = 99999999999999 // same as above but with one more 9
 
 const DollarIconElement = () => <IconCurrencyDollar size={20} />
 
 const AmountTooHigh = ({ label }: { label: string }) => (
-  <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+  <div className={calculatorModalStyles.valueBox}>
     <Text as="h5" variant="h5" style={{ color: 'var(--color-primary-30)', marginTop: '8px' }}>
-      [̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]
+      ( ͡~ ͜ʖ ͡°)
     </Text>
     <Text as="p" variant="p3semi" style={{ color: 'var(--color-neutral-80)' }}>
       {label}
@@ -27,7 +29,7 @@ const AmountTooHigh = ({ label }: { label: string }) => (
 
 const CalculatorModalRaysValue = ({ value, label }: { value: number; label: string }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+    <div className={calculatorModalStyles.valueBox}>
       <Text as="h4" variant="h4" style={{ color: 'var(--color-primary-30)' }}>
         {value > 99999 ? (
           <>{formatAsShorthandNumbers(new BigNumber(value), 1)}</>
@@ -78,13 +80,7 @@ const CalculatorModalContent = () => {
       <Text as="h5" variant="h5" style={{ marginTop: '16px' }}>
         $RAYS Calculator
       </Text>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)',
-          padding: 'var(--space-l) var(--space-l)',
-        }}
-      >
+      <div className={calculatorModalStyles.valuesWrapper}>
         {amount > amountTooHighValue ? (
           <>
             <AmountTooHigh label="Base" />
@@ -110,11 +106,7 @@ const CalculatorModalContent = () => {
             parseFloat(e.target.value) > amounttooHighLocked ? amount : parseFloat(e.target.value),
           )
         }
-        style={{
-          padding: 'var(--space-m) var(--space-m) var(--space-m) 50px',
-          width: '100%',
-          borderRadius: 'var(--radius-8)',
-        }}
+        className={calculatorModalStyles.amountInput}
       />
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <Text as="p" variant="p3semi" style={{ margin: '32px 0 8px 0' }}>
