4a2805100a722fe8a03e0153f93e0ffacc80dd4f
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-07-04 15:55:02 +0200

    Fixed small issues

--- sdk/sdk-server/src/earn-protocol-handlers/withdraw.ts
+++ sdk/sdk-server/src/earn-protocol-handlers/withdraw.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 import { publicProcedure } from '../TRPC'
 
-export const withdraw = publicProcedure.input(z.object({})).query(async (opts): Promise<any> => {
+export const withdraw = publicProcedure.input(z.object({})).query(async (opts): Promise<void> => {
+  opts
   throw new Error('Not implemented')
 })
--- summerfi-api/sdk-router-function/src/earn-protocol.ts
+++ summerfi-api/sdk-router-function/src/earn-protocol.ts
@@ -1,7 +1,7 @@
 import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda'
-import { sdkAppRouter, createSDKContext } from '@summerfi/sdk-server'
+import { earnProtocolAppRouter, createEarnProtocolContext } from '@summerfi/sdk-server'
 
 export const handler = awsLambdaRequestHandler({
-  router: sdkAppRouter,
-  createContext: createSDKContext,
+  router: earnProtocolAppRouter,
+  createContext: createEarnProtocolContext,
 })
