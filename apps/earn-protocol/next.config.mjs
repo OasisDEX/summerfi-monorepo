b53e77163b3c77730b8e73e8b2c5fbb847efc8e2
Marcin Ciarka <marcin@oazoapps.com>
2024-07-15 16:35:32 +0200

    Trying again with no standalone mode

--- apps/earn-protocol/next.config.mjs
+++ apps/earn-protocol/next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: false,
   webpack: (config, { isServer }) => {
     if (isServer) {
