17f56861082a4008795f6881384bf834f5e7c455
Marcin Ciarka <marcin@oazoapps.com>
2024-07-15 15:58:57 +0200

    chore: Update next.config.mjs to include 'output: standalone'

--- apps/earn-protocol/next.config.mjs
+++ apps/earn-protocol/next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   webpack: (config, { isServer }) => {
     if (isServer) {
