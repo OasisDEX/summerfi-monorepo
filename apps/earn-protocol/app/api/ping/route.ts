3087e78bb7d3cb47dbc234ba4b5013328dd4f656
Marcin Ciarka <marcin@oazoapps.com>
2024-07-17 15:28:06 +0200

    chore: Update ping route to use async function

--- apps/earn-protocol/app/api/ping/route.ts
+++ apps/earn-protocol/app/api/ping/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 
 export const revalidate = 0
 
-export function GET() {
+// eslint-disable-next-line require-await
+export async function GET() {
   return NextResponse.json({ pong: true, time: Date.now() })
 }
