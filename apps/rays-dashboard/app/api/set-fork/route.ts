d4973a3a5c70da137522826f1a53778ee4206ce9
Marcin Ciarka <marcin@oazoapps.com>
2024-07-17 14:34:17 +0200

    chore: Add API route for setting forks

---
+++ apps/rays-dashboard/app/api/set-fork/route.ts
@@ -0,0 +1,52 @@
+import { cookies } from 'next/headers'
+import { type NextRequest } from 'next/server'
+
+import { NetworkIds } from '@/constants/networks-list'
+
+export type SetForkRequest = {
+  [key in NetworkIds | 'clear']?: string
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = (await request.json()) as SetForkRequest
+
+    // Clear all forks or a specific fork
+    if (typeof body.clear !== 'undefined') {
+      const cookieStore = cookies()
+
+      if (Object.keys(NetworkIds).includes(body.clear)) {
+        const currentForks = JSON.parse(
+          (cookieStore.get('SummerFiForks')?.value as string | undefined) ?? '{}',
+        )
+
+        delete currentForks[body.clear]
+        cookieStore.set('SummerFiForks', JSON.stringify(currentForks))
+
+        return new Response('Fork updated', { status: 200 })
+      }
+
+      cookieStore.delete('SummerFiForks')
+
+      return new Response('Forks cleared', { status: 200 })
+    }
+
+    // Set a new fork
+    if (typeof body === 'object' && Object.keys(NetworkIds).includes(Object.keys(body)[0])) {
+      const cookieStore = cookies()
+
+      const currentForks = JSON.parse(
+        (cookieStore.get('SummerFiForks')?.value as string | undefined) ?? '{}',
+      )
+      const newForks = { ...currentForks, ...body }
+
+      cookieStore.set('SummerFiForks', JSON.stringify(newForks))
+
+      return new Response('Forks set', { status: 200 })
+    }
+
+    return new Response('Invalid body data', { status: 400 })
+  } catch (error) {
+    return new Response('Error setting fork', { status: 400 })
+  }
+}
