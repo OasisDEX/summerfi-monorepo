4d729f8748e66cb94b6fd760373ed26eb021a313
Roberto Cano <3525807+robercano@users.noreply.github.com>
2024-07-30 09:33:05 +0200

    fix: earn protocol app build

--- sdk/sdk-client/src/rpc/SDKMainClient.ts
+++ sdk/sdk-client/src/rpc/SDKMainClient.ts
@@ -3,16 +3,16 @@ import type { SDKAppRouter } from '@summerfi/sdk-server'
 import { createTRPCClient, httpBatchLink, loggerLink } from '@trpc/client'
 
 // Workaround to register all serializable classes for the old web app
-export * from '@summerfi/protocol-plugins'
-export * from '@summerfi/sdk-common/common'
-export * from '@summerfi/sdk-common/exchange'
-export * from '@summerfi/sdk-common/lending-protocols'
-export * from '@summerfi/sdk-common/orders'
-export * from '@summerfi/sdk-common/services'
-export * from '@summerfi/sdk-common/simulation'
-export * from '@summerfi/sdk-common/swap'
-export * from '@summerfi/sdk-common/tokens'
-export * from '@summerfi/sdk-common/user'
+// export * from '@summerfi/protocol-plugins'
+// export * from '@summerfi/sdk-common/common'
+// export * from '@summerfi/sdk-common/exchange'
+// export * from '@summerfi/sdk-common/lending-protocols'
+// export * from '@summerfi/sdk-common/orders'
+// export * from '@summerfi/sdk-common/services'
+// export * from '@summerfi/sdk-common/simulation'
+// export * from '@summerfi/sdk-common/swap'
+// export * from '@summerfi/sdk-common/tokens'
+// export * from '@summerfi/sdk-common/user'
 
 const EnableDeserialize = false
 
