494ea1b32f421ae936342047393e51b99f1cb560
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-08-07 11:44:18 +0200

    Add missing RPC_GATEWAY env in earn protocol

--- apps/earn-protocol/components/organisms/AccountKitFeatures/AccountKitFeatures.tsx
+++ apps/earn-protocol/components/organisms/AccountKitFeatures/AccountKitFeatures.tsx
@@ -19,7 +19,7 @@ const AccountKitFeatures = () => {
   const signer = useSigner()
   const user = useUser()
 
-  // Dummy ToS & TRM logic for now
+  // Dummy TOS & TRM logic for now
   const signMessage: TOSSignMessage = useCallback(
     async (data: string) => {
       return await signer?.signMessage(data)
