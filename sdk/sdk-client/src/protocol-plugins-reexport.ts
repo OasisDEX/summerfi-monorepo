8b382e7f8d0d16ca73f7570c268d7dc383976b78
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-08-07 15:43:16 +0200

    chore: Update protocol-plugins reexport

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -1,31 +1,7 @@
 // WORKAROUND: re-exporting protocol-plugins to give FE access to protocol plugins types
 
-export {
-  AaveV3LendingPoolId,
-  AaveV3LendingPosition,
-  AaveV3LendingPositionId,
-  isAaveV3LendingPoolId,
-  type IAaveV3LendingPoolId,
-} from '@summerfi/protocol-plugins/plugins/aave-v3'
+export * from '@summerfi/protocol-plugins/plugins/aave-v3'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
-export {
-  MakerLendingPoolId,
-  MakerLendingPosition,
-  MakerLendingPositionId,
-  isMakerLendingPoolId,
-  type IMakerLendingPoolId,
-} from '@summerfi/protocol-plugins/plugins/maker'
-export {
-  MorphoLendingPoolId,
-  MorphoLendingPosition,
-  MorphoLendingPositionId,
-  isMorphoLendingPoolId,
-  type IMorphoLendingPoolId,
-} from '@summerfi/protocol-plugins/plugins/morphoblue'
-export {
-  SparkLendingPoolId,
-  SparkLendingPosition,
-  SparkLendingPositionId,
-  isSparkLendingPoolId,
-  type ISparkLendingPoolId,
-} from '@summerfi/protocol-plugins/plugins/spark'
+export * from '@summerfi/protocol-plugins/plugins/maker'
+export * from '@summerfi/protocol-plugins/plugins/morphoblue'
+export * from '@summerfi/protocol-plugins/plugins/spark'
