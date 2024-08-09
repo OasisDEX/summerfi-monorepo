e0a056a2c7a3ef14e3af84888dd52e7d9c821284
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-08-09 10:30:05 +0200

    - Refactored SDK publish actions to remove code redundancy - Added a new publish action for protocol-plugins SDK package

--- sdk/sdk-client/src/protocol-plugins-reexport.ts
+++ sdk/sdk-client/src/protocol-plugins-reexport.ts
@@ -1,7 +1,37 @@
 // WORKAROUND: re-exporting protocol-plugins to give FE access to protocol plugins types
 
-export * from '@summerfi/protocol-plugins/plugins/aave-v3'
+export {
+  AaveV3LendingPoolId,
+  AaveV3LendingPosition,
+  AaveV3LendingPositionId,
+  isAaveV3LendingPoolId,
+  type IAaveV3LendingPoolId,
+  AaveV3Protocol,
+} from '@summerfi/protocol-plugins/plugins/aave-v3'
 export { EmodeType } from '@summerfi/protocol-plugins/plugins/common'
-export * from '@summerfi/protocol-plugins/plugins/maker'
-export * from '@summerfi/protocol-plugins/plugins/morphoblue'
-export * from '@summerfi/protocol-plugins/plugins/spark'
+export {
+  MakerLendingPoolId,
+  MakerLendingPosition,
+  type MakerLendingPositionParameters,
+  MakerLendingPositionId,
+  type MakerLendingPositionIdParameters,
+  isMakerLendingPoolId,
+  type IMakerLendingPoolId,
+  MakerProtocol,
+} from '@summerfi/protocol-plugins/plugins/maker'
+export {
+  MorphoLendingPoolId,
+  MorphoLendingPosition,
+  MorphoLendingPositionId,
+  isMorphoLendingPoolId,
+  type IMorphoLendingPoolId,
+  MorphoProtocol,
+} from '@summerfi/protocol-plugins/plugins/morphoblue'
+export {
+  SparkLendingPoolId,
+  SparkLendingPosition,
+  SparkLendingPositionId,
+  isSparkLendingPoolId,
+  type ISparkLendingPoolId,
+  SparkProtocol,
+} from '@summerfi/protocol-plugins/plugins/spark'
