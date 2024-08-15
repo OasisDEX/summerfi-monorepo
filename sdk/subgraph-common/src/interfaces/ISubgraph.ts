fbc6986e024070de59d4ecd79dceb35adddd6322
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-08-15 08:36:50 +0200

    chore: Update formatting

--- sdk/subgraph-common/src/interfaces/ISubgraph.ts
+++ sdk/subgraph-common/src/interfaces/ISubgraph.ts
@@ -1,8 +1,7 @@
-/** 
-  * @name ISubgraph
-  * @description FILL IN THE DESCRIPTION FOR THE SERVICE HERE interface ISubgraph
-  */
-export interface ISubgraph
-{ 
+/**
+ * @name ISubgraph
+ * @description FILL IN THE DESCRIPTION FOR THE SERVICE HERE interface ISubgraph
+ */
+export interface ISubgraph {
   // TODO: add here the methods that the service will implement
 }
