e8c4c18ceaee4bd039148ec8c0be590e75acf8e6
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-09-27 12:42:27 +0200

    Allow deploy from the hotfix branch

--- sst.config.ts
+++ sst.config.ts
@@ -105,7 +105,7 @@ export const sstConfig: SSTConfig = {
 
     if (_input.stage === 'staging' || _input.stage === 'production') {
       if (_input.stage === 'staging') {
-        if (currentBranch !== 'dev') {
+        if (currentBranch !== 'dev' && !currentBranch.startsWith('hotfix/')) {
           throw new Error('You can only deploy to staging from dev branch')
         }
       }
