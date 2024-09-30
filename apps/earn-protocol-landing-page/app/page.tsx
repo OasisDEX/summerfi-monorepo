a942b55ceccede7a0d773859674faf0bddb32609
Marcin Ciarka <marcin@oazoapps.com>
2024-09-30 10:17:13 +0200

    Initial commit for earn-protocol-landing-page app (#511)
    This pull request introduces a new deployment workflow for the Earn
    Protocol Landing Page application, as well as several updates to the
    application's configuration, layout, and dependencies.
    
    ### Deployment Workflow:
    *
    [`.github/workflows/deploy-earn-protocol-landing-page-staging.yaml`](diffhunk://#diff-5d9102d911ea097bb4ffad7af35eb6421639c679617d305b839e014be1955c77R1-R185):
    Added a new GitHub Actions workflow to deploy the Earn Protocol Landing
    Page application to the staging environment. This workflow includes
    steps for checking for changes, building, and deploying the application.
    
    ### Configuration and Dependencies:
    *
    [`apps/earn-protocol-landing-page/package.json`](diffhunk://#diff-87129ec90d09d7f2810101c0766dd79429fcb4635f11b05e1bb4feaa1adcc735R1-R49):
    Added new dependencies and scripts for building and running the Earn
    Protocol Landing Page application.
    *
    [`apps/earn-protocol-landing-page/.eslintrc.cjs`](diffhunk://#diff-0c23c2d32de0060ac1217d1d98abb33cb7533fe92f35a6c2a60e393837c3040fR1-R14):
    Added ESLint configuration for the Earn Protocol Landing Page
    application.
    *
    [`apps/earn-protocol-landing-page/next.config.mjs`](diffhunk://#diff-d278d74ec07c30471fc725f23397c34fcab41b79bc2922d68dad7e44063d0b87R1-R31):
    Updated Next.js configuration to include internationalization and Prisma
    plugin.
    
    ### Application Layout:
    *
    [`apps/earn-protocol-landing-page/app/layout.tsx`](diffhunk://#diff-05248072a66b5c8c736543077de1570027bf8d9b8eee13453b3e0114957a7901R1-R30):
    Added the root layout component with global styles and
    internationalization support.
    *
    [`apps/earn-protocol-landing-page/components/layout/MasterPage/MasterPage.tsx`](diffhunk://#diff-1e823ce08238aaf78e0de7daf0f1a1433c914e2f71c3ea9b2e6fbabd82715110R1-R34):
    Added the MasterPage component to handle the main layout, including
    navigation and footer.
    *
    [`apps/earn-protocol-landing-page/components/layout/MasterPage/MasterPage.module.scss`](diffhunk://#diff-36aeed4c8b6f25362382589762280c017c16e21e5e8c90dca30ddfde76069cabR1-R19):
    Added styles for the MasterPage component.
    
    ### Additional Changes:
    *
    [`apps/earn-protocol-landing-page/docker/Dockerfile`](diffhunk://#diff-2255df30aaa2b47f8f5778a2ac79ce24a92c8189ef1aea7b42ab995f44d6bc4eR1-R48):
    Added a Dockerfile for building and running the Earn Protocol Landing
    Page application in a container.
    *
    [`apps/earn-protocol-landing-page/helpers/fonts.ts`](diffhunk://#diff-723a9aa1cb91b4ea5f9cbeb77b6c57856a39c9de6e0c9283bf069f6414743127R1-R37):
    Added custom fonts for the application.
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Introduced a CI/CD pipeline for deploying the Earn Protocol
    application to a staging environment.
      - Added a user-friendly "404 Not Found" page.
    - Implemented a root layout component for the Earn Protocol Landing
    Page.
      - Added custom font configurations for enhanced typography.
    
    - **Improvements**
      - Enhanced internationalization support with new configuration files.
      - Introduced a Dockerfile for containerization of the application.
      - Added a new script for building the Earn Protocol landing page.
    
    - **Bug Fixes**
    - Updated `.gitignore` to exclude generated types from version control.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ apps/earn-protocol-landing-page/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div style={{ display: 'flex', gap: '8px', flexDirection: 'column', alignItems: 'center' }}>
+      <div style={{ display: 'flex', gap: '24px', alignItems: 'center', width: '100%' }}>
+        <div style={{ flex: 1 }}>Welcome to the landing page :)</div>
+      </div>
+    </div>
+  )
+}
