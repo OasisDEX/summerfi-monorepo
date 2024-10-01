86426ee4f0ef2aba87672029fffa314287d9f1b2
Marcin Ciarka <marcin@oazoapps.com>
2024-10-01 14:39:55 +0200

    fix workflow

--- apps/earn-protocol/tailwind.config.ts
+++ apps/earn-protocol/tailwind.config.ts
@@ -4,8 +4,8 @@ import { type Config } from 'tailwindcss'
 export default withAccountKitUi({
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
