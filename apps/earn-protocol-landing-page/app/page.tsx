fd709ff5f04baa42ac0c07677e950ae5a9df96ac
Marcin Ciarka <marcin@oazoapps.com>
2024-10-02 11:46:49 +0200

    Refactor welcome message in landing page

--- apps/earn-protocol-landing-page/app/page.tsx
+++ apps/earn-protocol-landing-page/app/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div style={{ display: 'flex', gap: '8px', flexDirection: 'column', alignItems: 'center' }}>
       <div style={{ display: 'flex', gap: '24px', alignItems: 'center', width: '100%' }}>
-        <div style={{ flex: 1 }}>Welcome to the landing page :)</div>
+        <div style={{ flex: 1 }}>Welcome to the landing page! :)</div>
       </div>
     </div>
   )
