e9e9ef112c11a3e96b9e74e71ce8dd653b6a406e
Marcin Ciarka <marcin@oazoapps.com>
2024-10-03 17:53:47 +0200

    Refactor NavigationExplore component and NavigationMenu styles

--- packages/app-earn-ui/src/components/atoms/NavigationItems/NavigationExplore.tsx
+++ packages/app-earn-ui/src/components/atoms/NavigationItems/NavigationExplore.tsx
@@ -1,4 +1,5 @@
 import { type IconNamesList } from '@summerfi/app-types'
+import Link from 'next/link'
 
 import { Icon } from '@/components/atoms/Icon/Icon'
 import { Text } from '@/components/atoms/Text/Text'
@@ -20,25 +21,27 @@ export const NavigationExplore = ({ items }: NavigationExploreProps) => {
   return (
     <div className={navigationExploreStyles.navigationExploreWrapper}>
       {items.map((item) => (
-        <div key={item.id} className={navigationExploreStyles.navigationExploreItem}>
-          <div className={navigationExploreStyles.navigationExploreIconWrapper}>
-            <Icon iconName={item.icon} size={item.iconSize ?? 24} />
+        <Link href={item.url} key={item.id}>
+          <div className={navigationExploreStyles.navigationExploreItem}>
+            <div className={navigationExploreStyles.navigationExploreIconWrapper}>
+              <Icon iconName={item.icon} size={item.iconSize ?? 24} />
+            </div>
+            <div>
+              <Text as="p" variant="p3semi">
+                {item.title}
+              </Text>
+              <Text
+                as="p"
+                variant="p3"
+                style={{
+                  color: 'var(--color-text-secondary)',
+                }}
+              >
+                {item.description}
+              </Text>
+            </div>
           </div>
-          <div>
-            <Text as="p" variant="p3semi">
-              {item.title}
-            </Text>
-            <Text
-              as="p"
-              variant="p3"
-              style={{
-                color: 'var(--color-text-secondary)',
-              }}
-            >
-              {item.description}
-            </Text>
-          </div>
-        </div>
+        </Link>
       ))}
     </div>
   )
--- packages/app-earn-ui/src/components/molecules/Carousel/Carousel.tsx
+++ packages/app-earn-ui/src/components/molecules/Carousel/Carousel.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useRef, useState } from 'react'
 
 import { Icon } from '@/components/atoms/Icon/Icon'
