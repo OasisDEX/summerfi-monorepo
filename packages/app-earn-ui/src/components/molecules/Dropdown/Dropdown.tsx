d908e07392184021c77e0387f827db93847c0419
Marcin Ciarka <marcin@oazoapps.com>
2024-10-04 17:53:57 +0200

    Refactor import statements in StrategyGrid and Dropdown components

--- packages/app-earn-ui/src/components/molecules/Dropdown/Dropdown.tsx
+++ packages/app-earn-ui/src/components/molecules/Dropdown/Dropdown.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { type DropdownOption } from '@summerfi/app-types'
 
 import { Icon } from '@/components/atoms/Icon/Icon'
