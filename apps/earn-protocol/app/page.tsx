66557bb15770185eaa0246ec60a960845ff21054
Marcin Ciarka <marcin@oazoapps.com>
2024-10-09 20:07:43 +0200

    redeployment...

--- apps/earn-protocol/app/page.tsx
+++ apps/earn-protocol/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div style={{ display: 'flex', gap: '8px', flexDirection: 'column', alignItems: 'center' }}>
-      Hello 🙌🙌
+      Hello 🙌
     </div>
   )
 }
