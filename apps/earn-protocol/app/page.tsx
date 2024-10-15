8776631af77838ec74a9e8663635973fb98d8ff1
Marcin Ciarka <marcin@oazoapps.com>
2024-10-15 17:42:32 +0200

    single app trigger change workflow buzzword

--- apps/earn-protocol/app/page.tsx
+++ apps/earn-protocol/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
+
 export default function HomePage() {
   return (
     <div style={{ display: 'flex', gap: '8px', flexDirection: 'column', alignItems: 'center' }}>
-      Hello 🙌
+      Hello 🙌 If youre seeing this page, it means that the app is running correctly. Have a great
+      day, I love you ❤️ BTW you are probably looking for <Link href="/earn">Earn page</Link>
     </div>
   )
 }
