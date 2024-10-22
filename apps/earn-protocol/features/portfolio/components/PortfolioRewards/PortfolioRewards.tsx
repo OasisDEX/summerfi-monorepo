89aa7e0c0867125e2985e411534253733cb520d9
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-10-22 12:03:35 +0200

    portfolio rewards page init

--- apps/earn-protocol/features/portfolio/components/PortfolioRewards/PortfolioRewards.tsx
+++ apps/earn-protocol/features/portfolio/components/PortfolioRewards/PortfolioRewards.tsx
@@ -1,17 +1,15 @@
-import { Card, Text } from '@summerfi/app-earn-ui'
+import { PortfolioRewardsBoost } from '@/features/portfolio/components/PortfolioRewardsBoost/PortfolioRewardsBoost'
+import { PortfolioRewardsMore } from '@/features/portfolio/components/PortfolioRewardsMore/PortfolioRewardsMore'
+import { PortfolioRewardsWhat } from '@/features/portfolio/components/PortfolioRewardsWhat/PortfolioRewardsWhat'
 
 import classNames from './PortfolioRewards.module.scss'
 
 export const PortfolioRewards = () => {
   return (
-    <Card className={classNames.wrapper}>
-      <Text as="h5" variant="h5" className={classNames.header}>
-        What are $SUMR?
-      </Text>
-      <Text as="p" variant="p2" className={classNames.description}>
-        The Summer Earn Protocol is a permissionless passive lending product, which sets out to
-        offer effortless and secure optimised yield, while diversifying risk.
-      </Text>
-    </Card>
+    <div className={classNames.wrapper}>
+      <PortfolioRewardsWhat />
+      <PortfolioRewardsBoost />
+      <PortfolioRewardsMore />
+    </div>
   )
 }
