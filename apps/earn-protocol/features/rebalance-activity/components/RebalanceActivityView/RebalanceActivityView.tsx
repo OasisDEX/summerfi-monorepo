0aa170f600699150988910763129f787aa322f4d
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-10-30 16:09:52 +0100

    infinite scroll

--- apps/earn-protocol/features/rebalance-activity/components/RebalanceActivityView/RebalanceActivityView.tsx
+++ apps/earn-protocol/features/rebalance-activity/components/RebalanceActivityView/RebalanceActivityView.tsx
@@ -1,6 +1,6 @@
 'use client'
-
 import { type FC, useMemo, useState } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
 import {
   GenericMultiselect,
   getTwitterShareUrl,
@@ -12,8 +12,12 @@ import {
   useQueryParams,
 } from '@summerfi/app-earn-ui'
 import { type SDKRebalancesType, type SDKVaultsListType } from '@summerfi/app-types'
-import { formatFiatBalance, formatShorthandNumber } from '@summerfi/app-utils'
 
+import {
+  getRebalanceActivityHeadingCards,
+  rebalanceActivityHeading,
+} from '@/features/rebalance-activity/components/RebalanceActivityView/cards'
+import { rebalanceActivityTableCarouselData } from '@/features/rebalance-activity/components/RebalanceActivityView/carousel'
 import { rebalancingActivityColumns } from '@/features/rebalance-activity/table/columns'
 import { rebalanceActivityFilter } from '@/features/rebalance-activity/table/filters/filters'
 import { mapMultiselectOptions } from '@/features/rebalance-activity/table/filters/mappers'
@@ -21,41 +25,13 @@ import { rebalancingActivityMapper } from '@/features/rebalance-activity/table/m
 
 import classNames from './RebalanceActivityView.module.scss'
 
-const carouselData = [
-  {
-    title: 'Reduced costs',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus massa ac sapien eleifend.',
-    link: {
-      label: 'Add to position',
-      href: '/',
-    },
-  },
-  {
-    title: 'Optimized Yield',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus massa ac sapien eleifend.',
-    link: {
-      label: 'Add to position',
-      href: '/',
-    },
-  },
-  {
-    title: 'Saved Time',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus massa ac sapien eleifend.',
-    link: {
-      label: 'Add to position',
-      href: '/',
-    },
-  },
-]
-
 interface RebalanceActivityViewProps {
   vaultsList: SDKVaultsListType
   rebalancesList: SDKRebalancesType
   searchParams?: { [key: string]: string[] }
 }
+
+const initialRows = 6
 
 export const RebalanceActivityView: FC<RebalanceActivityViewProps> = ({
   vaultsList,
@@ -69,14 +45,34 @@ export const RebalanceActivityView: FC<RebalanceActivityViewProps> = ({
   const [sortConfig, setSortConfig] = useState<TableSortedColumn<string>>()
   const currentUrl = useCurrentUrl()
 
+  const [current, setCurrent] = useState(initialRows)
+
+  const [currentlyLoadedList, setCurrentlyLoadedList] = useState(
+    rebalancesList.slice(0, initialRows),
+  )
+
   const { strategiesOptions, tokensOptions, protocolsOptions } = useMemo(
     () => mapMultiselectOptions(vaultsList),
     [vaultsList],
   )
 
+  const handleMoreItems = () => {
+    setCurrentlyLoadedList((prev) => [
+      ...prev,
+      ...rebalancesList.slice(current, current + initialRows),
+    ])
+    setCurrent(current + initialRows)
+  }
+
   const filteredList = useMemo(
-    () => rebalanceActivityFilter({ rebalancesList, strategyFilter, tokenFilter, protocolFilter }),
-    [rebalancesList, strategyFilter, tokenFilter, protocolFilter],
+    () =>
+      rebalanceActivityFilter({
+        rebalancesList: currentlyLoadedList,
+        strategyFilter,
+        tokenFilter,
+        protocolFilter,
+      }),
+    [currentlyLoadedList, strategyFilter, tokenFilter, protocolFilter],
   )
 
   const rows = useMemo(
@@ -84,37 +80,55 @@ export const RebalanceActivityView: FC<RebalanceActivityViewProps> = ({
     [filteredList, sortConfig],
   )
 
+  const genericMultiSelectFilters = [
+    {
+      options: strategiesOptions,
+      label: 'Strategies',
+      onChange: (strategies: string[]) => {
+        setQueryParams({ strategies })
+        setStrategyFilter(strategies)
+      },
+      initialValues: strategyFilter,
+    },
+    {
+      options: tokensOptions,
+      label: 'Tokens',
+      onChange: (tokens: string[]) => {
+        setQueryParams({ tokens })
+        setTokenFilter(tokens)
+      },
+      initialValues: tokenFilter,
+    },
+    {
+      options: protocolsOptions,
+      label: 'Protocols',
+      onChange: (protocols: string[]) => {
+        setQueryParams({ protocols })
+        setProtocolFilter(protocols)
+      },
+      initialValues: protocolFilter,
+    },
+  ]
+
+  const totalItems = rebalancesList.length
+
   // used 3 min as average rebalance action
-  const savedTimeInHours = (rows.length * 3) / 60
+  const savedTimeInHours = useMemo(() => (totalItems * 3) / 60, [totalItems])
 
   // used 0.5$ per each rebalance action
-  const savedGasCost = rows.length * 0.5
+  const savedGasCost = useMemo(() => totalItems * 0.5, [totalItems])
+
+  const cards = useMemo(
+    () => getRebalanceActivityHeadingCards({ totalItems, savedGasCost, savedTimeInHours }),
+    [totalItems, savedGasCost, savedTimeInHours],
+  )
 
   return (
     <div className={classNames.wrapper}>
       <HeadingWithCards
-        title="Lazy Summer Global Rebalance Activity"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Nullam quis risus eget."
-        cards={[
-          {
-            title: 'Rebalance actions',
-            value: formatShorthandNumber(rows.length, { precision: 0 }),
-            description:
-              'Justo enim sollicitudin suspendisse lectus tellus tortor magna. Velit id nulla tempor arcu quis condimentum parturient.',
-          },
-          {
-            title: 'User saved time',
-            value: `${formatShorthandNumber(savedTimeInHours, { precision: 1 })} hours`,
-            description:
-              'Justo enim sollicitudin suspendisse lectus tellus tortor magna. Velit id nulla tempor arcu quis condimentum parturient.',
-          },
-          {
-            title: 'Gas cost savings',
-            value: `$${formatFiatBalance(savedGasCost)}`,
-            description:
-              'Justo enim sollicitudin suspendisse lectus tellus tortor magna. Velit id nulla tempor arcu quis condimentum parturient.',
-          },
-        ]}
+        title={rebalanceActivityHeading.title}
+        description={rebalanceActivityHeading.description}
+        cards={cards}
         social={{
           linkToCopy: currentUrl,
           linkToShare: getTwitterShareUrl({
@@ -124,43 +138,27 @@ export const RebalanceActivityView: FC<RebalanceActivityViewProps> = ({
         }}
       />
       <div className={classNames.filtersWrapper}>
-        <GenericMultiselect
-          options={strategiesOptions}
-          label="Strategies"
-          onChange={(strategies) => {
-            setQueryParams({ strategies })
-            setStrategyFilter(strategies)
-          }}
-          initialValues={strategyFilter}
-        />
-        <GenericMultiselect
-          options={tokensOptions}
-          label="Tokens"
-          onChange={(tokens) => {
-            setQueryParams({ tokens })
-            setTokenFilter(tokens)
-          }}
-          initialValues={tokenFilter}
-        />
-        <GenericMultiselect
-          options={protocolsOptions}
-          label="Protocols"
-          onChange={(protocols) => {
-            setQueryParams({ protocols })
-            setProtocolFilter(protocols)
-          }}
-          initialValues={protocolFilter}
-        />
+        {genericMultiSelectFilters.map((filter) => (
+          <GenericMultiselect
+            key={filter.label}
+            options={filter.options}
+            label={filter.label}
+            onChange={filter.onChange}
+            initialValues={filter.initialValues}
+          />
+        ))}
       </div>
-      <Table
-        rows={rows}
-        columns={rebalancingActivityColumns}
-        customRow={{
-          idx: 3,
-          content: <TableCarousel carouselData={carouselData} />,
-        }}
-        handleSort={(_sortConfig) => setSortConfig(_sortConfig)}
-      />
+      <InfiniteScroll loadMore={handleMoreItems} hasMore={totalItems > currentlyLoadedList.length}>
+        <Table
+          rows={rows}
+          columns={rebalancingActivityColumns}
+          customRow={{
+            idx: 3,
+            content: <TableCarousel carouselData={rebalanceActivityTableCarouselData} />,
+          }}
+          handleSort={(_sortConfig) => setSortConfig(_sortConfig)}
+        />
+      </InfiniteScroll>
     </div>
   )
 }
