eeeb59a4f38e4dec0a6c0ab3853ae4918387a443
Marcin Ciarka <marcin@oazoapps.com>
2024-11-06 10:46:01 +0100

    Simplify homepage message by removing redundant text

--- apps/earn-protocol/app/page.tsx
+++ apps/earn-protocol/app/page.tsx
@@ -15,9 +15,7 @@ export default function HomePage() {
       }}
     >
       <Text as="p" variant="p2">
-        Hello 🙌 If youre seeing this page, it means that the app is running correctly.
-        <br />
-        Have a great day, I love you ❤️❤️
+        I love you ❤️❤️
         <br /> <br />
         BTW you are probably looking for the{' '}
         <Link href="/earn" style={{ display: 'inline', color: 'var(--color-text-link)' }}>
