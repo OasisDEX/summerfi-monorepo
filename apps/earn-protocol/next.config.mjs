37726cd09268c1517665455963abecba704e5ae4
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-11-29 08:48:28 +0100

    Safe global config

--- apps/earn-protocol/next.config.mjs
+++ apps/earn-protocol/next.config.mjs
@@ -22,6 +22,31 @@ const nextConfig = {
         );
         `,
   },
+  headers: () => {
+    return [
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors "self" https://app.safe.global;',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withNextIntl(nextConfig)
