c9782897e9e5e1b445567f656a48dee69929450c
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2024-12-12 22:03:27 +0100

    Remove references to ONE_INCH_ALLOWED_SWAP_PROTOCOLS from configuration provider tests

--- sdk/configuration-provider/tests/configuration-provider.spec.ts
+++ sdk/configuration-provider/tests/configuration-provider.spec.ts
@@ -7,15 +7,11 @@ describe('Configuration Provider', () => {
     const apiKey = configProvider.getConfigurationItem({ name: 'ONE_INCH_API_KEY' })
     const apiVersion = configProvider.getConfigurationItem({ name: 'ONE_INCH_API_VERSION' })
     const apiUrl = configProvider.getConfigurationItem({ name: 'ONE_INCH_API_URL' })
-    const allowedSwapProtocols = configProvider.getConfigurationItem({
-      name: 'ONE_INCH_ALLOWED_SWAP_PROTOCOLS',
-    })
     const swapChainIds = configProvider.getConfigurationItem({ name: 'ONE_INCH_SWAP_CHAIN_IDS' })
 
     expect(apiKey).toBeDefined()
     expect(apiVersion).toBeDefined()
     expect(apiUrl).toBeDefined()
-    expect(allowedSwapProtocols).toBeDefined()
     expect(swapChainIds).toBeDefined()
   })
 })
