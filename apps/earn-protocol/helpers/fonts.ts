5517e7e9cb04ae5e9ae321fb3da0f80062407346
sebastianPiekarczyk <sebastian@oazoapps.com>
2024-12-16 09:26:59 +0100

    Refactor font loading to use a single variable font file.

--- apps/earn-protocol/helpers/fonts.ts
+++ apps/earn-protocol/helpers/fonts.ts
@@ -3,16 +3,7 @@ import localFont from 'next/font/local'
 export const fontInter = localFont({
   src: [
     {
-      path: '../public/fonts/inter-bold.woff2',
-      weight: '700',
-    },
-    {
-      path: '../public/fonts/inter-semi-bold.woff2',
-      weight: '600',
-    },
-    {
-      path: '../public/fonts/inter-regular.woff2',
-      weight: '400',
+      path: '../public/fonts/inter-variable.ttf',
     },
   ],
   display: 'block',
