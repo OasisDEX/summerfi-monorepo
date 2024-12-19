29fbe693c4b526b38bedee145e3f359938e0276a
piekczyk <sebastian@oazoapps.com>
2024-12-19 10:42:55 +0100

    Claim acceptance terms scroll tracking (#623)
    ## Description
    [Provide a brief description of the changes in this PR]
    
    ## Changes
    - [List the main changes made in bullet points]
    
    ## Benefits
    1. [List the benefits of these changes]
    2.
    3.
    
    ## Testing
    [Describe how these changes were tested or how they can be tested]
    
    ## Next steps
    - [List any follow-up actions or considerations]
    
    ## Additional Notes
    [Any additional information, context, or notes for reviewers]
    
    Please review and provide any feedback or suggestions for improvement.
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Enhanced interactivity in the Claim Delegate Acceptance Step with
    dynamic content display and improved user engagement.
      - Added new terms and conditions for the claim delegation process.
    
    - **Bug Fixes**
    - Improved state management for user acceptance of terms, ensuring
    consistent user experience during interactions.
    
    - **Chores**
    - Removed outdated content structure and replaced it with a more
    flexible terms management system.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- apps/earn-protocol/features/claim-and-delegate/components/ClaimDelegateAcceptanceStep/ClaimDelegateAcceptanceStep.tsx
+++ apps/earn-protocol/features/claim-and-delegate/components/ClaimDelegateAcceptanceStep/ClaimDelegateAcceptanceStep.tsx
@@ -6,13 +6,14 @@ import { TOSStatus } from '@summerfi/app-types'
 import Link from 'next/link'
 
 import { accountType } from '@/account-kit/config'
-import { claimDelegateAcceptanceParagraphs } from '@/features/claim-and-delegate/components/ClaimDelegateAcceptanceStep/config'
+import { claimDelegateTerms } from '@/features/claim-and-delegate/components/ClaimDelegateAcceptanceStep/terms'
 import {
   type ClaimDelegateReducerAction,
   type ClaimDelegateState,
   ClaimDelegateSteps,
 } from '@/features/claim-and-delegate/types'
 import { useUserWallet } from '@/hooks/use-user-wallet'
+import { useVisibleParagraph } from '@/hooks/useVisibleParagraph'
 
 import classNames from './ClaimDelegateAcceptanceStep.module.scss'
 
@@ -32,6 +33,7 @@ export const ClaimDelegateAcceptanceStep: FC<ClaimDelegateAcceptanceStepProps> =
     client,
   })
   const signer = useSigner()
+  const { activeParagraph, paragraphRefs } = useVisibleParagraph()
 
   const signMessage: TOSSignMessage = useCallback(
     async (data: string) => {
@@ -89,77 +91,45 @@ export const ClaimDelegateAcceptanceStep: FC<ClaimDelegateAcceptanceStepProps> =
   return (
     <div className={classNames.claimDelegateAcceptanceStepWrapper}>
       <ol>
-        {claimDelegateAcceptanceParagraphs.map((item, idx) => (
-          <li key={idx}>
-            <Text as="p" variant="p3" style={{ color: 'var(--earn-protocol-secondary-60)' }}>
+        {claimDelegateTerms.map((item, idx) => (
+          <li
+            key={idx}
+            onClick={() => {
+              paragraphRefs.current[idx]?.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+              })
+            }}
+          >
+            <Text
+              as="p"
+              variant="p3"
+              style={{
+                color: activeParagraph === idx ? 'white' : 'var(--earn-protocol-secondary-60)',
+                cursor: 'pointer',
+              }}
+            >
               {idx + 1}. {item.label}
             </Text>
           </li>
         ))}
       </ol>
+
       <div className={classNames.mainContentWrapper}>
         <Card className={classNames.cardWrapper}>
           <div className={classNames.cardContentWrapper}>
-            <Text as="p" variant="p3">
-              Volutpat nunc mauris quis vitae metus a viverra nulla turpis. Quis hendrerit aenean eu
-              id viverra mattis facilisi diam ullamcorper. Diam faucibus sed mauris turpis urna
-              tortor dui. Volutpat odio ullamcorper nisi sodales volutpat in tortor metus egestas.
-              Cursus sem sit velit tincidunt mus fames blandit nascetur rhoncus. Donec massa id
-              suspendisse tortor felis accumsan adipiscing. Sodales in aliquet at sed. Adipiscing
-              sapien quisque at adipiscing. Blandit vivamus vitae mi viverra mattis lectus enim
-              donec libero. Commodo molestie amet viverra enim in luctus vel. Arcu id aliquam
-              molestie ac. Eu aliquet consequat non integer auctor nulla. Scelerisque mi nec
-              porttitor mi. Quis in vitae mollis tincidunt congue. Tincidunt ultrices id amet
-              volutpat eget fames. Odio eleifend duis amet ac vivamus facilisis. Ullamcorper
-              convallis nisi porta varius ullamcorper at enim. Aenean orci sagittis parturient ac
-              amet. Donec eget in sagittis vestibulum congue facilisis. Sit tincidunt morbi fusce in
-              molestie sed amet vitae. Varius gravida id lacinia habitasse senectus. Orci tempus nam
-              cras orci egestas mattis viverra urna. Metus morbi lorem maecenas enim. Sagittis sed
-              quis praesent cursus felis cursus. Fringilla tristique non ut enim nunc ac nulla.
-              Posuere quis ipsum suspendisse turpis facilisis mus elementum a. Leo faucibus pretium
-              facilisis malesuada leo ac bibendum et. Nunc leo vitae pretium diam egestas enim
-              justo. Nulla ullamcorper tortor mattis cras lobortis ultricies. Feugiat aliquam quis
-              cras ac sed donec. Adipiscing at egestas tempor sit luctus facilisi. Erat euismod
-              cursus fermentum elementum egestas felis feugiat. Felis urna tincidunt ultrices
-              dignissim. Arcu sit enim et mattis ac. Quam ultrices id suscipit at ac nibh in. Cursus
-              integer hendrerit amet diam viverra et malesuada. At aliquet ut pellentesque vulputate
-              nec. Metus nunc magna tincidunt nisl quis. Purus eget pellentesque amet mattis.
-              Egestas a elementum dignissim quam ultrices sed. Sem neque commodo vivamus ut praesent
-              faucibus id tristique eu. Mauris curabitur maecenas neque et. Suspendisse donec porta
-              nibh erat maecenas. Fringilla egestas volutpat rhoncus laoreet. Pellentesque interdum
-              tristique scelerisque lacus in. Eros feugiat gravida sem integer velit eu varius
-              tellus justo. Malesuada auctor morbi dictumst urna tincidunt. Urna vestibulum ornare
-              neque gravida molestie et lacus. Nisi morbi scelerisque s. Volutpat nunc mauris quis
-              vitae metus a viverra nulla turpis. Quis hendrerit aenean eu id viverra mattis
-              facilisi diam ullamcorper. Diam faucibus sed mauris turpis urna tortor dui. Volutpat
-              odio ullamcorper nisi sodales volutpat in tortor metus egestas. Cursus sem sit velit
-              tincidunt mus fames blandit nascetur rhoncus. Donec massa id suspendisse tortor felis
-              accumsan adipiscing. Sodales in aliquet at sed. Adipiscing sapien quisque at
-              adipiscing. Blandit vivamus vitae mi viverra mattis lectus enim donec libero. Commodo
-              molestie amet viverra enim in luctus vel. Arcu id aliquam molestie ac. Eu aliquet
-              consequat non integer auctor nulla. Scelerisque mi nec porttitor mi. Quis in vitae
-              mollis tincidunt congue. Tincidunt ultrices id amet volutpat eget fames. Odio eleifend
-              duis amet ac vivamus facilisis. Ullamcorper convallis nisi porta varius ullamcorper at
-              enim. Aenean orci sagittis parturient ac amet. Donec eget in sagittis vestibulum
-              congue facilisis. Sit tincidunt morbi fusce in molestie sed amet vitae. Varius gravida
-              id lacinia habitasse senectus. Orci tempus nam cras orci egestas mattis viverra urna.
-              Metus morbi lorem maecenas enim. Sagittis sed quis praesent cursus felis cursus.
-              Fringilla tristique non ut enim nunc ac nulla. Posuere quis ipsum suspendisse turpis
-              facilisis mus elementum a. Leo faucibus pretium facilisis malesuada leo ac bibendum
-              et. Nunc leo vitae pretium diam egestas enim justo. Nulla ullamcorper tortor mattis
-              cras lobortis ultricies. Feugiat aliquam quis cras ac sed donec. Adipiscing at egestas
-              tempor sit luctus facilisi. Erat euismod cursus fermentum elementum egestas felis
-              feugiat. Felis urna tincidunt ultrices dignissim. Arcu sit enim et mattis ac. Quam
-              ultrices id suscipit at ac nibh in. Cursus integer hendrerit amet diam viverra et
-              malesuada. At aliquet ut pellentesque vulputate nec. Metus nunc magna tincidunt nisl
-              quis. Purus eget pellentesque amet mattis. Egestas a elementum dignissim quam ultrices
-              sed. Sem neque commodo vivamus ut praesent faucibus id tristique eu. Mauris curabitur
-              maecenas neque et. Suspendisse donec porta nibh erat maecenas. Fringilla egestas
-              volutpat rhoncus laoreet. Pellentesque interdum tristique scelerisque lacus in. Eros
-              feugiat gravida sem integer velit eu varius tellus justo. Malesuada auctor morbi
-              dictumst urna tincidunt. Urna vestibulum ornare neque gravida molestie et lacus. Nisi
-              morbi scelerisque s.
-            </Text>
+            {claimDelegateTerms.map((item, index) => (
+              <Text
+                as="p"
+                variant="p3"
+                key={`content-${index}`}
+                ref={(element: HTMLParagraphElement | null) => {
+                  paragraphRefs.current[index] = element
+                }}
+              >
+                {item.content}
+              </Text>
+            ))}
           </div>
         </Card>
         <div className={classNames.footerWrapper}>
