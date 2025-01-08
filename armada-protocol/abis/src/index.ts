d4a36c08d603894e39f192f7df2e6c9a6d8b3db3
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-01-08 16:06:26 +0100

    updated generator

--- armada-protocol/abis/src/index.ts
+++ armada-protocol/abis/src/index.ts
@@ -1,1 +1,4 @@
-
+export * from './AdmiralsQuarters.sol';
+export * from './FleetCommander.sol';
+export * from './StakingRewardsManagerBase.sol';
+export * from './SummerRewardsRedeemer.sol';
---
+++ bin/generate.sh
@@ -0,0 +1,6 @@
+cd armada-protocol/contracts && pnpm build && cd packages/core-contracts/out/ && cp -r AdmiralsQuarters.sol FleetCommander.sol StakingRewardsManagerBase.sol ../../../../abis/src
+
+cd ../../rewards-contracts && pnpm i && forge build --extra-output-files abi && cd out && cp -r SummerRewardsRedeemer.sol ../../../../abis/src
+
+cd ../../../../.. && tsx ./scripts/transform-json-to-viem-abi.ts && cd armada-protocol/abis && pnpm genindex && pnpm build
+
