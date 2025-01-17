236039d041591da2356d63f310f97a68e84a28b4
Marcin Ciarka <marcin@oazoapps.com>
2025-01-17 14:45:20 +0100

    Remove web3-onboard from the earn app and  WalletInit component from MasterPage

--- apps/earn-protocol/components/layout/MasterPage/MasterPage.tsx
+++ apps/earn-protocol/components/layout/MasterPage/MasterPage.tsx
@@ -3,7 +3,6 @@ import { Footer, NewsletterWrapper, Text } from '@summerfi/app-earn-ui'
 
 // import dynamic from 'next/dynamic'
 import { NavigationWrapper } from '@/components/layout/Navigation/NavigationWrapper'
-import { WalletInit } from '@/components/molecules/WalletInit/WalletInit'
 
 import './global.css'
 import masterPageStyles from './MasterPage.module.scss'
@@ -16,58 +15,55 @@ interface MasterPageProps {}
 
 export const MasterPage: FC<PropsWithChildren<MasterPageProps>> = ({ children }) => {
   return (
-    <>
-      <WalletInit />
-      <div className={masterPageStyles.mainContainer}>
-        <NavigationWrapper />
-        <div className={masterPageStyles.appContainer}>{children}</div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            marginTop: '32px',
-            marginBottom: '32px',
-            justifyContent: 'center',
-            alignItems: 'center',
-            gap: '20px',
-          }}
-        >
-          {/* <SetForkModal /> */}
-          <Footer
-            logo="/earn/img/branding/logo-light.svg"
-            newsletter={
-              <div>
-                <Text
-                  as="h3"
-                  variant="p1semi"
-                  style={{
-                    marginBottom: 'var(--general-space-16)',
-                  }}
-                >
-                  Stay up to date
-                </Text>
-                <Text
-                  as="p"
-                  variant="p2"
-                  style={{
-                    color: 'var(--color-text-secondary)',
-                    marginBottom: 'var(--spacing-space-medium)',
-                  }}
-                >
-                  Subscribe to the newsletter for Oasis updates
-                </Text>
-                <div
-                  style={{
-                    maxWidth: '380px',
-                  }}
-                >
-                  <NewsletterWrapper inputBtnLabel="Subscribe" />
-                </div>
+    <div className={masterPageStyles.mainContainer}>
+      <NavigationWrapper />
+      <div className={masterPageStyles.appContainer}>{children}</div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          marginTop: '32px',
+          marginBottom: '32px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '20px',
+        }}
+      >
+        {/* <SetForkModal /> */}
+        <Footer
+          logo="/earn/img/branding/logo-light.svg"
+          newsletter={
+            <div>
+              <Text
+                as="h3"
+                variant="p1semi"
+                style={{
+                  marginBottom: 'var(--general-space-16)',
+                }}
+              >
+                Stay up to date
+              </Text>
+              <Text
+                as="p"
+                variant="p2"
+                style={{
+                  color: 'var(--color-text-secondary)',
+                  marginBottom: 'var(--spacing-space-medium)',
+                }}
+              >
+                Subscribe to the newsletter for Oasis updates
+              </Text>
+              <div
+                style={{
+                  maxWidth: '380px',
+                }}
+              >
+                <NewsletterWrapper inputBtnLabel="Subscribe" />
               </div>
-            }
-          />
-        </div>
+            </div>
+          }
+        />
       </div>
-    </>
+    </div>
   )
 }
