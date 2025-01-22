09a65f22eff1586c33e9b558269ed4705ef8d5ad
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-01-22 09:45:46 +0100

    Update clean-build.sh to exclude specific directories for faster cleanup

--- bin/clean-build.sh
+++ bin/clean-build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-find . -name dist -type d -exec rm -rvf {} \;
-find . -name tsconfig.build.tsbuildinfo -type f -exec rm -rvf {} \;
-find . -name tsconfig.tsbuildinfo -type f -exec rm -rvf {} \;
+# added to exclude armada-protocol, nested node_modules and .next folders
+# to be faster and not delete the wrong files
+find . -path '*/node_modules' -prune -o -path ./armada-protocol -prune -o -path '*/.next' -prune -o -name dist -type d -exec rm -rvf {} \;
+find . -path '*/node_modules' -prune -o -path ./armada-protocol -prune -o -path '*/.next' -prune -o -name tsconfig.build.tsbuildinfo -type f -exec rm -rvf {} \;
+find . -path '*/node_modules' -prune -o -path ./armada-protocol -prune -o -path '*/.next' -prune -o -name tsconfig.tsbuildinfo -type f -exec rm -rvf {} \;
