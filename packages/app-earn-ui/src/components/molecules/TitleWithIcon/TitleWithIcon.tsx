9da154958942ef8fa01f4906c4fcfd0fe1752ccc
sebastianPiekarczyk <sebastian@oazoapps.com>
2025-01-24 11:39:53 +0100

    Adjust Datablock tooltip width and white spaces handling

--- packages/app-earn-ui/src/components/molecules/TitleWithIcon/TitleWithIcon.tsx
+++ packages/app-earn-ui/src/components/molecules/TitleWithIcon/TitleWithIcon.tsx
@@ -18,6 +18,7 @@ export type TitleWithIconProps = {
   className?: string
   color?: string
   titleStyle?: CSSProperties
+  tooltipWrapperStyles?: CSSProperties
 }
 
 export const TitleWithIcon = ({
@@ -28,6 +29,7 @@ export const TitleWithIcon = ({
   className,
   color,
   titleStyle,
+  tooltipWrapperStyles,
 }: TitleWithIconProps) => {
   // clear undefined values from style properties to avoid overwrite
   const resolvedTitleStyle = pickBy(titleStyle, identity)
@@ -39,7 +41,7 @@ export const TitleWithIcon = ({
       </Text>
       {iconName && !tooltip && <Icon iconName={iconName} size={18} color={color} />}
       {iconName && tooltip && (
-        <Tooltip tooltip={tooltip}>
+        <Tooltip tooltip={tooltip} tooltipWrapperStyles={tooltipWrapperStyles}>
           <Icon iconName={iconName} size={18} color={color} />
         </Tooltip>
       )}
