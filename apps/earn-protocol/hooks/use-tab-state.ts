6b98761dc2bcbbceda05d1fb180c0ca9218cbd3b
Marcin Ciarka <marcin@oazoapps.com>
2025-01-28 21:33:00 +0100

    Remove unused Mixpanel tracking import from use-tab-state hook

--- apps/earn-protocol/hooks/use-tab-state.ts
+++ apps/earn-protocol/hooks/use-tab-state.ts
@@ -3,9 +3,7 @@
 import { useEffect, useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 
-import { trackButtonClick } from '@/helpers/mixpanel'
-
-/**
+**
  * Extracts the values from an enum-like object or returns the array as-is.
  */
 function extractTabValues<T extends string>(
