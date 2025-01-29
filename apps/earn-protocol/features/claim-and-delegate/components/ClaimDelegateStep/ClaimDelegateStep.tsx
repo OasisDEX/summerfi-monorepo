03b04e47f15af90303f853048c7175f815d8fa2f
Marcin Ciarka <marcin@oazoapps.com>
2025-01-29 14:25:47 +0100

    Update governance link in ClaimDelegateStep component

--- apps/earn-protocol/features/claim-and-delegate/components/ClaimDelegateStep/ClaimDelegateStep.tsx
+++ apps/earn-protocol/features/claim-and-delegate/components/ClaimDelegateStep/ClaimDelegateStep.tsx
@@ -318,7 +318,7 @@ export const ClaimDelegateStep: FC<ClaimDelegateStepProps> = ({
         >
           Every delegate declares their governance objectives.
         </Text>
-        <Link href="/" target="_blank">
+        <Link href="https://forum.summer.fi/c/delegates/19" target="_blank">
           <WithArrow as="p" variant="p3">
             Learn more about Governance
           </WithArrow>
