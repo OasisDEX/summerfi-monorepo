2fc52d2a41e0bbd134c85772cbdf186f2ca5a0c5
halaprix <panicz.klos@gmail.com>
2025-02-07 17:14:30 +0100

    feat: protocol endpoint

--- packages/summer-earn-protocol-subgraph/src/utils/subgraphNameByChainMap.ts
+++ packages/summer-earn-protocol-subgraph/src/utils/subgraphNameByChainMap.ts
@@ -2,4 +2,6 @@ import { ChainId } from '@summerfi/serverless-shared'
 
 export const subgraphNameByChainMap: Partial<Record<ChainId, string>> = {
   [ChainId.BASE]: 'summer-protocol-base',
+  [ChainId.ARBITRUM]: 'summer-protocol-arbitrum',
+  [ChainId.MAINNET]: 'summer-protocol',
 }
--- packages/summer-earn-protocol-subgraph/src/wrappers/index.ts
+++ packages/summer-earn-protocol-subgraph/src/wrappers/index.ts
@@ -1,1 +1,4 @@
-export * from './getUserPositions'
+export { getUserPositions } from './getUserPositions'
+export { getUsersPositions } from './getUsersPositions'
+export { getVaults } from './getVaults'
+export { getUsers } from './getUsers'
