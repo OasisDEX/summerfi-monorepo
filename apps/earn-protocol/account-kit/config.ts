0d427aff97bbd9cf12c99193e5f4ed803854ec4c
sebastianPiekarczyk <sebastian@oazoapps.com>
2025-02-10 19:32:01 +0100

    Override gas sponsorship when needed

--- apps/earn-protocol/account-kit/config.ts
+++ apps/earn-protocol/account-kit/config.ts
@@ -85,3 +85,8 @@ export const getAccountKitConfig = ({
 }
 
 export const accountType = 'MultiOwnerModularAccount'
+
+// to be used for cases when user is logged in using smart account and is no longer eligible for gas sponsorship
+export const overridesGasSponsorship: { paymasterAndData: `0x${string}` } = {
+  paymasterAndData: '0x',
+}
