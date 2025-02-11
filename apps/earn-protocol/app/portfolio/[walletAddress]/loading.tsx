411444c804198fc69ed2c2ac1c0f17ea5a11dd92
Marcin Ciarka <marcin@oazoapps.com>
2025-02-11 10:01:47 +0100

    Add loading state component for portfolio page and update styles

---
+++ apps/earn-protocol/app/portfolio/[walletAddress]/loading.tsx
@@ -0,0 +1,7 @@
+import { PortfolioPageViewLoadingState } from '@/components/layout/PortfolioPageView/PortfolioPageViewLoadingState'
+
+const PortfolioPageLoadingState = () => {
+  return <PortfolioPageViewLoadingState walletAddress="0x1234567890" />
+}
+
+export default PortfolioPageLoadingState
