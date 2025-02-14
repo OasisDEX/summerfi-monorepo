7966fa1e301bb2e7f49481a321dc27bcedffaa51
Marcin Ciarka <marcin@oazoapps.com>
2025-02-14 17:28:00 +0100

    Add revalidation tags and increase cache duration for interest rates

--- apps/earn-protocol/helpers/revalidation-handlers.ts
+++ apps/earn-protocol/helpers/revalidation-handlers.ts
@@ -1,5 +1,6 @@
 'use server'
 
+import { REVALIDATION_TAGS } from '@summerfi/app-earn-ui'
 import { revalidatePath, revalidateTag } from 'next/cache'
 
 export const revalidateUser = (walletAddress?: string) => {
@@ -12,6 +13,7 @@ export const revalidateUser = (walletAddress?: string) => {
 export const revalidateVaultsListData = () => {
   // clears the cache and revalidates the vaults list data
   revalidatePath('/earn')
+  revalidateTag(REVALIDATION_TAGS.INTEREST_RATES)
 }
 
 export const revalidatePositionData = (
