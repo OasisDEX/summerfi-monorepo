d74a503fa341b0dfe3cf681002d1cc452f72727b
piekczyk <sebastian@oazoapps.com>
2025-03-05 09:02:27 +0100

    Fix issue with cleanAmount method

--- packages/app-utils/src/formatters/clean-amount.ts
+++ packages/app-utils/src/formatters/clean-amount.ts
@@ -1,12 +1,12 @@
 /**
  * Removes commas from a numeric string
- * @param amount - The amount string that may contain commas
+ * @param amount - The amount string | number | undefined that may contain commas
  * @returns The amount string with all commas removed
  */
-export const cleanAmount = (amount: string | undefined): string => {
+export const cleanAmount = (amount: string | number | undefined): string => {
   if (!amount) {
     return ''
   }
 
-  return amount.replace(/,/gu, '')
+  return amount.toString().replace(/,/gu, '')
 }
