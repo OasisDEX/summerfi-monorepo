0eb70c5c44ee397e9e0e6a4ecb7a582366e4dd7b
piekczyk <sebastian@oazoapps.com>
2025-03-06 13:25:03 +0100

    Add compound platform logo

--- apps/earn-protocol/helpers/platform-logo-map.ts
+++ apps/earn-protocol/helpers/platform-logo-map.ts
@@ -1,6 +1,7 @@
 import { ArmadaMigrationType } from '@summerfi/sdk-common'
 
 import aaveLogo from '@/public/img/platform_icons/platform_logo_aave.svg'
+import compoundLogo from '@/public/img/platform_icons/platform_logo_compound.svg'
 import morphoLogo from '@/public/img/platform_icons/platform_logo_morpho.svg'
 import sparkLogo from '@/public/img/platform_icons/platform_logo_spark.svg'
 import summerLogo from '@/public/img/platform_icons/platform_logo_summerfi.svg'
@@ -10,10 +11,11 @@ export const platformLogoMap = {
   morpho: morphoLogo,
   spark: sparkLogo,
   summer: summerLogo,
+  compound: compoundLogo,
 }
 
 export const platformLogoMapByMigrationType = {
   [ArmadaMigrationType.AaveV3]: 'aave',
-  [ArmadaMigrationType.Compound]: 'morpho',
+  [ArmadaMigrationType.Compound]: 'compound',
   [ArmadaMigrationType.Erc4626]: 'morpho',
 }
