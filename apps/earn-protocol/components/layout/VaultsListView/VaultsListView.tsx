a5a3b2b59b4ef4f21bdc035d2b9f0e8c6f48cd68
Marcin Ciarka <marcin@oazoapps.com>
2025-03-18 15:17:32 +0100

    Add metadata generation for vault and position pages to enhance SEO and user experience

--- apps/earn-protocol/components/layout/VaultsListView/VaultsListView.tsx
+++ apps/earn-protocol/components/layout/VaultsListView/VaultsListView.tsx
@@ -38,6 +38,7 @@ import { networkIconByNetworkName } from '@/constants/networkIcons'
 import { useDeviceType } from '@/contexts/DeviceContext/DeviceContext'
 import { getResolvedForecastAmountParsed } from '@/helpers/get-resolved-forecast-amount-parsed'
 import { revalidateVaultsListData } from '@/helpers/revalidation-handlers'
+import { getVaultsProtocolsList } from '@/helpers/vaults-protocols-list'
 import { useAppSDK } from '@/hooks/use-app-sdk'
 import { usePosition } from '@/hooks/use-position'
 import { useTokenBalances } from '@/hooks/use-tokens-balances'
@@ -176,57 +177,7 @@ export const VaultsListView = ({
   }, [vaultsList])
 
   const formattedProtocolsSupportedList = useMemo(
-    () => [
-      ...new Set(
-        vaultsList.reduce<string[]>(
-          (acc, { arks }) => [
-            // converting a list which looks like `protocolName-token-chainId`
-            // into a unique list of protocols for all vaults
-            ...acc,
-            ...arks
-              .map((ark) => {
-                if (ark.name === 'BufferArk' || !ark.name) {
-                  return null
-                }
-
-                const arkNameArray = ark.name.split('-')
-
-                // special case for ERC4626
-                if (ark.name.startsWith('ERC4626')) {
-                  // examples
-                  // ERC4626-Euler_Prime-usdc-1
-                  // ERC4626-Gearbox-weth-1
-                  return `${arkNameArray[1]}`.replaceAll(/_+/gu, ' ')
-                }
-                // special case for MorphoVault
-                if (ark.name.startsWith('MorphoVault')) {
-                  // examples
-                  // MorphoVault-usdc-Flagship_USDC-1
-                  // MorphoVault-weth-Steakhouse_WETH-1
-                  const morphoArkName = `Morpho ${arkNameArray[2].split('_')[0]}`
-
-                  if (
-                    acc.filter((item) => item.toLowerCase() === morphoArkName.toLowerCase())
-                      .length > 0
-                  ) {
-                    return false
-                  }
-
-                  return morphoArkName
-                }
-
-                // the rest should be like this:
-                // AaveV3-usdc-1
-                // CompoundV3-usdt-1
-                // Spark-weth-1
-                return arkNameArray[0]
-              })
-              .filter((arkName): arkName is string => Boolean(arkName)),
-          ],
-          [],
-        ),
-      ),
-    ],
+    () => getVaultsProtocolsList(vaultsList),
     [vaultsList],
   )
 
