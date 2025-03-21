b79d78aa108f6a2b04eac221081026357f3e9fa2
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-03-21 14:58:32 +0100

    chore: update dependencies to latest versions for @trpc/client, @aws-lambda-powertools packages, and superjson

--- sdk/sdk-router-function/src/index.ts
+++ sdk/sdk-router-function/src/index.ts
@@ -1,15 +1,9 @@
 import { awsLambdaRequestHandler } from '@trpc/server/adapters/aws-lambda'
 import { sdkAppRouter, createSDKContext } from '@summerfi/sdk-server'
-import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
-import middy from '@middy/core'
-const logger = new Logger({
-  serviceName: 'sdk-router-function',
-  logLevel: 'DEBUG',
-})
 
 export const baseHandler = awsLambdaRequestHandler({
   router: sdkAppRouter,
   createContext: createSDKContext,
 })
 
-export const handler = middy(baseHandler).use(injectLambdaContext(logger))
+export const handler = baseHandler
