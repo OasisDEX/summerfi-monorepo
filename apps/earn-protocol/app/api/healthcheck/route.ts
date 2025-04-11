b78f1ecdcac86234cfee0f105b78b847e8cd3a7b
Marcin Ciarka <marcin@oazoapps.com>
2025-04-11 14:55:39 +0200

    Implement health check endpoint for memory usage statistics (#950)
    This pull request includes enhancements to the health check endpoint in
    the `apps/earn-protocol` project. The changes add detailed memory usage
    information for both the operating system and the Node.js process.
    
    Enhancements to health check endpoint:
    
    *
    [`apps/earn-protocol/app/api/healthcheck/route.ts`](diffhunk://#diff-ad8d340d09e54170df2ca6ea4fbcbd2b881c777b673ccb0c877e24e020c9d5dfR2-R38):
    Added imports for `freemem` and `totalmem` from the `os` module, and a
    new function `formatMemoryUsage` to format memory data. Updated the
    `GET` function to include memory usage details for the OS and Node.js
    process in the JSON response.

--- apps/earn-protocol/app/api/healthcheck/route.ts
+++ apps/earn-protocol/app/api/healthcheck/route.ts
@@ -1,8 +1,39 @@
 import { NextResponse } from 'next/server'
+import { freemem, totalmem } from 'os'
 
 export const revalidate = 0
 
+const formatMemoryUsage = (data: number) => `${Math.round((data / 1024 / 1024) * 100) / 100} MB`
+
 // eslint-disable-next-line require-await
 export async function GET() {
-  return NextResponse.json({ pong: true, time: Date.now() })
+  try {
+    const freeMemory = freemem()
+    const totalMemory = totalmem()
+    const os = {
+      freeMemory: `${formatMemoryUsage(freeMemory)} -> free memory available`,
+      totalMemory: `${formatMemoryUsage(totalMemory)} -> total memory available`,
+    }
+
+    const memoryData = process.memoryUsage()
+    const node = {
+      rss: `${formatMemoryUsage(memoryData.rss)} -> Resident Set Size - total memory allocated for the process execution`,
+      heapTotal: `${formatMemoryUsage(memoryData.heapTotal)} -> total size of the allocated heap`,
+      heapUsed: `${formatMemoryUsage(memoryData.heapUsed)} -> actual memory used during the execution`,
+      external: `${formatMemoryUsage(memoryData.external)} -> V8 external memory`,
+    }
+
+    return NextResponse.json({
+      pong: true,
+      time: Date.now(),
+      os,
+      node,
+    })
+  } catch (error) {
+    return NextResponse.json({
+      pong: true,
+      time: Date.now(),
+      error: error instanceof Error ? error.toString() : String(error),
+    })
+  }
 }
