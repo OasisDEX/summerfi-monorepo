81958d59ee621fab3bb4d7d1225a437cc4247e7e
Marcin Ciarka <marcin@oazoapps.com>
2025-04-15 16:26:31 +0200

    chore: simplify healthcheck GET endpoint by removing unused code and returning a basic response

--- apps/earn-protocol/app/api/healthcheck/route.ts
+++ apps/earn-protocol/app/api/healthcheck/route.ts
@@ -1,50 +1,8 @@
-import { readFileSync } from 'fs'
-import { type NextRequest, NextResponse } from 'next/server'
-import path from 'path'
-import { writeHeapSnapshot } from 'v8'
+import { NextResponse } from 'next/server'
 
 export const revalidate = 0
 
 // eslint-disable-next-line require-await
-export async function GET(request: NextRequest) {
-  const { searchParams, host } = request.nextUrl
-  const jwtSecret = process.env.EARN_PROTOCOL_JWT_SECRET
-
-  if (!jwtSecret) {
-    return NextResponse.json({ error: 'Required ENV variable is not set' }, { status: 500 })
-  }
-
-  if (searchParams.get('secret') !== jwtSecret) {
-    return NextResponse.json({ error: 'Invalid secret' }, { status: 401 })
-  }
-  const filename = searchParams.get('filename')
-
-  if (filename) {
-    // return the snapshot file
-    const filePath = path.join(process.cwd(), filename)
-    const file = readFileSync(filePath)
-
-    return new NextResponse(file, {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Disposition': `attachment; filename=${filename}`,
-      },
-    })
-  }
-  // create a heap snapshot
-  const snapshot = writeHeapSnapshot()
-
-  return NextResponse.json(
-    {
-      message: 'Heap snapshot created',
-      url: `${host}/earn/api/healthcheck?filename=${snapshot}&secret=${jwtSecret}`,
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      status: 200,
-    },
-  )
+export async function GET() {
+  return NextResponse.json({ pong: true, time: Date.now() })
 }
