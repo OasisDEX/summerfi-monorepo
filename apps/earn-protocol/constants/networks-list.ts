82c3c9fd874130bda7679650bb3e8b384a779dfe
Marcin Ciarka <marcin@oazoapps.com>
2025-04-16 14:49:26 +0200

    chore: remove unused network configuration types for improved code clarity and maintainability

--- apps/earn-protocol/constants/networks-list.ts
+++ apps/earn-protocol/constants/networks-list.ts
@@ -2,8 +2,6 @@ import { SDKChainId } from '@summerfi/app-types'
 
 import { clientId } from '@/helpers/client-id'
 
-type NetworkConfigHexId = `0x${number | string}`
-
 export enum NetworkNames {
   ethereumMainnet = 'ethereum',
   arbitrumMainnet = 'arbitrum',
@@ -28,47 +26,6 @@ export enum NetworkIds {
   BASEGOERLI = 84531,
   EMPTYNET = 0,
   SONICMAINNET = 146,
-}
-
-enum NetworkHexIds {
-  MAINNET = '0x1',
-  GOERLI = '0x5',
-  DEFAULTFORK = '0x859',
-  ARBITRUMMAINNET = '0xa4b1',
-  ARBITRUMGOERLI = '0x66eed',
-  POLYGONMAINNET = '0x89',
-  POLYGONMUMBAI = '0x13881',
-  OPTIMISMMAINNET = '0xa',
-  OPTIMISMGOERLI = '0x1a4',
-  BASEMAINNET = '0x2105',
-  BASEGOERLI = '0x14a33',
-  EMPTYNET = '0x0',
-  SONICMAINNET = '0x92',
-}
-
-type NetworkLabelType =
-  | 'Ethereum'
-  | 'Ethereum Goerli'
-  | 'Arbitrum'
-  | 'Arbitrum Goerli'
-  | 'Polygon'
-  | 'Polygon Mumbai'
-  | 'Optimism'
-  | 'Optimism Goerli'
-  | 'Base'
-  | 'Base Goerli'
-  | 'Sonic'
-
-type NetworkConfig = {
-  name: NetworkNames
-  testnet: boolean
-  badge: string
-  hexId: NetworkConfigHexId
-  label: NetworkLabelType
-  token: string
-  color: `#${number | string}`
-  id: NetworkIds
-  rpcUrl: string
 }
 
 function getRpc(network: NetworkNames): string {
