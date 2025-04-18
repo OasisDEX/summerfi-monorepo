9bf98dd590e96a0dff33f74a9977ef6f0f9d7cef
Marcin Ciarka <marcin@oazoapps.com>
2025-04-18 13:37:55 +0200

    fix: update format script in package.json to use cache and clean up exports in index.ts

--- sdk/subgraph-manager-service/src/index.ts
+++ sdk/subgraph-manager-service/src/index.ts
@@ -1,4 +1,3 @@
-
-export { SubgraphManagerFactory } from './implementation/SubgraphManagerFactory';
-export { ArmadaSubgraphManager } from './implementation/armada/ArmadaSubgraphManager';
-export type { SubgraphConfig } from './implementation/armada/ArmadaSubgraphManager';
+export { SubgraphManagerFactory } from './implementation/SubgraphManagerFactory'
+export { ArmadaSubgraphManager } from './implementation/armada/ArmadaSubgraphManager'
+export type { SubgraphConfig } from './implementation/armada/ArmadaSubgraphManager'
