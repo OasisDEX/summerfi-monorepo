de994a418885d92195f3489dad9eda6da08def26
piekczyk <sebastian@oazoapps.com>
2025-04-23 11:27:01 +0200

    refactor: streamline SDK environment variable validation logic

--- stacks/sdk-router-lambda.ts
+++ stacks/sdk-router-lambda.ts
@@ -34,60 +34,35 @@ const {
   SUMMER_DEPLOYMENT_CONFIG,
 } = process.env
 
+const requiredEnvs = {
+  COINGECKO_API_URL,
+  COINGECKO_API_VERSION,
+  COINGECKO_API_KEY,
+  COINGECKO_API_AUTH_HEADER,
+  COINGECKO_SUPPORTED_CHAIN_IDS,
+  ONE_INCH_API_URL,
+  ONE_INCH_API_VERSION,
+  ONE_INCH_API_KEY,
+  ONE_INCH_API_AUTH_HEADER,
+  ONE_INCH_SWAP_CHAIN_IDS,
+  ONE_INCH_API_SPOT_URL,
+  ONE_INCH_API_SPOT_VERSION,
+  ONE_INCH_API_SPOT_KEY,
+  ONE_INCH_API_SPOT_AUTH_HEADER,
+  SDK_RPC_GATEWAY,
+  SDK_SUBGRAPH_CONFIG,
+  SDK_DISTRIBUTIONS_BASE_URL,
+  SDK_DISTRIBUTIONS_FILES,
+  SUMMER_HUB_CHAIN_ID,
+  SUMMER_DEPLOYED_CHAINS_ID,
+  SUMMER_DEPLOYMENT_CONFIG,
+}
+
 export function addSdkConfig({ stack }: StackContext, api: Api) {
-  if (
-    !COINGECKO_API_URL ||
-    !COINGECKO_API_VERSION ||
-    !COINGECKO_API_KEY ||
-    !COINGECKO_API_AUTH_HEADER ||
-    !COINGECKO_SUPPORTED_CHAIN_IDS ||
-    !ONE_INCH_API_URL ||
-    !ONE_INCH_API_VERSION ||
-    !ONE_INCH_API_KEY ||
-    !ONE_INCH_API_AUTH_HEADER ||
-    !ONE_INCH_SWAP_CHAIN_IDS ||
-    !ONE_INCH_API_SPOT_URL ||
-    !ONE_INCH_API_SPOT_VERSION ||
-    !ONE_INCH_API_SPOT_KEY ||
-    !ONE_INCH_API_SPOT_AUTH_HEADER ||
-    !SDK_RPC_GATEWAY ||
-    !SDK_SUBGRAPH_CONFIG ||
-    !SDK_DISTRIBUTIONS_BASE_URL ||
-    !SDK_DISTRIBUTIONS_FILES ||
-    !SUMMER_HUB_CHAIN_ID ||
-    !SUMMER_DEPLOYED_CHAINS_ID ||
-    !SUMMER_DEPLOYMENT_CONFIG
-  ) {
-    throw new Error(
-      'Some SDK configuration env variables are missing: ' +
-        JSON.stringify(
-          Object.entries({
-            COINGECKO_API_URL,
-            COINGECKO_API_VERSION,
-            COINGECKO_API_KEY,
-            COINGECKO_API_AUTH_HEADER,
-            COINGECKO_SUPPORTED_CHAIN_IDS,
-            ONE_INCH_API_URL,
-            ONE_INCH_API_VERSION,
-            ONE_INCH_API_KEY,
-            ONE_INCH_API_AUTH_HEADER,
-            ONE_INCH_SWAP_CHAIN_IDS,
-            ONE_INCH_API_SPOT_URL,
-            ONE_INCH_API_SPOT_VERSION,
-            ONE_INCH_API_SPOT_KEY,
-            ONE_INCH_API_SPOT_AUTH_HEADER,
-            SDK_RPC_GATEWAY,
-            SDK_SUBGRAPH_CONFIG,
-            SDK_DISTRIBUTIONS_BASE_URL,
-            SDK_DISTRIBUTIONS_FILES,
-            SUMMER_HUB_CHAIN_ID,
-            SUMMER_DEPLOYED_CHAINS_ID,
-            SUMMER_DEPLOYMENT_CONFIG,
-          }),
-          null,
-          2,
-        ),
-    )
+  for (const [key, value] of Object.entries(requiredEnvs)) {
+    if (!value) {
+      throw new Error(`${key} is missing`)
+    }
   }
 
   const sdkRouterFunction = new Function(stack, 'sdk-router-function', {
