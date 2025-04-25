5be68cdcee8c6cf8854de4405e2b78e6713a785b
piekczyk <sebastian@oazoapps.com>
2025-04-25 12:28:18 +0200

    fix: update VPC security group ID to use NEW_SECURITY_GROUP_ID

--- stacks/vpc.ts
+++ stacks/vpc.ts
@@ -4,19 +4,19 @@ import * as process from 'node:process'
 import { VPCContext } from './summer-stack-context'
 
 export function attachVPC({ stack, isDev }: StackContext & { isDev: boolean }): VPCContext | null {
-  const { VPC_ID, SECURITY_GROUP_ID } = process.env
+  const { VPC_ID, NEW_SECURITY_GROUP_ID } = process.env
 
   if (isDev) {
     console.info('VPC is not attached in dev stages')
     return null
   }
 
-  if (!VPC_ID || !SECURITY_GROUP_ID) {
+  if (!VPC_ID || !NEW_SECURITY_GROUP_ID) {
     if (!isDev) {
       console.error(
-        'VPC_ID or SECURITY_GROUP_ID are not set, VPC will not be attached to the stack. This is only allowed in dev stages.',
+        'VPC_ID or NEW_SECURITY_GROUP_ID are not set, VPC will not be attached to the stack. This is only allowed in dev stages.',
       )
-      throw new Error('VPC_ID or SECURITY_GROUP_ID are not set')
+      throw new Error('VPC_ID or NEW_SECURITY_GROUP_ID are not set')
     }
   }
 
@@ -24,6 +24,6 @@ export function attachVPC({ stack, isDev }: StackContext & { isDev: boolean }): 
     vpcId: VPC_ID,
   })
 
-  const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(stack, 'SG', SECURITY_GROUP_ID!)
+  const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(stack, 'SG', NEW_SECURITY_GROUP_ID!)
   return { vpc, securityGroup }
 }
