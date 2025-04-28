39bf489dae9741a4fe178a3b18048fe7805a1886
Marcin Ciarka <marcin@oazoapps.com>
2025-04-28 16:02:14 +0200

    feat: enhance vault naming by including risk level in getVaultNiceName utility

--- packages/app-utils/src/helpers/get-vault-nice-name.ts
+++ packages/app-utils/src/helpers/get-vault-nice-name.ts
@@ -4,4 +4,4 @@ import { capitalize } from 'lodash-es'
 export const getVaultNiceName = ({ vault }: { vault: SDKVaultishType }): string =>
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   vault?.customFields?.name ??
-  `Summer ${vault.inputToken.symbol} ${capitalize(vault.protocol.network)} Vault`
+  `Summer ${vault.inputToken.symbol} ${capitalize(vault.protocol.network)}${vault.customFields?.risk ? ` ${capitalize(vault.customFields.risk)} risk ` : ''} Vault`
