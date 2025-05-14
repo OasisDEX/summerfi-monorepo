6f44e67bec018836e1880149fcc1056162590bf7
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-05-14 16:03:05 +0200

    fix: correct TX_CONFIRMATION_WAIT_TIME to 5 seconds

--- sdk/sdk-e2e/e2e/utils/constants.ts
+++ sdk/sdk-e2e/e2e/utils/constants.ts
@@ -1,5 +1,5 @@
 /**
  * Number of seconds to wait after sending transactions for confirmation
  */
-export const TX_CONFIRMATION_WAIT_TIME = 10
+export const TX_CONFIRMATION_WAIT_TIME = 5
 export const DEFAULT_SLIPPAGE_PERCENTAGE = 0.5
