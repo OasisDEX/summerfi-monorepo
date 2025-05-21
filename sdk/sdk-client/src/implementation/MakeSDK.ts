95273d2673a5ec4fa98086f93506e88c8b92eca9
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-05-21 20:22:33 +0200

    fix: update SDK version import and adjust tsconfig include path

--- sdk/sdk-client/src/implementation/MakeSDK.ts
+++ sdk/sdk-client/src/implementation/MakeSDK.ts
@@ -1,9 +1,9 @@
 import { createMainRPCClient } from '../rpc/SDKMainClient'
 import { SDKManager } from './SDKManager'
-import { version as SDK_VERSION } from '../../package.json'
+import { version as sdkClientVersion } from '../../bundle/package.json'
 
 export function makeSDK(params: { apiURL: string; logging?: boolean }) {
-  const versionedURL = new URL('v' + SDK_VERSION, params.apiURL).toString()
+  const versionedURL = new URL(`/api/sdk/v${sdkClientVersion.charAt(0)}`, params.apiURL).toString()
   const rpcClient = createMainRPCClient(versionedURL, params.logging)
 
   return new SDKManager({ rpcClient })
