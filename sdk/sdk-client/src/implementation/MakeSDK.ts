250296b7e9269f5bf54aabde0945d821a8b0f836
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-05-23 14:49:00 +0200

    feat: enhance makeSDK function to support apiDomain for flexible URL generation

--- sdk/sdk-client/src/implementation/MakeSDK.ts
+++ sdk/sdk-client/src/implementation/MakeSDK.ts
@@ -2,8 +2,18 @@ import { createMainRPCClient } from '../rpc/SDKMainClient'
 import { SDKManager } from './SDKManager'
 import { version as sdkClientVersion } from '../../bundle/package.json'
 
-export function makeSDK(params: { apiURL: string; logging?: boolean }) {
-  const versionedURL = new URL(`/api/sdk/v${sdkClientVersion.charAt(0)}`, params.apiURL).toString()
+export function makeSDK(params: { apiURL: string; logging?: boolean; apiDomain?: string }) {
+  const apiVersion = `v${sdkClientVersion.charAt(0)}`
+  let versionedURL: string
+  if (params.apiDomain) {
+    // url based on direct url
+    versionedURL = new URL(`/api/sdk/${apiVersion}`, params.apiDomain).toString()
+  } else {
+    // url based on domain
+    const apiUrlNormalized = params.apiURL.replace(/\/+$/, '')
+    versionedURL = `${apiUrlNormalized}/${apiVersion}`
+  }
+
   const rpcClient = createMainRPCClient(versionedURL, params.logging)
 
   return new SDKManager({ rpcClient })
