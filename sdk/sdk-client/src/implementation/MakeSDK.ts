c91106a0db31f2185f68430579d34df0225fb1b9
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-05-26 15:08:38 +0200

    Refactor: Update makeSDK function to use MakeSDKParams type and improve parameter handling

--- sdk/sdk-client/src/implementation/MakeSDK.ts
+++ sdk/sdk-client/src/implementation/MakeSDK.ts
@@ -2,16 +2,25 @@ import { createMainRPCClient } from '../rpc/SDKMainClient'
 import { SDKManager } from './SDKManager'
 import { version as sdkClientVersion } from '../../bundle/package.json'
 
-export function makeSDK(params: { apiURL: string; logging?: boolean; apiDomain?: string }) {
+export type MakeSDKParams = { logging?: boolean } & ({ apiDomainUrl: string } | { apiURL: string })
+
+/*
+ * makeSDK is a factory function that creates an instance of SDKManager.
+ * It can take either an apiDomainUrl or a direct apiURL, along with an optional logging flag.
+ * Best to use apiDomainUrl as it provide automatic versioning and routing depending on the client version.
+ */
+export function makeSDK(params: MakeSDKParams) {
   const apiVersion = `v${sdkClientVersion.charAt(0)}`
   let versionedURL: string
-  if (params.apiDomain) {
+  if ('apiDomainUrl' in params) {
     // url based on direct url
-    versionedURL = new URL(`/api/sdk/${apiVersion}`, params.apiDomain).toString()
-  } else {
+    versionedURL = new URL(`/api/sdk/${apiVersion}`, params.apiDomainUrl).toString()
+  } else if ('apiURL' in params) {
     // url based on domain
     const apiUrlNormalized = params.apiURL.replace(/\/+$/, '')
     versionedURL = `${apiUrlNormalized}/${apiVersion}`
+  } else {
+    throw new Error('Either apiDomainUrl or apiURL must be provided')
   }
 
   const rpcClient = createMainRPCClient(versionedURL, params.logging)
