f935f2803579e79888213cd336e39a2566b5d1e6
Marcin Ciarka <marcin@oazoapps.com>
2025-05-27 14:09:26 +0200

    Reorder LandingPageBanners and GoogleTagManager in RootLayout for improved structure

--- apps/earn-protocol-landing-page/app/layout.tsx
+++ apps/earn-protocol-landing-page/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <GlobalStyles />
       </head>
       <body className={` ${fontInter.variable}`}>
-        <LandingPageBanners />
         <GoogleTagManager />
         <LandingPageDataContextProvider>
+          <LandingPageBanners />
           <LandingMasterPage>{children}</LandingMasterPage>
         </LandingPageDataContextProvider>
         <div id="portal" style={{ position: 'absolute' }} />
