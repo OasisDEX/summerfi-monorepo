11720f0d91cb9c80eb46d6294c1ff747b4d84c2e
Marcin Ciarka <marcin@oazoapps.com>
2025-06-02 10:51:44 +0200

    Add .cache directory removal to clear-all-artifacts script

--- bin/clear-all-artifacts.sh
+++ bin/clear-all-artifacts.sh
@@ -2,5 +2,6 @@
 ./bin/clean-build.sh
 find . -name node_modules -type d -exec rm -rvf {} \;
 find . -name cache -type d -exec rm -rvf {} \;
+find . -name .cache -type d -exec rm -rvf {} \;
 find . -name coverage -type d -exec rm -rvf {} \;
 find . -name .turbo -type d -exec rm -rvf {} \;
