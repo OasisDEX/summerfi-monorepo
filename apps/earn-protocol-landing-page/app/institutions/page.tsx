f953ca2fee10597e2c758b7316f5ab8cd22ca3bc
Marcin Ciarka <marcin@oazoapps.com>
2025-06-02 15:59:35 +0200

    Add institutions page with styling and initial structure

---
+++ apps/earn-protocol-landing-page/app/institutions/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+// import { redirect } from 'next/navigation'
+
+// import { useLandingPageData } from '@/contexts/LandingPageContext'
+
+import institutionsPageStyles from './institutionsPage.module.css'
+
+export default function InstitutionsPage() {
+  // const { landingPageData } = useLandingPageData()
+
+  // const institutionsEnabled = landingPageData?.systemConfig.features.BeachClub
+
+  // if (institutionsEnabled === false) {
+  //   redirect('/')
+  // }
+
+  return <div className={institutionsPageStyles.wrapper}>eyy</div>
+}
