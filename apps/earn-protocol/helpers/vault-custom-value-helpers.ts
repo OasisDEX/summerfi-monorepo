d3b9e26494516c63f72ec758aaf7697e474f3d86
piekczyk <sebastian@oazoapps.com>
2025-06-07 22:10:29 +0200

    fix: Resolve vault name issue by stripping appended file extensions

--- apps/earn-protocol/helpers/vault-custom-value-helpers.ts
+++ apps/earn-protocol/helpers/vault-custom-value-helpers.ts
@@ -32,14 +32,21 @@ export const getVaultIdByVaultCustomName = (
   if (!fleetMap) {
     return ''
   }
+
+  // temporary fix for appended .txt like the one below
+  // No vault found with the name 0x98c49e13bf99d7cad8069faa2a370933ec9ecf17.txt on the network 42161
+  const resovledVaultCustomName = vaultCustomName.split('.')['0']
+
   const vaultNetworkConfig = fleetMap[String(networkId) as keyof typeof fleetMap]
   const customFields = Object.values(vaultNetworkConfig).find(
-    (fleet) => fleet.slug === vaultCustomName,
+    (fleet) => fleet.slug === resovledVaultCustomName,
   ) as EarnAppFleetCustomConfigType | undefined
 
   if (!customFields?.address) {
     // eslint-disable-next-line no-console
-    console.log(`No vault found with the name ${vaultCustomName} on the network ${networkId}`)
+    console.log(
+      `No vault found with the name ${resovledVaultCustomName} on the network ${networkId}`,
+    )
 
     return ''
   }
