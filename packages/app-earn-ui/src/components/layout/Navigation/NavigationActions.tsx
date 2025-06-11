7364c13f9b5461d192df124fcea8f9b5a3f07c92
piekczyk <sebastian@oazoapps.com>
2025-06-11 23:06:51 +0200

    Set tabbar in portfolio as controlled, update beach club link to internal specification

--- packages/app-earn-ui/src/components/layout/Navigation/NavigationActions.tsx
+++ packages/app-earn-ui/src/components/layout/Navigation/NavigationActions.tsx
@@ -37,7 +37,7 @@ export const NavigationActions = ({
 
   const resolvedBeachClubLink =
     isEarnApp && userWalletAddress
-      ? `${host}/earn/portfolio/${userWalletAddress}?tab=beach-club`
+      ? `/portfolio/${userWalletAddress}?tab=beach-club`
       : `${host}${INTERNAL_LINKS.beachClub}`
 
   return (
