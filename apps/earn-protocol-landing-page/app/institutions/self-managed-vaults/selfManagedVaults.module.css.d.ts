a59a13a1c0153dcd76cf31d9c711dfc1fd766dfa
Marcin Ciarka <marcin@oazoapps.com>
2025-07-11 18:31:39 +0200

    feat: add benefits section with customizable options and corporate governance details to SelfManagedVaults page

--- apps/earn-protocol-landing-page/app/institutions/self-managed-vaults/selfManagedVaults.module.css.d.ts
+++ apps/earn-protocol-landing-page/app/institutions/self-managed-vaults/selfManagedVaults.module.css.d.ts
@@ -1,4 +1,6 @@
 declare const styles: {
+  readonly "benefitsDescriptionAndPoints": string;
+  readonly "benefitsHeaderWrapper": string;
   readonly "descriptionBlock": string;
   readonly "descriptionCard": string;
   readonly "descriptionText": string;
