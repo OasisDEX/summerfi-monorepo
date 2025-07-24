bf05fe81922ca953cc4c3c3e79bcfb717f33a477
piekczyk <sebastian@oazoapps.com>
2025-07-24 17:29:26 +0200

    refactor: restructure Tooltip component in GamesList for improved readability

--- apps/earn-protocol/app/secure/game-leaderboard/GamesList.tsx
+++ apps/earn-protocol/app/secure/game-leaderboard/GamesList.tsx
@@ -167,38 +167,40 @@ export function GamesList({
                   textAlign: 'center',
                 }}
               >
-                <Tooltip
-                  tooltip={
-                    <div>
-                      <strong>Summer Anti Cheat Score:</strong>
-                      <p>{sacScore.explanation}</p>
-                      {sacScore.mean && (
-                        <p>
-                          Mean: {sacScore.mean} ms, StdDev: {sacScore.stdDev} ms
-                        </p>
-                      )}
-                    </div>
-                  }
-                  style={{
-                    display: 'inline-block',
-                  }}
-                  tooltipWrapperStyles={{
-                    width: '300px',
-                  }}
-                >
-                  <span
+                <div>
+                  <Tooltip
+                    tooltip={
+                      <div>
+                        <strong>Summer Anti Cheat Score:</strong>
+                        <p>{sacScore.explanation}</p>
+                        {sacScore.mean && (
+                          <p>
+                            Mean: {sacScore.mean} ms, StdDev: {sacScore.stdDev} ms
+                          </p>
+                        )}
+                      </div>
+                    }
                     style={{
-                      color: sacScore.score < 50 ? 'red' : 'inherit',
-                      fontWeight: sacScore.score < 50 ? 'bold' : 'normal',
+                      display: 'inline-block',
+                    }}
+                    tooltipWrapperStyles={{
+                      width: '300px',
                     }}
                   >
-                    {sacScore.score}
+                    <span
+                      style={{
+                        color: sacScore.score < 50 ? 'red' : 'inherit',
+                        fontWeight: sacScore.score < 50 ? 'bold' : 'normal',
+                      }}
+                    >
+                      {sacScore.score}
+                    </span>
+                  </Tooltip>
+                  <br />
+                  <span style={{ fontSize: '0.8em', color: '#666' }}>
+                    {((entry.responseTimes ?? []) as number[] | undefined)?.length ?? 'n/a'} rounds
                   </span>
-                </Tooltip>
-                <br />
-                <span style={{ fontSize: '0.8em', color: '#666' }}>
-                  {((entry.responseTimes ?? []) as number[] | undefined)?.length ?? 'n/a'} rounds
-                </span>
+                </div>
               </td>
               <td>{averageResponseTime}</td>
               <td>
