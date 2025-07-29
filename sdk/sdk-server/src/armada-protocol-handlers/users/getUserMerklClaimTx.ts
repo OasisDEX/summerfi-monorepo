951e2a791a0aca232e9cdce45221da42cb734c08
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-07-29 21:29:16 +0200

    Refactor chainId validation to use custom zod schema in getUserMerklClaimTx

--- sdk/sdk-server/src/armada-protocol-handlers/users/getUserMerklClaimTx.ts
+++ sdk/sdk-server/src/armada-protocol-handlers/users/getUserMerklClaimTx.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod'
 import { publicProcedure } from '../../SDKTRPC'
-import { AddressValue, isAddressValue, ChainIdSchema } from '@summerfi/sdk-common'
+import { AddressValue, isAddressValue, type ChainId, isChainId } from '@summerfi/sdk-common'
 
 export const getUserMerklClaimTx = publicProcedure
   .input(
     z.object({
       address: z.custom<AddressValue>(isAddressValue),
-      chainId: ChainIdSchema,
+      chainId: z.custom<ChainId>(isChainId),
     }),
   )
   .query(async (opts) => {
