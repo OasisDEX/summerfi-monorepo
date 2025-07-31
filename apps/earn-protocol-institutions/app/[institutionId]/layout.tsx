9957699deac79076c87996137f7768bc598649b1
Marcin Ciarka <marcin@oazoapps.com>
2025-07-31 17:45:20 +0200

    feat: Enhance InstitutionMainLayout and InstitutionPageHeader for wallet integration and improved dropdown functionality

--- apps/earn-protocol-institutions/app/[institutionId]/layout.tsx
+++ apps/earn-protocol-institutions/app/[institutionId]/layout.tsx
@@ -1,6 +1,10 @@
+import { cookies } from 'next/headers'
+
 import { getInstitutionData } from '@/app/server-handlers/institution-data'
+import { getWalletInstitutions } from '@/app/server-handlers/wallet-institutions'
 import { InstitutionPageDataBlocks } from '@/components/layout/InstitutionPageDataBlocks/InstitutionPageDataBlocks'
 import { InstitutionPageHeader } from '@/components/layout/InstitutionPageHeader/InstitutionPageHeader'
+import { LOGIN_COOKIE_WALLET_NAME } from '@/constants/login-cookie'
 
 import institutionMainLayoutStyles from './InstitutionMainLayout.module.css'
 
@@ -11,12 +15,22 @@ export default async function InstitutionMainLayout({
   children: React.ReactNode
   params: Promise<{ institutionId: string }>
 }) {
-  const [{ institutionId }] = await Promise.all([params])
+  const [{ institutionId }, awaitedCookies] = await Promise.all([params, cookies()])
+  const walletAddress = awaitedCookies.get(LOGIN_COOKIE_WALLET_NAME)?.value
+
+  if (!walletAddress) {
+    throw new Error('Wallet address is not available in cookies')
+  }
+
   const institution = await getInstitutionData(institutionId)
+  const walletInstitutionsList = await getWalletInstitutions(walletAddress)
 
   return (
     <div className={institutionMainLayoutStyles.institutionPageView}>
-      <InstitutionPageHeader institutionName={institution.institutionName} />
+      <InstitutionPageHeader
+        selectedInstitution={institution}
+        institutionsList={walletInstitutionsList}
+      />
       <InstitutionPageDataBlocks
         totalValue={institution.totalValue}
         numberOfVaults={institution.numberOfVaults}
