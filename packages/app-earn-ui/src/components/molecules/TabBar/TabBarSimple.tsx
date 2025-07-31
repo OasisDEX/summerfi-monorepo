b61bb633a2e905fe0a181e061b68737ca574dc0c
Marcin Ciarka <marcin@oazoapps.com>
2025-07-31 16:55:13 +0200

    refactor: Replace redirect with useRouter push for tab navigation

--- packages/app-earn-ui/src/components/molecules/TabBar/TabBarSimple.tsx
+++ packages/app-earn-ui/src/components/molecules/TabBar/TabBarSimple.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { type CSSProperties, type FC, type ReactNode } from 'react'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 import { Text } from '@/components/atoms/Text/Text'
 import type TextClassNames from '@/components/atoms/Text/Text.module.css'
@@ -28,8 +28,9 @@ export const TabBarSimple: FC<TabBarProps> = ({
   tabHeadersStyle,
   activeTabId,
 }) => {
+  const { push } = useRouter()
   const onTabClick = (tab: Tab) => {
-    redirect(tab.url)
+    push(tab.url)
   }
 
   return (
