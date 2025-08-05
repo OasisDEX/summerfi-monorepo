310fadcd3febfc054780c14702b301d84a88cdd4
Marcin Ciarka <marcin@oazoapps.com>
2025-08-05 16:50:03 +0200

    fix: Expand sdkDebug to include additional roles in access control retrieval

--- apps/earn-protocol-institutions/app/server-handlers/sdk/sdk-debug.ts
+++ apps/earn-protocol-institutions/app/server-handlers/sdk/sdk-debug.ts
@@ -11,20 +11,36 @@ export const sdkDebug = async (institutionID: string) => {
     value: '0x0000000000000000000000000000000000000000',
   })
   // eslint-disable-next-line camelcase
-  const [debugAQ, debugGrantGeneralRoleTX_DO_NOT_EXECUTE] = await Promise.all([
-    institutionSDK.armada.accessControl.getAllAddressesWithGeneralRole({
-      chainId: NetworkIds.BASEMAINNET, // Base
-      role: GeneralRoles.ADMIRALS_QUARTERS_ROLE,
-    }),
-    institutionSDK.armada.accessControl.grantGeneralRole({
-      chainId: NetworkIds.BASEMAINNET, // Base
-      role: GeneralRoles.DECAY_CONTROLLER_ROLE,
-      targetAddress: dummyAddress, // Replace with actual address
-    }),
-  ])
+  const [debugAQ, debugDC, debugG, debugSK, debugGrantGeneralRoleTX_DO_NOT_EXECUTE] =
+    await Promise.all([
+      institutionSDK.armada.accessControl.getAllAddressesWithGeneralRole({
+        chainId: NetworkIds.BASEMAINNET, // Base
+        role: GeneralRoles.ADMIRALS_QUARTERS_ROLE,
+      }),
+      institutionSDK.armada.accessControl.getAllAddressesWithGeneralRole({
+        chainId: NetworkIds.BASEMAINNET, // Base
+        role: GeneralRoles.DECAY_CONTROLLER_ROLE,
+      }),
+      institutionSDK.armada.accessControl.getAllAddressesWithGeneralRole({
+        chainId: NetworkIds.BASEMAINNET, // Base
+        role: GeneralRoles.GOVERNOR_ROLE,
+      }),
+      institutionSDK.armada.accessControl.getAllAddressesWithGeneralRole({
+        chainId: NetworkIds.BASEMAINNET, // Base
+        role: GeneralRoles.SUPER_KEEPER_ROLE,
+      }),
+      institutionSDK.armada.accessControl.grantGeneralRole({
+        chainId: NetworkIds.BASEMAINNET, // Base
+        role: GeneralRoles.DECAY_CONTROLLER_ROLE,
+        targetAddress: dummyAddress, // Replace with actual address
+      }),
+    ])
 
   return {
     debugAQ,
+    debugDC,
+    debugG,
+    debugSK,
     // eslint-disable-next-line camelcase
     debugGrantGeneralRoleTX_DO_NOT_EXECUTE,
   }
