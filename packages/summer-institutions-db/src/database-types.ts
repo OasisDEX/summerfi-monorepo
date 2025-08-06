04ad1405b5d04f94400d1f50654023f2056e18dd
Marcin Ciarka <marcin@oazoapps.com>
2025-08-06 17:13:49 +0200

    refactor: Adjust indentation for improved readability in database-types

--- packages/summer-institutions-db/src/database-types.ts
+++ packages/summer-institutions-db/src/database-types.ts
@@ -2,8 +2,8 @@ import type { ColumnType } from 'kysely'
 
 export type Generated<T> =
   T extends ColumnType<infer S, infer I, infer U>
-  ? ColumnType<S, I | undefined, U>
-  : ColumnType<T, T | undefined, T>
+    ? ColumnType<S, I | undefined, U>
+    : ColumnType<T, T | undefined, T>
 
 export interface Institutions {
   displayName: string
