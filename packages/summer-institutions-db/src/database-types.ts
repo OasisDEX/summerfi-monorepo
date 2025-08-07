ce976cd2bec67f0a7aacf12c3588fae0570562a8
Marcin Ciarka <marcin@oazoapps.com>
2025-08-07 12:45:46 +0200

    feat: Update database schema and configuration for institutions and users

--- packages/summer-institutions-db/src/database-types.ts
+++ packages/summer-institutions-db/src/database-types.ts
@@ -1,18 +1,38 @@
-import type { ColumnType } from 'kysely'
+import type { ColumnType } from "kysely";
 
-export type Generated<T> =
-  T extends ColumnType<infer S, infer I, infer U>
-    ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>
+export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
+  ? ColumnType<S, I | undefined, U>
+  : ColumnType<T, T | undefined, T>;
+
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export type UserRole = "RoleAdmin" | "SuperAdmin" | "Viewer";
+
+export interface GlobalAdmins {
+  createdAt: Generated<Timestamp>;
+  id: Generated<number>;
+  userSub: string;
+}
 
 export interface Institutions {
-  displayName: string
-  id: Generated<number>
-  logoFile: Buffer | null
-  logoUrl: Generated<string>
-  name: string
+  createdAt: Generated<Timestamp>;
+  displayName: string;
+  id: Generated<number>;
+  logoFile: Buffer | null;
+  logoUrl: Generated<string>;
+  name: string;
+}
+
+export interface InstitutionUsers {
+  createdAt: Generated<Timestamp>;
+  id: Generated<number>;
+  institutionId: number;
+  role: UserRole | null;
+  userSub: string;
 }
 
 export interface Database {
-  institutions: Institutions
+  globalAdmins: GlobalAdmins;
+  institutions: Institutions;
+  institutionUsers: InstitutionUsers;
 }
