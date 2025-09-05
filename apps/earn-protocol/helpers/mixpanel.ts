fc689bd7fc6b8bff262b4f27ac5f20a3bd0ce614
Marcin Ciarka <marcin@oazoapps.com>
2025-09-05 15:31:23 +0200

    Remove console logging of hostname from Mixpanel event tracking

--- apps/earn-protocol/helpers/mixpanel.ts
+++ apps/earn-protocol/helpers/mixpanel.ts
@@ -35,9 +35,6 @@ export function trackEvent<E extends EarnProtocolEventNames>(
     },
   }
 
-  // eslint-disable-next-line no-console
-  console.log('window.location.hostname', window.location.hostname)
-
   // eslint-disable-next-line turbo/no-undeclared-env-vars, @typescript-eslint/prefer-nullish-coalescing
   if (process.env.TURBOPACK || window.location.hostname.includes('staging')) {
     // eslint-disable-next-line no-console
