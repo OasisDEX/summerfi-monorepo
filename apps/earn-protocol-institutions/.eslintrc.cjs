8353b50a06fd0399c88caf44a8c8afb830f3f7a0
Marcin Ciarka <marcin@oazoapps.com>
2025-09-08 17:13:14 +0200

    Upgrade Next.js to version 15.5.2 (#1149)

--- apps/earn-protocol-institutions/.eslintrc.cjs
+++ apps/earn-protocol-institutions/.eslintrc.cjs
@@ -3,6 +3,7 @@ module.exports = {
   root: true,
   extends: ['@summerfi/eslint-config/next.cjs'],
   parser: '@typescript-eslint/parser',
+  ignorePatterns: ['next-env.d.ts'],
   parserOptions: {
     project: ['./tsconfig.json'],
     sourceType: 'module',
