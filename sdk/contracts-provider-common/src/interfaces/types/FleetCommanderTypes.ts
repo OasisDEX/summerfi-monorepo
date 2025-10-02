5cec64c597368b27665b958645664002463c6785
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-10-02 19:17:07 +0200

    Refactor FleetCommanderTypes and EncodeStrategy to use IAddress type for Ark and target parameters

--- sdk/contracts-provider-common/src/interfaces/types/FleetCommanderTypes.ts
+++ sdk/contracts-provider-common/src/interfaces/types/FleetCommanderTypes.ts
@@ -1,12 +1,14 @@
+import type { IAddress } from '@summerfi/sdk-common'
+
 /**
  * @name RebalanceDataSolidity
  * @description Data structure for rebalancing assets, used by Keepers of a fleet
  */
 export interface RebalanceDataSolidity {
   /** Ark where the tokens are taken from */
-  fromArk: bigint
+  fromArk: IAddress
   /** Ark where the tokens are moved to */
-  toArk: bigint
+  toArk: IAddress
   /** Amount of tokens to be moved */
   amount: bigint
 }
