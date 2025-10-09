efd2394f412679f1107e16aa9a41001a5e28de15
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-10-09 12:20:53 +0200

    Refactor vault tests to improve APY validation and enhance code clarity

--- sdk/sdk-e2e/e2e/armadaProtocol.vault.test.ts
+++ sdk/sdk-e2e/e2e/armadaProtocol.vault.test.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Address, ArmadaVaultId, getChainInfoByChainId } from '@summerfi/sdk-common'
+import {
+  Address,
+  ArmadaVaultId,
+  getChainInfoByChainId,
+  type IArmadaVaultInfo,
+} from '@summerfi/sdk-common'
 
 import { ChainConfigs, SharedConfig } from './utils/testConfig'
 import { createTestSDK } from './utils/sdkInstance'
@@ -26,45 +31,30 @@ describe('Armada Protocol - Vault', () => {
     const vaults = await sdk.armada.users.getVaultInfoList({
       chainId,
     })
+    assert(vaults.list.length > 0, 'No vaults found')
     console.log('All vaults info:\n', vaults.list.map(stringifyArmadaVaultInfo).join('\n'))
 
-    vaults.list.forEach((vault) => {
-      assert(vault.apys != null, `Vault ${vault.id.toString()} should have apys property`)
-      assert('live' in vault.apys, `Vault ${vault.id.toString()} apys should have live property`)
-      assert(
-        'sma24h' in vault.apys,
-        `Vault ${vault.id.toString()} apys should have sma24h property`,
-      )
-      assert(
-        'sma7day' in vault.apys,
-        `Vault ${vault.id.toString()} apys should have sma7day property`,
-      )
-      assert(
-        'sma30day' in vault.apys,
-        `Vault ${vault.id.toString()} apys should have sma30day property`,
-      )
-    })
-    console.log('All vaults have valid apys property')
-  })
+    vaults.list.forEach(validateApys)
 
-  it('should get a specific vault info', async () => {
-    const vaultInfo = await sdk.armada.users.getVaultInfo({
-      vaultId,
-    })
-    assert(vaultInfo != null, 'Vault not found')
-    console.log('Specific vault info:\n', stringifyArmadaVaultInfo(vaultInfo))
+    it('should get a specific vault info', async () => {
+      const vaultInfo = await sdk.armada.users.getVaultInfo({
+        vaultId,
+      })
+      assert(vaultInfo != null, 'Vault not found')
+      console.log('Specific vault info:\n', stringifyArmadaVaultInfo(vaultInfo))
 
-    assert(vaultInfo != null, 'Vault not found')
-    assert(vaultInfo.apys != null, 'apys property should exist')
-    assert('live' in vaultInfo.apys, 'apys should have live property')
-    assert('sma24h' in vaultInfo.apys, 'apys should have sma24h property')
-    assert('sma7day' in vaultInfo.apys, 'apys should have sma7day property')
-    assert('sma30day' in vaultInfo.apys, 'apys should have sma30day property')
-    console.log('APYs:', {
-      live: vaultInfo.apys.live?.toString(),
-      sma24h: vaultInfo.apys.sma24h?.toString(),
-      sma7day: vaultInfo.apys.sma7day?.toString(),
-      sma30day: vaultInfo.apys.sma30day?.toString(),
+      validateApys(vaultInfo)
     })
   })
 })
+
+const validateApys = (vault: IArmadaVaultInfo) => {
+  assert(vault.apys != null, `Vault ${vault.id.toString()} should have apys property`)
+  assert('live' in vault.apys, `Vault ${vault.id.toString()} apys should have live property`)
+  assert('sma24h' in vault.apys, `Vault ${vault.id.toString()} apys should have sma24h property`)
+  assert('sma7day' in vault.apys, `Vault ${vault.id.toString()} apys should have sma7day property`)
+  assert(
+    'sma30day' in vault.apys,
+    `Vault ${vault.id.toString()} apys should have sma30day property`,
+  )
+}
