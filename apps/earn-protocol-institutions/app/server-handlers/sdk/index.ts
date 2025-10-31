54a8289ec834860459b87c9fcb00e31528a19a9a
Marcin Ciarka <marcin@oazoapps.com>
2025-10-31 17:02:00 +0100

    fix: Update environment variable references from SDK_API_URL to EARN_APP_URL in getInstitutionsSDK

--- apps/earn-protocol-institutions/app/server-handlers/sdk/index.ts
+++ apps/earn-protocol-institutions/app/server-handlers/sdk/index.ts
@@ -7,8 +7,8 @@ const apiClientsList: {
 export const getInstitutionsSDK: (institutionName: string) => SDKAdminManager = (
   institutionName: string,
 ) => {
-  if (!process.env.SDK_API_URL) {
-    throw new Error('SDK_API_URL is not set')
+  if (!process.env.EARN_APP_URL) {
+    throw new Error('EARN_APP_URL is not set')
   }
   if (!institutionName) {
     throw new Error('Institution ID is required')
@@ -18,7 +18,7 @@ export const getInstitutionsSDK: (institutionName: string) => SDKAdminManager = 
     apiClientsList[institutionName] = makeAdminSDK({
       clientId: institutionName, // institution ID
       version: 'v2',
-      apiDomainUrl: process.env.SDK_API_URL,
+      apiDomainUrl: process.env.EARN_APP_URL, // serves as SDK proxy
     })
   }
 
