8032abc67e1ccdd508824d2060d5ea50bd2e79d0
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-11-14 20:41:15 +0100

    feat: Improve code readability by formatting parameter lists in staking reward and simulation handler functions

--- sdk/sdk-client-react/src/handlers/getStakingRewardRatesV2Handler.ts
+++ sdk/sdk-client-react/src/handlers/getStakingRewardRatesV2Handler.ts
@@ -3,6 +3,12 @@ import type { IAddress } from '@summerfi/sdk-common'
 
 export const getStakingRewardRatesV2Handler =
   (sdk: ISDKManager | ISDKAdminManager) =>
-  async ({ rewardTokenAddress, sumrPriceUsd }: { rewardTokenAddress: IAddress; sumrPriceUsd?: number }) => {
+  async ({
+    rewardTokenAddress,
+    sumrPriceUsd,
+  }: {
+    rewardTokenAddress: IAddress
+    sumrPriceUsd?: number
+  }) => {
     return sdk.armada.users.getStakingRewardRatesV2({ rewardTokenAddress, sumrPriceUsd })
   }
