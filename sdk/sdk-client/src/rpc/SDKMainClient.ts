e5bf11c6c97ec3adbcdb265316f1df1bd7e0a952
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2025-12-10 20:17:58 +0100

    chore: Update @trpc/client and @trpc/server to version 11.7.2 across multiple packages

--- sdk/sdk-client/src/rpc/SDKMainClient.ts
+++ sdk/sdk-client/src/rpc/SDKMainClient.ts
@@ -23,7 +23,7 @@ export function createMainRPCClient(params: {
       httpBatchLink({
         url: params.apiURL,
         transformer: SerializationService.getTransformer(),
-        maxURLLength: 13000,
+        maxURLLength: 5000,
         headers() {
           return {
             ...(params.clientId && { 'Client-Id': params.clientId }),
