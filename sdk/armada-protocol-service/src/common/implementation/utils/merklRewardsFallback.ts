2ad10ef1e87ae04bec7ab0c6ca54995c313aee62
Piotr Witek <739075+piotrwitek@users.noreply.github.com>
2026-02-09 15:01:32 +0100

    refactor: update daily emission values in getByFleetAddressFallback function for various chain IDs

--- sdk/armada-protocol-service/src/common/implementation/utils/merklRewardsFallback.ts
+++ sdk/armada-protocol-service/src/common/implementation/utils/merklRewardsFallback.ts
@@ -14,64 +14,64 @@ export const getByFleetAddressFallback = (
   }[]
 } => {
   switch (chainId) {
-    case ChainIds.Mainnet:
-      return {
-        // eth low risk
-        '0x67e536797570b3d8919df052484273815a0ab506': [
-          { token, dailyEmission: BigNumber('67611').shiftedBy(token.decimals).toFixed() },
-        ],
-        // eth high risk
-        '0x2e6abcbcced9af05bc3b8a4908e0c98c29a88e10': [
-          { token, dailyEmission: BigNumber('55556').shiftedBy(token.decimals).toFixed() },
-        ],
-        // usdc low risk
-        '0x98c49e13bf99d7cad8069faa2a370933ec9ecf17': [
-          { token, dailyEmission: BigNumber('55556').shiftedBy(token.decimals).toFixed() },
-        ],
-        // usdc high risk
-        '0xe9cda459bed6dcfb8ac61cd8ce08e2d52370cb06': [
-          { token, dailyEmission: BigNumber('41667').shiftedBy(token.decimals).toFixed() },
-        ],
-        // usdt low risk
-        '0x17ee2d03e88b55e762c66c76ec99c3a28a54ad8d': [
-          { token, dailyEmission: BigNumber('34250').shiftedBy(token.decimals).toFixed() },
-        ],
-      }
     case ChainIds.Base:
       return {
         // usdc low risk
         '0x98c49e13bf99d7cad8069faa2a370933ec9ecf17': [
-          { token, dailyEmission: BigNumber('18111').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('9898').shiftedBy(token.decimals).toFixed() },
         ],
         // eurc low risk
         '0x64db8f51f1bf7064bb5a361a7265f602d348e0f0': [
-          { token, dailyEmission: BigNumber('6000').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('3192').shiftedBy(token.decimals).toFixed() },
         ],
         // eth low risk
         '0x2bb9ad69feba5547b7cd57aafe8457d40bf834af': [
-          { token, dailyEmission: BigNumber('18111').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('24284').shiftedBy(token.decimals).toFixed() },
         ],
       }
     case ChainIds.ArbitrumOne:
       return {
+        // usdc low risk
+        '0x71d77c39db0eb5d086611a2e950198e3077cf58a': [
+          { token, dailyEmission: BigNumber('16221').shiftedBy(token.decimals).toFixed() },
+        ],
         // usdt low risk
         '0x98c49e13bf99d7cad8069faa2a370933ec9ecf17': [
-          { token, dailyEmission: BigNumber('17694').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('2850').shiftedBy(token.decimals).toFixed() },
         ],
-        // usdc low risk (new)
-        '0x71d77c39db0eb5d086611a2e950198e3077cf58a': [
-          { token, dailyEmission: BigNumber('16389').shiftedBy(token.decimals).toFixed() },
-        ],
-        // usdc low risk (hacked)
+        // usdc low risk (hacked, old)
         '0x4f63cfea7458221cb3a0eee2f31f7424ad34bb58': [
           { token, dailyEmission: BigNumber('0').shiftedBy(token.decimals).toFixed() },
+        ],
+      }
+    case ChainIds.Mainnet:
+      return {
+        // eth low risk
+        '0x67e536797570b3d8919df052484273815a0ab506': [
+          { token, dailyEmission: BigNumber('78226').shiftedBy(token.decimals).toFixed() },
+        ],
+        // eth high risk
+        '0x2e6abcbcced9af05bc3b8a4908e0c98c29a88e10': [
+          { token, dailyEmission: BigNumber('47957').shiftedBy(token.decimals).toFixed() },
+        ],
+        // usdc low risk
+        '0x98c49e13bf99d7cad8069faa2a370933ec9ecf17': [
+          { token, dailyEmission: BigNumber('42163').shiftedBy(token.decimals).toFixed() },
+        ],
+        // usdc high risk
+        '0xe9cda459bed6dcfb8ac61cd8ce08e2d52370cb06': [
+          { token, dailyEmission: BigNumber('17054').shiftedBy(token.decimals).toFixed() },
+        ],
+        // usdt low risk
+        '0x17ee2d03e88b55e762c66c76ec99c3a28a54ad8d': [
+          { token, dailyEmission: BigNumber('3717').shiftedBy(token.decimals).toFixed() },
         ],
       }
     case ChainIds.Sonic:
       return {
         // usdc low risk
         '0x507a2d9e87dbd3076e65992049c41270b47964f8': [
-          { token, dailyEmission: BigNumber('10041').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('4,198').shiftedBy(token.decimals).toFixed() },
         ],
       }
 
@@ -79,11 +79,11 @@ export const getByFleetAddressFallback = (
       return {
         // usdc
         '0x252e5aa42c1804b85b2ce6712cd418a0561232ba': [
-          { token, dailyEmission: BigNumber('8767').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('8,826').shiftedBy(token.decimals).toFixed() },
         ],
         // usdt
         '0x2cc190fb654141dfbeac4c0f718f4d511674d346': [
-          { token, dailyEmission: BigNumber('8767').shiftedBy(token.decimals).toFixed() },
+          { token, dailyEmission: BigNumber('8,826').shiftedBy(token.decimals).toFixed() },
         ],
       }
 
