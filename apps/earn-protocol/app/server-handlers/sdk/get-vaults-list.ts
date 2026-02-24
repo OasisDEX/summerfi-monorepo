af5e41ad5772d35b1b1e22c6494c8facd8d617d7
Marcin Ciarka <marcin@oazoapps.com>
2026-02-24 17:28:03 +0100

    Remove GraphQL queries for vault and vaults from test-vault.graphql

--- apps/earn-protocol/app/server-handlers/sdk/get-vaults-list.ts
+++ apps/earn-protocol/app/server-handlers/sdk/get-vaults-list.ts
@@ -1,10 +1,7 @@
 import { SupportedNetworkIds } from '@summerfi/app-types'
 import { getChainInfoByChainId } from '@summerfi/sdk-common'
-import { GraphQLClient } from 'graphql-request'
 
-import { getCachedConfig } from '@/app/server-handlers/cached/get-config'
 import { backendSDK } from '@/app/server-handlers/sdk/sdk-backend-client'
-import { GetVaultsDocument, type GetVaultsQuery } from '@/graphql/clients/test-vault/client'
 
 export const getVaultsListRaw = async () => {
   const vaultsListByNetwork = await Promise.all(
@@ -16,27 +13,6 @@ export const getVaultsListRaw = async () => {
         }),
       ),
   )
-
-  const config = await getCachedConfig()
-  const daoManagedVaultsEnabled = !!config.features?.DaoManagedVaults
-
-  if (daoManagedVaultsEnabled) {
-    const networkGraphQlClient = new GraphQLClient(
-      `https://api.goldsky.com/api/public/project_cmgyeezx300294yp2bgo8cfjo/subgraphs/summer-protocol/1.6.2-deep-cleanup-optimizations-staging/gn`,
-    )
-    const testVaultData = (await networkGraphQlClient.request<GetVaultsQuery>(
-      GetVaultsDocument,
-      {},
-      {
-        origin: 'earn-protocol-app',
-      },
-    )) as unknown as Awaited<ReturnType<typeof backendSDK.armada.users.getVaultsRaw>>
-
-    return {
-      vaults: [...vaultsListByNetwork.flatMap(({ vaults }) => vaults), ...testVaultData.vaults],
-      callDataTimestamp: Date.now(),
-    }
-  }
 
   return {
     vaults: vaultsListByNetwork.flatMap(({ vaults }) => vaults),
