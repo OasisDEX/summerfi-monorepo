fe3253d3c7925733c68de3bdaa455943eb808b37
halaprix <6533433+halaprix@users.noreply.github.com>
2026-02-25 13:34:50 +0100

    refactor: update vaults queries to use VaultsLiteQuery for improved performance and add new VaultsLite query for simplified vault data retrieval

--- packages/summer-earn-protocol-subgraph/src/types.ts
+++ packages/summer-earn-protocol-subgraph/src/types.ts
@@ -4,6 +4,7 @@ import {
   Vault,
   VaultsQuery as GetVaultsQuery,
   HistoricalVaultsQuery as HistoricalVaultsQueryType,
+  VaultsLiteQuery as GetVaultsLiteQuery,
 } from './generated/client'
 
 export interface SubgraphClientConfig {
@@ -11,6 +12,7 @@ export interface SubgraphClientConfig {
   urlBase: string
 }
 export type VaultsQuery = GetVaultsQuery
+export type VaultsLiteQuery = GetVaultsLiteQuery
 export type SubgraphClient = ReturnType<typeof getSdk>
 export type Arks = Vault['arks']
 export type Ark = Arks[number]
