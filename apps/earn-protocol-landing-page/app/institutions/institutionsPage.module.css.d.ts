204f8b86ff67b2bb7927507977c37eb9077d1fee
Marcin Ciarka <marcin@oazoapps.com>
2026-04-27 12:21:20 +0200

    Remove Public Access Vaults and Self-Managed Vaults Pages and Styles; Update Navigation Links for Institutional Support

--- apps/earn-protocol-landing-page/app/institutions/institutionsPage.module.css.d.ts
+++ apps/earn-protocol-landing-page/app/institutions/institutionsPage.module.css.d.ts
@@ -1,22 +1,4 @@
 declare const styles: {
-  readonly "buildBySummerSpacer": string;
-  readonly "enhancedRiskManagementBlock": string;
-  readonly "faqWrapper": string;
-  readonly "finalCTAElement": string;
-  readonly "finalCTAElementsList": string;
-  readonly "finalCTAs": string;
-  readonly "onboardingBlock": string;
-  readonly "pageHeader": string;
-  readonly "pageHeaderDetails": string;
-  readonly "pageSubHeader": string;
-  readonly "promoBlocks": string;
-  readonly "scrollDownButton": string;
-  readonly "scrollDownButtonHidden": string;
-  readonly "securityAndCompliance": string;
-  readonly "securityAndComplianceBlock": string;
-  readonly "subpageDescriptionBlock": string;
-  readonly "subpageDescriptionCard": string;
-  readonly "subpageDescriptionText": string;
   readonly "wrapper": string;
 };
 export = styles;
--- apps/earn-protocol-landing-page/app/institutions/self-managed-vaults/selfManagedVaults.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-declare const styles: {
-  readonly "benefitsDescriptionAndPoints": string;
-  readonly "benefitsHeaderWrapper": string;
-  readonly "faqWrapper": string;
-  readonly "integrationsBlock": string;
-  readonly "wrapper": string;
-};
-export = styles;
-
